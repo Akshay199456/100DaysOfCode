ab837988784dc77b2d818301bd74274594b45a38
Akshay199456 <akshay.kum94@gmail.com>
2019-12-20 16:36:21 -0600

    Added font type

--- Topics/CSS Animations/ProjectLoop/scripts/script.js
+++ Topics/CSS Animations/ProjectLoop/scripts/script.js
@@ -194,3 +194,15 @@ spanListener.addEventListener("mouseout", () => {
 spanListener.addEventListener("click", ()=>{
 	window.open("https://www.amazon.co.jp/%E3%80%90%E5%9B%BD%E5%86%85%E6%AD%A3%E8%A6%8F%E5%93%81%E3%80%91%E3%80%90%E9%9F%B3%E6%A5%BD%E3%83%A9%E3%82%A4%E3%83%96%E7%94%A8-%E3%83%8F%E3%82%A4%E3%83%95%E3%82%A1%E3%82%A4-%E3%82%A4%E3%83%A4%E3%83%BC%E3%83%97%E3%83%A9%E3%82%B0%E3%80%91%E3%83%AB%E3%83%BC%E3%83%97-%E3%80%88%E9%9F%B3%E8%B3%AA%E3%81%AF%E3%82%AF%E3%83%AA%E3%82%A2%E3%81%AA%E3%81%BE%E3%81%BE%E9%9F%B3%E9%87%8F%E3%82%92%E5%9D%87%E7%AD%89%E3%81%AB%E5%AE%89%E5%85%A8%E3%83%AC%E3%83%99%E3%83%AB%E3%81%BE%E3%81%A7%E4%BD%8E%E6%B8%9B%E3%80%89%E3%83%9F%E3%83%A5%E3%83%BC%E3%82%B8%E3%82%B7%E3%83%A3%E3%83%B3-%E3%82%B0%E3%83%AD%E3%83%AA%E3%82%A2%E3%82%B9%E3%82%B4%E3%83%BC%E3%83%AB%E3%83%89/dp/B079TZQX2V/ref=pd_rhf_se_p_img_5?_encoding=UTF8&psc=1&refRID=YTKKA5VHYV44M44HYR8G", "_blank");
 });
+
+
+const twitterListener  = document.getElementById("twitterIcon");
+twitterListener.addEventListener("click", ()=>{
+	window.open("https://twitter.com/loopearplugs?lang=en", "_blank");
+});
+
+const facebookListener  = document.getElementById("facebookIcon");
+facebookListener.addEventListener("click", ()=>{
+	window.open("https://www.facebook.com/loopearplugs/", "_blank");
+});
+
