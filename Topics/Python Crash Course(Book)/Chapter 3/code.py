e1d51483a5a56dcfa00cfefff9e1a461df9e7b13
Akshay199456 <akshay.kum94@gmail.com>
2020-01-12 00:40:30 -0600

    Working on printing list in reverse order

--- Topics/Python Crash Course(Book)/Chapter 3/code.py
+++ Topics/Python Crash Course(Book)/Chapter 3/code.py
@@ -1,12 +1,48 @@
-lists = [1,2,3,"hello"];
+def printList(lists, message):
+	print(message);
+	for i in range(len(lists)):
+		print(lists[i], end = ' ');
+	print("\n");
+
+
+
+lists = ["bye","asta","lavista","hello"];
 
 # Appends element to end of list
-
-lists.append('"world');
-print(lists);
+lists.append('world');
+printList(lists, "\nUsing append: ");
 
 
 # Used to insert elements
+lists.insert(1, "bye");
+printList(lists, "\nUsing insert: ");
 
-lists.insert(1, "bye")
-print(lists);
+
+# Used to delete elements
+del lists[0];
+printList(lists, "\nUsing delete: ");
+
+
+# Used to remove element from end of list or from any other position
+lists.pop();
+printList(lists, "\nUsing pop: ");
+
+
+# Used to remove an element from the list based on the value;
+lists.remove("asta");
+printList(lists, "\nUsing remove: ");
+
+"""
+# Used to sort elements in ascending order. Can't sort element of different data types.
+lists.sort();
+printList(lists, "\nAfter assceding sort: ");
+# For descending order
+lists.sort(reverse=True);
+printList(lists, "\nAfter descending sort: ");
+"""
+
+# To maintain original order of the list
+sortedList = sorted(lists);
+printList(sortedList, "\nUsing sorted() function; new list: ");
+printList(lists, "\nOriginal list:  ");
+
