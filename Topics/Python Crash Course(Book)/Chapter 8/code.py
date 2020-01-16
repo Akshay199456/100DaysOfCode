f22d1aea58eabe63a16f1508fcd89c3ceff0e8db
Akshay199456 <akshay.kum94@gmail.com>
2020-01-15 18:04:36 -0600

    Completed chapter 8

--- Topics/Python Crash Course(Book)/Chapter 8/code.py
+++ Topics/Python Crash Course(Book)/Chapter 8/code.py
@@ -1,8 +1,13 @@
+import hello_world
+
+'''
 test = []
 print(1==True)
+'''
+
 
 # Testing for list modification
-
+'''
 def modify_list(test_list):
 	""" Modifying the contents of the list """
 	count = 0
@@ -19,7 +24,21 @@ print('List before modification: ', test_list)
 
 
 
-# Slicing doesn't modify the list
+# Slicing doesn't modify the list, rather generates a copy
 print("\n\n Slicing the list now \n ")
 modify_list(test_list[:])
 print('List after modification: ', test_list)
+'''
+
+# Testing arbitrary number of arguments
+'''
+def test_function(age, *location, **arguments):
+	print("Age: ", age)
+	print("Location: ", location)
+	print("Arguments: ", arguments)
+
+test_function(23, 'Houston', 'Akshay')
+'''
+
+# Testing importing from different module
+hello_world.hello()
