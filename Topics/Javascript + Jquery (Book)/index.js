380919a873b9dce08069d46dc4bd2bf3f52348ac
Akshay199456 <akshay.kum94@gmail.com>
2020-01-19 19:12:54 -0600

    Completed jQuery

--- Topics/Javascript + Jquery (Book)/index.js
+++ Topics/Javascript + Jquery (Book)/index.js
@@ -1,1 +1,11 @@
 console.log('connected!');
+
+let $hotClass = $('.hot');
+console.log("Hot class: ", $hotClass);
+
+
+// Testing animation
+$hotClass.on('click', function(){
+	console.dir(`${$(this)}`);
+	$(this).slideUp('slow');
+});
