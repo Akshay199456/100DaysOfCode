699be35d30adf0f38002195b8f6e1d09362b50c9
Akshay199456 <akshay.kum94@gmail.com>
2020-01-29 21:37:08 -0600

    map layout implemented

---
+++ Topics/Data Structures and Alogrithms/Practice/map.cpp
@@ -0,0 +1,17 @@
+/*
+	Will implement it when I have the time. Most of the methods are same to that of unordered_map.
+	
+	Maps are basically like unordered_maps but the elements in maps are sorted by its key unlike a 
+	Usually take O(log n) for their operations as it is implemented using a tree.
+
+	Methods are:
+		1. begin() -> iterator to beginning
+		2. end() -> iterator to end
+		3. empty() -> checks if map is empty
+		4. size() -> returns the size of the map
+		5. insert() -> inserts element. Inserts iterator to element if it exists else it inerts the element
+		6. erase() -> erases elements
+		7. clear() -> clears the map
+		8. find() -> finds the element in the map and returns the iterator to it else returns map.end()
+		9. count() -< returns the number of occurrences of the element in the map
+*/
