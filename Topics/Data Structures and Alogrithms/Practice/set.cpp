a5cbb152b5dc22a8526fd1851a923f9334347dee
Akshay199456 <akshay.kum94@gmail.com>
2020-01-29 21:50:41 -0600

    Set layout implemented

---
+++ Topics/Data Structures and Alogrithms/Practice/set.cpp
@@ -0,0 +1,15 @@
+/*
+	Set - Same as an unordered_set but stores its keys in sorted order
+	O(log N) operation time as it it's implemented as a tree
+	
+	Methods:
+		1. begin() -> returns an iterator the beginning of the set
+		2. end() -> returns an iterator to the end of the set
+		3. size() -> retuns the number of elements in the set
+		4. insert() -> inserts element into the set
+		5. erase() -> removes an element from the set
+		6. clear() -> removes all the elements from the set
+		7. find() -> finds an element in the set and returns an iterator to it if found else returns set.end()
+		8. count() -> returns the number of occurrences of element in the set
+*/
+
