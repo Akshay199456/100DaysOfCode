73c02f40b4bad765713b260bd907ab1523f437e7
Akshay199456 <akshay.kum94@gmail.com>
2020-02-02 11:50:42 -0600

    Implemented graph

--- Topics/Data Structures and Alogrithms/Practice/C++/graph.cpp
+++ Topics/Data Structures and Alogrithms/Practice/C++/graph.cpp
@@ -5,11 +5,21 @@ const int VERTICES = 5;
 class Graph{
     private:
         vector<int> graph[5];
+        vector<bool> visited;
+        queue<int> elements;
+        void bfsImplement(queue<int> &, vector<bool> &);
     public:
+        Graph();
         void createGraph(vector<vector<int>>);
         void displayGraph();
-        
+        void bfs();      
 };
+
+Graph::Graph(){
+    for(int i=0; i<VERTICES; i++){
+        visited.push_back(false);
+    }
+}
 
 void Graph::createGraph(vector<vector<int>> list){
     cout<<"\n Creating the graph \n";
@@ -22,11 +32,37 @@ void Graph::createGraph(vector<vector<int>> list){
 void Graph::displayGraph(){
     cout<<"\n Displaying the contents of the graph \n";
     for(int i=0; i<VERTICES; i++){
-        cout<<"Element "<<i<<"->";
+        cout<<" Element "<<i<<"->";
         for(int j=0; j<graph[i].size(); j++)
             cout<<" "<<graph[i][j];
         cout<<"\n";
     }
+}
+
+void Graph::bfsImplement(queue<int> & elements, vector<bool> & visited){
+    cout<<"\n BFS being implemented \n";
+    if(!elements.empty()){
+        int currentIndex = elements.front();
+        elements.pop();
+        if(!visited[currentIndex]){
+            visited[currentIndex] = true;
+            cout<<"\n Element visited: "<<currentIndex<<endl;
+            for(int j=0; j<graph[currentIndex].size(); j++){
+                if(!visited[graph[currentIndex][j]])
+                    elements.push(graph[currentIndex][j]);
+            }
+        }
+        bfsImplement(elements, visited);
+    }
+}
+
+void Graph::bfs(){
+//     starting index to implement dfs from
+    elements.push(0);
+    cout<<"--------------------------\n";
+    cout<<"\n Implementing BFS \n";
+    cout<<"--------------------------\n";
+    bfsImplement(elements, visited);    
 }
 
 int main(){
@@ -35,5 +71,6 @@ int main(){
     Graph object = Graph();
     object.createGraph(list);
     object.displayGraph();
+    object.bfs();
     return 0;
 }
