fafbde462a5c6e09512fd90aa09f4f42dad622ba
Akshay199456 <akshay.kum94@gmail.com>
2020-02-05 13:36:30 -0600

    Completed problem 350(Leetcode)

--- Topics/Data Structures and Alogrithms/Leetcode(Website)/350(Intersection of Two Arrays II).cpp
+++ Topics/Data Structures and Alogrithms/Leetcode(Website)/350(Intersection of Two Arrays II).cpp
@@ -29,7 +29,35 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 
 1. Using maps
 
+The way I solved this was that you would contruct 2 maps for each of the arrays. You would then go over
+the map of one and check for the number of min occurrences in the second map if the element existed in it.
 
+Time complexity: O(m+n)
+Space complexity: O(m+n)
+
+*/
+
+/*
+-------------------------    Other approaches:
+
+1. Using map(Better)
+
+You can improve upon the previous solution by only using one map instead of two. This way we save a 
+little amount of space. After constructing one of the maps and going over the vector of the other,
+you would check if that particular element existed in the map and if it does, subtract its occurrence
+by one in the map and append it to the result vector.
+
+Time complexity: O(m+n)
+Space complexity: O(m)
+
+
+2. Sorting and using a double pointer(Better in some instances)
+
+If the inputs are already sorted, this solution will be more efficient since we are just using two pointers
+to iterate through the arrays. 
+
+Time complexity: O(mlogm) + O(nlogn) + O(n)[shorter array] = O(mlogm + nlogn)
+Space complexity: O(1);
 */
 
 
@@ -69,3 +97,52 @@ public:
             return returnIntersection(map2, map1);
     }
 };
+
+
+// Other approaches(2)
+
+class Solution {
+public:
+    
+    void sortVectors(vector<int> & nums1, vector<int> & nums2){
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+    }
+    
+    vector<int> findIntersection(vector<int> nums1, vector<int> nums2){
+        int index1 = 0, index2 = 0;
+        vector<int> result;
+        while( (index1 < nums1.size()) && (index2 < nums2.size())){
+            if(nums1[index1] == nums2[index2]){
+                result.push_back(nums1[index1]);
+                index1++;
+                index2++;
+            }
+            else if(nums1[index1] < nums2[index2])
+                index1++;
+            else
+                index2++;
+        }
+        return result;
+    }
+    
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sortVectors(nums1, nums2);
+        return findIntersection(nums1, nums2);
+    }
+};
+
+/*
+-------------------------Follow up questions:
+
+1. Use the other approaches(2) solution. It would be a O(n) time complexity solution[n is shorted array]
+2. The other approach(1) would be a better solution since it will be a O(m) time comlpexity solution
+while the sorting solution would take O(mlogm) time.
+3.  I think the goal of this question is to test whether the interview understands some of the data engineering techniques. From a data engineer's perspective, basically there are three ideas to solve the question:
+
+Store the two strings in distributed system(whether self designed or not), then using MapReduce technique to solve the problem;
+
+Processing the Strings by chunk, which fits the memory, then deal with each chunk of data at a time;
+
+Processing the Strings by streaming, then check. 
+*/
