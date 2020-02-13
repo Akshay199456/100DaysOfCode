ff87ab97f2d23796bc90739b3c879e67dc830816
Akshay199456 <akshay.kum94@gmail.com>
2020-02-12 23:46:43 -0600

    Added my approaches for problem 28(Leetcode)

--- Topics/Data Structures and Alogrithms/Leetcode(Website)/28(Implement strStr()).cpp
+++ Topics/Data Structures and Alogrithms/Leetcode(Website)/28(Implement strStr()).cpp
@@ -20,3 +20,74 @@ What should we return when needle is an empty string? This is a great question t
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 */
 
+/*
+-------------------------    My Approaches:
+
+1. 
+
+While I was able to figure out this approach almost immediately, I implemented it poorly. Whatever
+be the reason(was probably tired), I didn't take into consideration different edge cases that would
+have caused the algorithm to fail. As a result, while implementing this algorithm and running, I ran
+into 'wrong answers' 4 times.
+
+The approach used in this algorithm basically uses two pointers to keep track of the needle and
+haystack. Obviously, if needle.size() > haystack.size() as well as haystack.size() ==0, we return
+-1 as we can't find the needle in it. If instead, the needle.size() == 0, we return 0 as defined
+by the problem statement in this case. If they are of the same size, we write a separate algo to
+increase the speed taken for that particular case. Else we follow a general algorithm where we 
+check if the characters of the haystack and the needle are the same.
+
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+// My aoproaches(1)
+class Solution {
+public:
+    int getIndex(string haystack, string needle){
+        if(needle.size() == 0)
+            return 0;
+        else if(haystack.size() == 0 || haystack.size() < needle.size())
+            return -1;
+        else if(haystack.size() == needle.size()){
+            int pointer = 0;
+            while(pointer < haystack.size()){
+                if(!(haystack[pointer] == needle[pointer]))
+                    return -1;
+                else
+                    pointer++;
+            }    
+            return 0;
+        }
+        else{
+            int hayptr = 0, needptr = 0, index = 0;
+            bool found = false;
+            while(hayptr < haystack.size() && needptr < needle.size()){
+                if(haystack[hayptr] == needle[needptr]){
+                    if(!found){
+                        index = hayptr;
+                        found = true;
+                    }
+                    needptr++;
+                }
+                
+                else{
+                    if(found)
+                        hayptr = index;
+                    found = false;
+                    needptr = 0;
+                }
+                hayptr++;
+            }
+            
+            if(needptr == needle.size())
+                return index;
+            else
+                return -1;
+        }
+    }
+    
+    int strStr(string haystack, string needle) {
+        return getIndex(haystack, needle);
+    }
+};
