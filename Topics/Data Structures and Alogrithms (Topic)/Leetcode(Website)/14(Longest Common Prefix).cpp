fed70071d3e5d2e8a4832ee1b2d9968a84393c7e
Akshay199456 <akshay.kum94@gmail.com>
2020-02-21 23:33:55 -0600

    Currently working on Problem 14(Leetcode) My approaches(1)

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/14(Longest Common Prefix).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/14(Longest Common Prefix).cpp
@@ -19,5 +19,34 @@ All given inputs are in lowercase letters a-z.
 */
 
 /*
+-------------------------My Approaches:
 
 */
+
+// My Approaches(1)
+class Solution {
+public:
+    string longestPrefix(vector<string> strs){
+        string result = "";
+        string begElement = strs[0];
+//         i -> character; j -> string element
+        for( int i = 0; i < begElement.size(); i++){
+            for( int j = 1; j < strs.size(); j++){
+                string currElement = strs[j];
+                if( (i == currElement.size()) || (begElement[i] != currElement[i]))
+                    return result;
+            }
+            result.push_back(begElement[i]);
+        }
+        return result;
+    }
+    
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0)
+            return "";
+        else if(strs.size() == 1)
+            return strs[0];
+        else
+            return longestPrefix(strs);
+    }
+};
