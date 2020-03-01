9985926b9102fc1cc6849ae626e0860b7e6f2a56
Akshay199456 <akshay.kum94@gmail.com>
2020-02-29 23:38:47 -0600

    Completed Chapter 1

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 1/Welcome.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 1/Welcome.java
@@ -1,3 +1,8 @@
+/*
+To compile a java file:  javac Welcome.java
+To run a java file: java Welcome
+*/
+
 public class Welcome{
 	public static void main(String [] args){
 		System.out.println("Hello World");
