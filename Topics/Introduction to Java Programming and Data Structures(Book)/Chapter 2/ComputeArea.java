781009c4eb73b4bd3b9882d80b5dd81e05751beb
Akshay199456 <akshay.kum94@gmail.com>
2020-03-05 20:55:10 -0600

    Working on chapter 2 of Introduction to java algorithms and data structures

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 2/ComputeArea.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 2/ComputeArea.java
@@ -3,6 +3,7 @@ import java.util.Scanner; // Scanner is in the java.util package
 public class ComputeArea{
 	public static void main(String [] args){
 		double radius, area;
+		final double PI = 22.0/7.0;
 
 		// Creates Scanner object 
 		Scanner input = new Scanner(System.in);
@@ -10,7 +11,8 @@ public class ComputeArea{
 		radius = input.nextDouble();
 
 		area = Math.PI * radius * radius;
-		System.out.print("The area of the circle is : " + area);
-		System.out.println('Area');
+		System.out.println("The area of the circle is : " + area);
+
+		System.out.println("The value of PI is : " + PI);
 	}
 }
