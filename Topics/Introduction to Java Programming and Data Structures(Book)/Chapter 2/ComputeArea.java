e07af1ef303260a2a2f1b1e22eebdf582ecee41b
Akshay199456 <akshay.kum94@gmail.com>
2020-03-05 23:44:05 -0600

    Working on chapter 2 of Introduction to java algorithms and data structures

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 2/ComputeArea.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 2/ComputeArea.java
@@ -3,6 +3,7 @@ import java.util.Scanner; // Scanner is in the java.util package
 public class ComputeArea{
 	public static void main(String [] args){
 		double radius, area;
+		// declaring and defining a constant which in Java is indicated with 'final' keyword
 		final double PI = 22.0/7.0;
 
 		// Creates Scanner object 
@@ -13,6 +14,14 @@ public class ComputeArea{
 		area = Math.PI * radius * radius;
 		System.out.println("The area of the circle is : " + area);
 
+		// Using a final variable
 		System.out.println("The value of PI is : " + PI);
+
+		// This results in an integer output even though you are using a division operator
+		System.out.println("Result of division: " + 5/2 );
+
+		// This results in a float operation; either one of the operands must be float
+		System.out.println("Result of division 2: " + 5.0/2 );
+
 	}
 }
