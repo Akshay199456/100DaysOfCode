45e3a9980010a52f28263d5d30918eaed79834df
Akshay199456 <akshay.kum94@gmail.com>
2020-03-07 23:13:59 -0600

    Completed chapter 4 of Introduction to Java programming and data structures(Book)

---
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 4/HelloWorld.java
@@ -0,0 +1,8 @@
+import java.lang.Character;
+
+class HelloWorld{
+	public static void main(String [] args){
+		System.out.println("Hello World!");
+		System.out.println("Uppercase of a: " + Character.toUpperCase('a'));
+	}
+}
