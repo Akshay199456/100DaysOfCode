7e1d7f01721394108289d38f678af74f3c96ff7c
Akshay199456 <akshay.kum94@gmail.com>
2020-03-12 14:34:39 -0500

    Completed Problem 21 - Leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/21(Merge Two Sorted Lists).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/21(Merge Two Sorted Lists).cpp
@@ -15,10 +15,22 @@ Output: 1->1->2->3->4->4
 
 The first approach uses two pointers to compare the values of the two linked lists and then creates
 new nodes and inserts the values into them progressing the pointers that had the lower value. This
-continues till all the values have been inserted into the new list.
+continues till all the values have been inserted into the new list.r
 
 Time complexity: O(m+n)
 Space complexity: O(m+n)
+
+
+2. Using two pointers but not creating new nodes and instead redirecting the next pointers of the 
+nodes (Better)
+
+Rather than creating new nodes in this approach, we instead change the next pointer of the nodes
+so that they point to the next nodes. We continue this till all the nodes are linked.
+
+Time complexity: O(m+n)
+Space complexity: O(1)
+
+
 
 */
 
@@ -66,6 +78,56 @@ public:
         
         return head;
     }
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        return mergedLists(l1, l2, NULL);
+    }
+};
+
+
+
+// My approach(2)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void linkNode(ListNode * & pointer, ListNode * & head, ListNode * & temp){
+        if(!head)
+            head = pointer;
+        else
+            temp->next = pointer;
+        temp = pointer;
+        pointer = pointer->next;
+    }
+    
+    void copyToEnd(ListNode * & pointer, ListNode * & head, ListNode * & temp){
+        while(pointer)
+            linkNode(pointer, head, temp);
+    }
+    
+    ListNode * mergedLists(ListNode * l1, ListNode * l2, ListNode * head){
+        ListNode * temp = head;
+        while(l1 && l2){
+            if(l2->val <= l1->val)
+                linkNode(l2, head, temp);
+            else
+                linkNode(l1, head, temp);
+        }
+        
+        if(!l1)
+            copyToEnd(l2, head, temp);
+        else
+            copyToEnd(l1, head, temp);
+        
+        return head;
+    }
+    
     
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         return mergedLists(l1, l2, NULL);
