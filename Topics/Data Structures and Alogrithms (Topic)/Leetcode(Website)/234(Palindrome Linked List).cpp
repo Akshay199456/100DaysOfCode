0805b95f1a568bd7bf5e92fd36a8e58a8aad2dc3
Akshay199456 <akshay.kum94@gmail.com>
2020-03-14 21:52:07 -0500

    Added My Approaches(2) - Pb 234(Leetcode)

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/234(Palindrome Linked List).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/234(Palindrome Linked List).cpp
@@ -26,6 +26,29 @@ a palindrome by checking each element with its counter element from the end of t
 
 Time complexity : O(n)
 Space complexity: O(n)
+
+
+
+2. Modifying the original linked list[Same]
+
+This approach modifies the original linked list. It breaks it down into a few steps
+	a. getting the length of the original linked list.
+	b. getting the second head by detecting whether the original list had an even or ordd number
+	of elements. If it had odd, we remove the middle element as it doesn't factor into determing
+	whether the ;inked list is a pallindrome or not. If it's even, we keep the linked list as it
+	is. The second head will start at the middle of the linked list(modified or not).
+	c. reverse the linked list that start at the second head. At the end of the reversal, you want
+	to return the tail of this linkedlist
+	d. Compare the values starting at both the heads.
+
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+
+/*
+-------------------------Other approaches
+
+1. 
 */
 
 
@@ -63,3 +86,107 @@ public:
             return checkPalindrome(head);
     }
 };
+
+
+
+
+// My Approaches(2)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getLength(ListNode * head){
+        int count = 0;
+        ListNode * temp = head;
+        while(temp){
+            ++count;
+            temp = temp -> next;
+        }
+        return count;
+    }
+    
+    void removeMiddleNode(ListNode * head, int length){
+        ListNode * temp = head, * curr = NULL;
+        int count = 0;
+        while(count < length/2){
+            curr = temp;
+            temp = temp -> next;
+            ++count;
+        }
+        
+        curr->next = temp->next;
+        temp->next = NULL;
+    }
+    
+    ListNode * getSecondHead(int length, ListNode * head){
+        ListNode * temp = head;
+        int count = 0;
+        if(length %2 !=0)
+            removeMiddleNode(head, length--);
+        while(count < length/2){
+            temp = temp->next;
+            ++count;
+        }
+        return temp;
+    }
+    
+    ListNode * reverseList(ListNode * secondHead, ListNode *& newHead){
+        if(!(secondHead->next)){
+            newHead = secondHead;
+            return secondHead;
+        }
+        else{
+            ListNode * nextNode = reverseList(secondHead->next, newHead);
+            nextNode->next = secondHead;
+            secondHead->next = NULL;
+            return secondHead;
+        }
+        
+    }
+    
+    void displayList(ListNode * head){
+        ListNode * temp = head;
+        while(temp){
+            cout<<temp->val<<"->";
+            temp = temp->next;
+        }
+        cout<<"\n";
+    }
+    
+    bool compareHeads(ListNode * head, ListNode * newHead){
+        ListNode * temp = head, * newTemp  = newHead;
+        while(temp && newTemp){
+            if(temp-> val != newTemp->val)
+                return false;
+            temp = temp->next;
+            newTemp = newTemp->next;
+        }
+        return true;
+    }
+    
+    bool checkPalindrome(ListNode * head){
+        ListNode * newHead = NULL;
+        int length = getLength(head);
+        ListNode * secondHead = getSecondHead(length, head);
+        reverseList(secondHead, newHead);
+        return compareHeads(head, newHead);
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        if( !head || !(head->next))
+            return true;
+        else
+            return checkPalindrome(head);
+    }
+};
+
+
+
+
+// Other Approaches(1)
