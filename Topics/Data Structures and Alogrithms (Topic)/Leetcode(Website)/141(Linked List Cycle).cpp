1a8a2d4f9040f1a3d4db6e5ba154b9355be6d1c2
Akshay199456 <akshay.kum94@gmail.com>
2020-03-27 18:16:30 -0500

    Completed Problem 141 - Leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/141(Linked List Cycle).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/141(Linked List Cycle).cpp
@@ -46,6 +46,40 @@ then we know there's no cycle.
 
 Time complexity: O(n)
 Space complexity: O(1)
+
+
+Complexity analysis
+
+Time complexity : O(n)O(n). Let us denote nn as the total number of nodes in the linked list. To analyze its time complexity, we consider the following two cases separately.
+
+List has no cycle:
+The fast pointer reaches the end first and the run time depends on the list's length, which is O(n)O(n).
+
+List has a cycle:
+We break down the movement of the slow pointer into two steps, the non-cyclic part and the cyclic part:
+
+The slow pointer takes "non-cyclic length" steps to enter the cycle. At this point, the fast pointer has already reached the cycle. \text{Number of iterations} = \text{non-cyclic length} = NNumber of iterations=non-cyclic length=N
+
+Both pointers are now in the cycle. Consider two runners running in a cycle - the fast runner moves 2 steps while the slow runner moves 1 steps at a time. Since the speed difference is 1, it takes \dfrac{\text{distance between the 2 runners}}{\text{difference of speed}} 
+difference of speed
+distance between the 2 runners
+​	
+  loops for the fast runner to catch up with the slow runner. As the distance is at most "\text{cyclic length K}cyclic length K" and the speed difference is 1, we conclude that
+\text{Number of iterations} = \text{almost}Number of iterations=almost "\text{cyclic length K}cyclic length K".
+
+Therefore, the worst case time complexity is O(N+K)O(N+K), which is O(n)O(n).
+*/
+
+/*
+-------------------------Other approaches
+
+1. Using hashtable
+
+We can use a hashtable to store the position of the pointers. This way whenever we approach a 
+duplicate, we know that we have encountered a cycle start point
+
+Time complexity: O(n)
+Space complexity: O(n)
 */
 
 
