634c5078c05719034f6cac838f8f75ed64d84ede
Akshay199456 <akshay.kum94@gmail.com>
2020-03-28 14:38:35 -0500

    Completed Problem 104 - Leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/104(Maximum Depth of Binary Tree).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/104(Maximum Depth of Binary Tree).cpp
@@ -28,7 +28,8 @@ This approach uses DFS to get the max level at each node from the right and the 
 and returns the max of the two.
 
 Time complexity: O(n)
-Space complexity: O(n)
+Space complexity: O(h) -> to clear up, if the tree is skewed the space complexity depends on
+the height of the tree which will be of height n; if it's balanced, then the height will be log n.
 */
 
 // My Approaches(1)
@@ -41,6 +42,19 @@ Space complexity: O(n)
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/*
+-------------------------Other approaches
+
+1. BFS (Worse)
+
+Using a queue to store the elements as well as their level
+
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+
+// My Approaches(1)
 class Solution {
 public:
     int getMaxDepth(TreeNode * node, int level){
@@ -57,5 +71,48 @@ public:
             return 0;
         else
             return getMaxDepth(root, 1);
+    }
+};
+
+
+// Other Approaches(1)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getMaxDepth(queue<pair<TreeNode *, int>> q){
+        int maxLevel = 1;
+        
+        while(!q.empty()){
+            pair<TreeNode *, int> currentElement = q.front();
+            q.pop();
+            TreeNode * node = currentElement.first;
+            int level = currentElement.second;
+            if(level > maxLevel)
+                maxLevel = level;
+            if(node->left)
+                q.push(make_pair(node->left, level+1));
+            if(node->right)
+                q.push(make_pair(node->right, level+1));
+        }
+        
+        return maxLevel;
+    }
+    
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        else{
+            queue<pair<TreeNode *, int>> q;
+            q.push(make_pair(root, 1));
+            return getMaxDepth(q);
+        }
     }
 };
