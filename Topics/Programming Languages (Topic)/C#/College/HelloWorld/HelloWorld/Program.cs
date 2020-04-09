b859bdb40e8b8a4b17cabef9f99faa1980ffddac
Akshay199456 <akshay.kum94@gmail.com>
2020-04-08 21:56:10 -0500

    Got C# set up along with LaYumba

---
+++ Topics/Programming Languages (Topic)/C#/College/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Option<int> age = Some(23);
+            string ageFunc(Option<int?> agegreeting) => agegreeting.Match(None: () => "hey hey!", Some: (num) => $"Your age is {num}");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ageFunc(23));
+            Console.WriteLine(ageFunc(None));
+        }
+    }
+}
