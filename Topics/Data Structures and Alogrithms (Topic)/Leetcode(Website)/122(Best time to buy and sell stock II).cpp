2c3e60b7d8375282c8098458311d6632a07db854
Akshay199456 <akshay.kum94@gmail.com>
2020-04-09 23:02:20 -0500

    Formatted pb 122 - leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/122(Best time to buy and sell stock II).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/122(Best time to buy and sell stock II).cpp
@@ -65,7 +65,14 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
     
     Algorithm
 
-    This solution follows the logic used in Approach 2 itself, but with only a slight variation. In this case, instead of looking for every peak following a valley, we can simply go on crawling over the slope and keep on adding the profit obtained from every consecutive transaction. In the end,we will be using the peaks and valleys effectively, but we need not track the costs corresponding to the peaks and valleys along with the maximum profit, but we can directly keep on adding the difference between the consecutive numbers of the array if the second number is larger than the first one, and at the total sum we obtain will be the maximum profit. This approach will simplify the solution. This can be made clearer by taking this example:
+    This solution follows the logic used in Approach 2 itself, but with only a slight variation. 
+    In this case, instead of looking for every peak following a valley, we can simply go on 
+    crawling over the slope and keep on adding the profit obtained from every consecutive 
+    transaction. In the end,we will be using the peaks and valleys effectively, but we need not 
+    track the costs corresponding to the peaks and valleys along with the maximum profit, but we 
+    can directly keep on adding the difference between the consecutive numbers of the array if the 
+    econd number is larger than the first one, and at the total sum we obtain will be the maximum 
+    profit. This approach will simplify the solution. This can be made clearer by taking this example:
 
     [1, 7, 2, 3, 6, 7, 6, 7]
 
