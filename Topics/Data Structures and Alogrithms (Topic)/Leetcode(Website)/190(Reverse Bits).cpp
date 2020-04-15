3fb1f20ef0d73eb8fe302ba4aa2b1499940d47ed
Akshay199456 <akshay.kum94@gmail.com>
2020-04-15 18:42:47 -0500

    Completed pb 190 - leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/190(Reverse Bits).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/190(Reverse Bits).cpp
@@ -45,6 +45,17 @@ Space complexity: O(n)
 /*
 -------------------------Other approaches
 
+
+1. Reversing bits in groups
+
+
+This approach has been summarized well in the 'Helping Images' folder with respect to this 
+problem so please check that out to get a good idea of how this algorithm works.
+
+To summarize this approach, we divide the bits into 2 groups. We select the first groups of bits by 
+using a bit mask and then shift them right. We then select the second group of bits by using another
+bit mask and shift the result left. We then append the first and second group of bits. We continue
+this till we have reached a single bit and then return the result. 
 */
 
 
@@ -60,5 +71,20 @@ public:
             bitRep[end--] = temp;
         }
         return (uint32_t) (bitRep.to_ulong());
+    }
+};
+
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = n >> 16 | n << 16;
+        n = ((n & 0xff00ff00) >> 8) |((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) |((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) |((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) |((n & 0x55555555) << 1);
+        return n;
     }
 };
