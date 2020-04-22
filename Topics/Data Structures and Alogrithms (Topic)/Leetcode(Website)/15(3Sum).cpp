1a10705119b4b28d6b40620ae2f018e573feaa47
Akshay199456 <akshay.kum94@gmail.com>
2020-04-21 23:00:14 -0500

    Added pb 15 - leetcode

---
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/15(3Sum).cpp
@@ -0,0 +1,36 @@
+/*
+-------------------------Question:
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+/*
+-------------------------My Approaches:
+
+While I was able to obtain all possible answers to this problem including the duplicates in 
+O(n^2) time complexity using the two-sum approach, I wasn't able to get the answer to this specific
+problem where the duplicates can't occur in the final result. Hence my answer will not be valid
+here
+*/
+
+/*
+-------------------------Other approaches
+
+*/
+
+
+// Other Approaches(1)
