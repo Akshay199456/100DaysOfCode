b760dd97c49211b9fb6a584e02c1c6e55015c9ab
Akshay199456 <akshay.kum94@gmail.com>
2020-05-03 20:57:35 -0500

    Completed pb 383 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/383(Ransom Note).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/383(Ransom Note).cpp
@@ -23,11 +23,25 @@ can check for the number of occurrences of the ransom note in the map. If at any
 drops to 0, we delete it from the map. This way we know if the character in the ransom Note no longer exists,
 we return false. At the end, we return true if it doesn;t throw any error for each of the characters.
 
+This also has a time complexity advantage as compared to map. With a map, we have to hash the entity and
+then check if the entity exists and then return the value if it exists. This takes quite a bit of time.
+However, if you use a character vector, we can easily find the value we are looking for using the index.
+While in theory both have the same runtime, in practice the character array is better
+
 Time complexity: O(m+n)
 Space complexity: O(n)
 */
 
 /*
+
+1. Using character array instead of map
+
+When we have an idea of the entities that are being stored in the map eg: only uppercase letters or only
+lowercase letters, letters of a certian language, we can use a character vector instead of a map. This makes
+sure that our space complexity is constant instead of changing with the input.
+
+Time complexity: O(m+n)
+Space complexity: O(1)
 */
 
 // My Approaches(1)
@@ -59,6 +73,37 @@ public:
         unordered_map<char, int> map;
         constructMap(magazine, map);
         return checkRansomFromMap(ransomNote, map);
+    }
+    
+    bool canConstruct(string ransomNote, string magazine) {
+        if(magazine.empty() && ransomNote.empty())
+            return true;
+        else
+            return checkValidRansom(ransomNote, magazine);
+    }
+};
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    void generateCharacterMap(vector<int> & charMap, string magazine){
+        for(int i = 0; i < magazine.size(); i++)
+            ++charMap[magazine[i] - 'a'];
+    }
+    
+    bool checkRansom(vector<int> & charMap, string ransomNote){
+        for(int i = 0; i < ransomNote.size(); i++){
+            if(--charMap[ransomNote[i] - 'a'] < 0)
+                return false;
+        }
+        return true;
+    }
+    
+    bool checkValidRansom(string ransomNote, string magazine){
+        vector<int> charMap(26, 0);
+        generateCharacterMap(charMap, magazine);
+        return checkRansom(charMap, ransomNote);
     }
     
     bool canConstruct(string ransomNote, string magazine) {
