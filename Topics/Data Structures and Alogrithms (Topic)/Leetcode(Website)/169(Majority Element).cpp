af34c226d7a35fcd6ac1f9cc47896a530c092971
Akshay199456 <akshay.kum94@gmail.com>
2020-05-06 15:50:09 -0500

    Added other approaches(2) pb 169 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/169(Majority Element).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/169(Majority Element).cpp
@@ -46,6 +46,23 @@ of where the majorith element exists, it will always be in the middle of the arr
 
 Time complexity:O(nlogn)
 Space complexity: O(1)
+
+
+2. Divide and Conquer
+
+We can use the divide and conquer approach to solve this problem. For the divide part, we keep dividing the
+left and right subarray till we hit a single element. 
+
+Once we hit a single element we can then compare the results we obtained from left and right. If they are 
+the same, we know that the majority element will be the left/right from beg to end so we can return it. 
+However, if they are not the same, we make the check in the array from beg to end to see which element 
+occurred the max number of times and return that. This becomes the conquer approach.
+
+Since we are dividing recursively and for each element running O(n) search on it, our time complexity is
+O(nlogn) while our space complexity is O(log n) as the recusive call proceeds to O(log n) in depth.
+
+Time complexity: O(nlogn)
+Space complexity: O(logn) -> recursive call structure
 */
 
 
@@ -80,5 +97,40 @@ public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         return nums[nums.size() / 2];     
+    }
+};
+
+
+// Other Approaches(2)
+class Solution {
+public:
+    int getCount(vector<int> nums, int element, int beg, int end){
+        int count = 0;
+        for(int i = beg; i <= end; i++){
+            if(nums[i] == element)
+                ++count;
+        }
+        return count;
+    }
+    
+    int getMajorityElement(vector<int> nums, int beg, int end){
+        if(beg == end)
+            return nums[beg];
+        
+        int mid = (end - beg) / 2 + beg;
+        int left = getMajorityElement(nums, beg, mid);
+        int right = getMajorityElement(nums, mid+1, end);
+        
+        if(left == right)
+            return left;
+        else{
+            int leftCount = getCount(nums, left, beg, end);
+            int rightCount = getCount(nums, right, beg, end);
+            return leftCount > rightCount ? left : right;
+        }
+    }
+    
+    int majorityElement(vector<int>& nums) {
+        return getMajorityElement(nums, 0, nums.size() - 1);
     }
 };
