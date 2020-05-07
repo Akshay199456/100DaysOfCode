cccf058d43642a7646273bce0953e94054d6f149
Akshay199456 <akshay.kum94@gmail.com>
2020-05-06 23:17:52 -0500

    Completed chapter 11 java

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 11/Chapter11.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 11/Chapter11.java
@@ -1,16 +1,65 @@
+import java.util.ArrayList;
 
-// Testing method overriding and method overloading 
 public class Chapter11 {
     public static void main(String [] args){
         System.out.println("Hello World!");
-        GeometricObject geo = new GeometricObject("Geo", 23);
-        System.out.println(geo);
 
-        Circle cir =  new Circle("Cir", 36);
-        System.out.println(cir);
+        // Implementing stack using ArrayList
+        Stack object = new Stack();
+        object.push(2);
+        object.push(3);
+        int poppedElement = object.pop();
+        object.pop();
+        int topElement = object.peek();
+        System.out.println("Popped element: " + poppedElement);
+        System.out.println("Peeked element: " + topElement);
+        System.out.println(object.isEmpty());
+        System.out.println(object.stack);
+        /*
+        // Typecasting: Includign downcasting and upcasting
+        // Testing method overriding and method overloading 
+
+        Object gen1 = new GeometricObject("Geo", 123);
+        Object gen2 = new Circle("Cir", 36);
+        System.out.println((GeometricObject)gen1);
+        System.out.println((Circle)gen2);
+        */
     }
 }
 
+class Stack{
+    ArrayList<Integer> stack;
+
+    public Stack(){
+        this.stack = new ArrayList<>();
+    }
+
+    public void push(int element){
+        this.stack.add(element);
+    }
+
+    public Integer pop(){
+        if(this.stack.isEmpty()){
+            return -1;
+        }
+        return this.stack.remove(this.stack.size() - 1);
+    }
+
+    public Integer peek(){
+        if(this.stack.isEmpty()){
+            return -1;
+        }
+        return this.stack.get(this.stack.size() - 1);
+    }
+
+    public boolean isEmpty(){
+        return this.stack.isEmpty();
+    }
+}
+
+
+
+/*
 class GeometricObject{
     public String name;
     public int result;
@@ -40,3 +89,4 @@ class Circle extends GeometricObject{
         return ("Name in Circle: " + this.name +" Value in Cicle: " + this.result);
     }
 }
+*/
