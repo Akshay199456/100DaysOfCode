4dc8a8a631988383b51f345b97fce9c1b65aa206
Akshay199456 <akshay.kum94@gmail.com>
2020-05-09 23:51:14 -0500

    Working on chapter 13 java

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 13/Solution.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 13/Solution.java
@@ -1,5 +1,42 @@
+
+// Testing out abstract methods and classes
+abstract class GeometricObject{
+    public String toString(){
+        return("This is a geometric object");
+    }
+
+    public abstract double getArea();
+    public abstract double getVolume();
+}
+
+
+class Circle extends GeometricObject{
+    private double radius;
+
+    public Circle(){
+        this.radius = 0;
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public double getVolume(){
+        return (Math.pow(this.radius, 3) * Math.PI * 4) / 3;
+    }
+}
+
+
 public class Solution {
+    
     public static void main(String [] args){
         System.out.println("Hello World!");
+        GeometricObject obj = new Circle(1);
+        System.out.println("Area of circle: " + obj.getArea());
+        System.out.println("Volume of circle: " + obj.getVolume());
     }
 }
