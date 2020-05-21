ba04812ef2a8660c51e9f0797d0ac76c36eba640
Akshay199456 <akshay.kum94@gmail.com>
2020-05-21 18:25:42 -0500

    Working on expression solver java chapter 20

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 20/Solution.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 20/Solution.java
@@ -1,15 +1,54 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 // Working on Chapter 20
 public class Solution {
     public static void main(String [] args){
-
+        /*
         // Testing binary search
         List<Integer> list = Arrays.asList(1,2,3,4,5,6);
         System.out.println(Collections.binarySearch(list, 0));
         System.out.println("Hello world!");
+
+        // Queue interface
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+        while(!queue.isEmpty()){
+            System.out.println("Element: " + queue.poll());
+        }
+        */
+
+        Stack<Double> numberStack = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+
+        String expression = "(51+(54*(3+2)))";
+        generateStacks(numberStack, operatorStack, expression);
+        System.out.println("Number stack: " + Arrays.toString(numberStack.toArray()));
+        System.out.println("Operator stack: " + Arrays.toString(operatorStack.toArray()));
+    }
+
+
+    public static void generateStacks(Stack<Double> numberStack, Stack<Character> operatorStack, String expression){
+        StringBuffer buffer = new StringBuffer();
+        for(int i = 0; i < expression.length(); i++){
+            char currentChar = expression.charAt(i);
+            if(Character.isDigit(currentChar))
+                buffer.append(Character.toString(currentChar));
+            else{
+                if(buffer.length() > 0)
+                    numberStack.add(Double.parseDouble(buffer.toString()));
+                operatorStack.add(currentChar);
+                buffer.delete(0, buffer.length());
+            }
+        }
     }
 }
