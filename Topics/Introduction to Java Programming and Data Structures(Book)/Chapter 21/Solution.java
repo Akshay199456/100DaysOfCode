2d593e0c317b91e1a254e9fbee9efa7c4ddaf262
Akshay199456 <akshay.kum94@gmail.com>
2020-05-22 23:41:52 -0500

    Completed chapter 21 java

--- Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 21/Solution.java
+++ Topics/Introduction to Java Programming and Data Structures(Book)/Chapter 21/Solution.java
@@ -1,6 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 // Working on Chapter 21
 public class Solution {
     public static void main(String [] args){
-        System.out.println("Hello World!");
+        String element = "This is this america";
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        generateMap(element.toLowerCase(), map);
+        printMap(map);
+    }
+
+	private static void generateMap(String element, Map<String, Integer> map) {
+        String [] wordTokens = element.split(" ");
+        for(String token: wordTokens){
+            if(!map.containsKey(token))
+                map.put(token, 1);
+            else
+                map.put(token, map.get(token) + 1);
+        }
+    }
+    
+    private static void printMap(Map<String, Integer> map){
+        for(Map.Entry<String, Integer> iterator: map.entrySet()){
+            System.out.println("Key: " + iterator.getKey() + " Value: " + iterator.getValue());
+        }
     }
 }
