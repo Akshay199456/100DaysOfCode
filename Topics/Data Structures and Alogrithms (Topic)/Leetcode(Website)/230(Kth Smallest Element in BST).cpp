82221c9e3f48f775989fd746e68b3b08251b06f6
Akshay199456 <akshay.kum94@gmail.com>
2020-05-24 12:04:39 -0500

    Added my approaches(2) pb 230 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/230(Kth Smallest Element in BST).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/230(Kth Smallest Element in BST).cpp
@@ -45,6 +45,23 @@ that information to return the element at k-1 index which will satisfy the ocndi
 
 Time complexity: O(n)
 Space complexity: O(n)
+
+
+2. Using minHeap
+
+We can also use a minHeap to return the Kth smallest element. Since the binary heap is self-managing, we
+can delete the k-2 elements to get the k-1th index element we are looking for.
+
+Constructing a heap takes O(n) time. However, deleting the elements from a heap takes O(log n) time. As a 
+result, this approach is more useful when continous insertion and deletion takes place in the BST. Under
+these conditions, My Approaches(1) would have to continously create the inorder array again and again
+every time a modificaiton is made. This can take up O(m * n) time complexity where m is the number of 
+modifications. On the otehr hand, My Approaches(2) would only take up O(m * log n) since any modification
+for the BST means we could modify the bianry heap in place by just adding and deleting elements to it
+without having to create the binary search tree again.
+
+Time complexity: O(k * log n)
+Space complexity: O(n)
 */
 
 /*
@@ -80,5 +97,41 @@ public:
         createInorder(root, inorder);
         return inorder[k-1];
         
+    }
+};
+
+
+// My Approaches(2)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fillPriorityQueue(priority_queue<int, vector<int>, greater<int>> & pq, TreeNode * root){
+        if(root){
+            fillPriorityQueue(pq, root->left);
+            pq.push(root->val);
+            fillPriorityQueue(pq, root->right);
+        }
+    }
+    
+    int getKthSmallest(priority_queue<int, vector<int>, greater<int>> pq, int k){
+        for(int i = 0 ; i < k - 1; i++)
+            pq.pop();
+        return pq.top();
+    }
+    
+    int kthSmallest(TreeNode* root, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        fillPriorityQueue(pq, root);
+        return getKthSmallest(pq, k);
     }
 };
