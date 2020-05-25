d3cc3e7ca86d2763ef85d3cf5310d42c710bb4df
Akshay199456 <akshay.kum94@gmail.com>
2020-05-25 13:46:55 -0500

    added my approaches(2); completed pb 993 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/993(Cousins in Binary Tree).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/993(Cousins in Binary Tree).cpp
@@ -38,7 +38,7 @@ Each node has a unique integer value from 1 to 100.
 /*
 -------------------------My Approaches:
 
-1. Traversing through the tree
+1. Traversing through the tree in a DFS approach
 
 We can traverse through the tree and when we hit the values set pair1 and pair2 resepectively. This
 way by the time we have iterated through the tree, we would have filled both  pair1 and pair2. The only thing
@@ -46,6 +46,19 @@ left is to compare the levels and the parents at the end.
 
 Time complexity: O(n)
 Space complexity: O(H) h->height of tree
+
+2. BFS approach
+
+We can also traverse the tree in a BFS approach. With the BFS approach, we traverse the tree in a level by
+level manner. At the end of the lvel, we check if we have encountered any of the values x and y. If we 
+have only encountered one, then we can return false since both the nodes will not be on the same level.
+If we encounter both, we check the parents of those nodes  to see if they are differnet and depending on
+that either return true or false. Else, we continue till we hit one of those conditions or the queue is
+empty.
+
+Time complexity: O(n)
+Space complexity: O(l); l->no of nodes in a level
+
 */
 
 /*
@@ -90,6 +103,74 @@ public:
         
         if(pair1.first == pair2.first && pair1.second != pair2.second)
             return true;
+        return false;
+    }
+};
+
+
+// My Approaches(2)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        queue<pair<TreeNode *, TreeNode *>> elementQueue;
+        TreeNode * node1, * node2, * parent1, * parent2;
+        node1 = node2 = parent1 = parent2 = NULL;
+        int nextLevelChildren = 0;
+        
+        elementQueue.push(make_pair(root, node1));
+        ++nextLevelChildren;
+        
+        while(!elementQueue.empty()){
+            
+            int currentLevelChildren = nextLevelChildren;
+            nextLevelChildren = 0;
+            
+            while(currentLevelChildren-- > 0){
+                pair<TreeNode *, TreeNode *> nodePair = elementQueue.front();
+                elementQueue.pop();
+                
+                if(nodePair.first->left){
+                    elementQueue.push(make_pair(nodePair.first->left, nodePair.first));
+                    nextLevelChildren++;
+                }
+                if(nodePair.first->right){
+                    elementQueue.push(make_pair(nodePair.first->right, nodePair.first));
+                    nextLevelChildren++;
+                }
+                
+                if(nodePair.first->val == x || nodePair.first->val == y){
+                    if(!node1){
+                        node1 = nodePair.first;
+                        parent1 = nodePair.second;
+                    }
+                    else{
+                        node2 = nodePair.first;
+                        parent2 = nodePair.second;
+                    }
+                }
+            }
+            
+            if(node1 && node2){
+                if(parent1 != parent2)
+                    return true;
+                else
+                    return false;
+            } 
+            
+            else if((!node1 && node2) || (!node2 && node1))
+                return false;
+        }
         return false;
     }
 };
