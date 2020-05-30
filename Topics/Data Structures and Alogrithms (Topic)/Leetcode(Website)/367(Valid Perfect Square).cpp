28cf3b07d96b44b18b70f6d0b18d2ee5f0096673
Akshay199456 <akshay.kum94@gmail.com>
2020-05-30 10:38:44 -0500

    Added other approaches (1) pb 367 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/367(Valid Perfect Square).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/367(Valid Perfect Square).cpp
@@ -46,6 +46,21 @@ Space complexity: O(!)
 /*
 -------------------------Other approaches
 
+1. Property of perfect square numbers
+
+One of the property of perfect square numbers is that they are composed from the sum of odd numbers
+Eg:
+1 = 1
+4 = 1 + 3
+9 = 1 + 3 + 5
+16 = 1 + 3 + 5 + 7
+....
+
+We can use this property to test if a number is a perfect square
+
+Time complexity: O(sqrt n)
+Space complexity: O(1)
+
 */
 
 // My Approaches(1)
@@ -67,6 +82,22 @@ public:
     bool isPerfectSquare(int num) {
         for(int i = 1; i <= num / i; i++){
             if(i == (num / (double)i))
+                return true;
+        }
+        return false;
+    }
+};
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int valueCheck = 1;
+        while(num > 0){
+            num-= valueCheck;
+            valueCheck+=2;
+            if(!num)
                 return true;
         }
         return false;
