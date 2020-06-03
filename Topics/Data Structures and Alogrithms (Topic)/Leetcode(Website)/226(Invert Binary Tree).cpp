d1fdafa2d77a4f9afea0361ada4d397495159496
Akshay199456 <akshay.kum94@gmail.com>
2020-06-02 21:05:43 -0500

    added my approaches(1) pb 226 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/226(Invert Binary Tree).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/226(Invert Binary Tree).cpp
@@ -1,10 +1,40 @@
 /*
+-------------------------Question:
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
 */
 
 /*
+-------------------------My Approaches:
+1. DFS
+
+We can implement DFS as we solve this problem. The key in this problem is to interchange the left and
+right side of each node. As long as we can do that for every node in the tree, we can invert the tree.
+
+Time complexity: O(n)
+Space complexity: O(h)
 */
 
 /*
+-------------------------Other approaches
+
 */
 
 
