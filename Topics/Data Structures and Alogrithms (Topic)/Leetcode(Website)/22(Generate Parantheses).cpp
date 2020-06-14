2019eefb49fcc28a4f74b3e769102d2c465781fd
Akshay Mysoe <akshay.kum94@gmail.com>
2020-06-14 12:12:23 -0500

    completed pb 22 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/22(Generate Parantheses).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/22(Generate Parantheses).cpp
@@ -43,6 +43,35 @@ open brackets
 
 Time complexity: O(n*2^n)
 Space complexity: O(n*2^n)
+
+
+2. Dynamic programming with backtracking
+
+If we analyze the problem, we can understand that this problem is an instance of the dynamic
+programming concept and hence you can build the solution for the curent situation based on the
+previous situations. 
+
+There are 2 basic conditions which dictate all the other situations that depend on it:
+
+f(0) = ""
+f(1) = "()"
+
+Using these 2 conditions, can deduce what f(2) is:
+
+f(2) = ["()()", "(())"]
+
+If we analyze f(2) carefully, the solutions for it can be represented as:
+f(2) = [{f(0)}f(1), {f(1)}f(0)]
+where the {} represents the "(" and ")" respectively
+
+Using this, we can extend it to any other layer as 
+f(n) = {f(0)}f(n-1) + {f(1)}f(n-2) + {f(2)}f(n-3) + ........ + {f(n-1)}f(0)
+     = Sigma ( {f(i)}f(n-i-1))
+
+We can therefore use this information to come up with an algorithmic solution
+
+Time complexity: O(4^n/sqrt(n))
+Space complexity: O(4^n/sqrt(n))
 */
 
 // My Approaches(1)
@@ -67,5 +96,35 @@ public:
         else if(!n)
             result.push_back("");
         return result;
+    }
+};
+
+
+// Other Approaches(2)
+class Solution {
+public:
+    vector<vector<string>> result;
+    
+    void generateSolution(int n){
+        for(int i = 1; i <= n; i++){
+            vector<string> list;
+            for(int j = 0; j < i; j++){
+                for(string firstElement: result[j])
+                    for(string secondElement: result[i - j - 1])
+                        list.push_back("(" + firstElement + ")" + secondElement);
+            }
+            result.push_back(list);
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> list;
+        if( n < 0)
+            return list;
+        
+        list.push_back("");
+        result.push_back(list);
+        generateSolution(n);
+        return result[n];
     }
 };
