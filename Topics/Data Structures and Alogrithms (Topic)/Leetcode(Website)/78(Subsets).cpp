d9b0368d6046c9da76d86bed8c4a34eef40dac19
Akshay Mysoe <akshay.kum94@gmail.com>
2020-06-18 20:42:23 -0500

    Added my approaches(1) pb 78 leetcode

---
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/78(Subsets).cpp
@@ -0,0 +1,65 @@
+/*
+-------------------------Question:
+
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+*/
+
+/*
+-------------------------My Approaches:
+
+1. Dynamic programming
+
+We can build up the solution for the current step using the solutions from the previous step. To each of the solutions from
+the previous step, we make a copy of it and add the new element to those solutions
+
+Time complexity: O(n!)
+Space complexity: O(n!)
+*/
+
+/*
+-------------------------Other approaches
+
+*/
+
+// My Approaches(1)
+class Solution {
+public:
+    vector<vector<int>> result;
+    
+    void generateSubsets(vector<int> nums){
+        for(int i = 0; i < nums.size(); i++){
+            int size = result.size();
+            for(int j = 0; j < size; j++){
+                vector<int> listCopy = result[j];
+                listCopy.push_back(nums[i]);
+                result.push_back(listCopy);
+            }
+        }
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums){
+        vector<int> list;
+        result.push_back(list);
+        if(nums.size())
+            generateSubsets(nums);
+        return result;
+    }
+};
