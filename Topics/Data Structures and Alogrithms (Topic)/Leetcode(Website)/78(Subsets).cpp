d7c29f3aab5c9d776b562b2234effd3d0ef0c6b8
Akshay Mysoe <akshay.kum94@gmail.com>
2020-06-20 15:04:47 -0500

    Added other approaches(1); added helping images pb 78 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/78(Subsets).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/78(Subsets).cpp
@@ -25,7 +25,7 @@ Output:
 /*
 -------------------------My Approaches:
 
-1. Dynamic programming
+1. Dynamic programming using solutions from the previous step for the current step without length restrictions
 
 We can build up the solution for the current step using the solutions from the previous step. To each of the solutions from
 the previous step, we make a copy of it and add the new element to those solutions
@@ -34,7 +34,7 @@ Time complexity: O(n*2^n)
 Space complexity: O(n*2^n)
 
 
-2. Dynamic programming recursive approach
+2. Dynamic programming recursive approach of My Approaches(1)
 
 We can implement the previous approach in a recursive manner as well. As long as the currIndex is less than the length of
 the array, we can keep copying elements and inserting them into the result.
@@ -45,6 +45,14 @@ Space complexity: O(n*2^n)
 
 /*
 -------------------------Other approaches
+
+1. Dynamic programming using only solutions from the previous step that are 1 length away from the current step
+
+We can implement another approach of dynamic programming but this time we will loop over the length of the combination
+rather than the candidate number itself and generate all combinations for a given length
+
+Time complexity: O(n*2^n)
+Space complexity: O(n*2^n)
 
 */
 
@@ -95,6 +103,57 @@ public:
         result.push_back(list);
         if(nums.size())
             generateSubsets(nums, 0);
+        return result;
+    }
+};
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    vector<vector<int>> result;
+    unordered_map<int,int> elementMap;
+    
+    void generateSubsets(vector<int> nums, vector<vector<int>> levelCopy, int level){
+        if(level < nums.size()){
+            vector<vector<int>> nextLevel;
+            if(level == 0){
+                for(int i = 0; i < nums.size(); i++){
+                    vector<int> list;
+                    list.push_back(nums[i]);
+                    result.push_back(list);
+                    nextLevel.push_back(list);
+                }
+            }
+            
+            else{
+                for(int i = 0 ; i < levelCopy.size(); i++){
+                    vector<int> list = levelCopy[i];
+                    int nextIndex = elementMap[list[list.size() - 1]] + 1;
+                    for(int j = nextIndex; j < nums.size(); j++){
+                        list = levelCopy[i];
+                        list.push_back(nums[j]);
+                        result.push_back(list);
+                        nextLevel.push_back(list);
+                    }
+                }
+            }
+            generateSubsets(nums, nextLevel, level + 1);
+        }
+    }
+    
+    void createMap(vector<int> nums){
+        for(int i = 0 ; i < nums.size(); i++)
+            elementMap[nums[i]] = i;
+    }
+        
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> list;
+        result.push_back(list);
+        if(nums.size()){
+            createMap(nums);
+            generateSubsets(nums, result, 0);
+        }
         return result;
     }
 };
