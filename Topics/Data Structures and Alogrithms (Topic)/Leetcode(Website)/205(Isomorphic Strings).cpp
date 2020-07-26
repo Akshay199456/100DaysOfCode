6146ff1fb5ea354be25d26fc48debad7b093f68b
Akshay Mysoe <akshay.kum94@gmail.com>
2020-07-26 16:08:17 -0500

    Added other approaches(1) -2 pb 205 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/205(Isomorphic Strings).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/205(Isomorphic Strings).cpp
@@ -34,6 +34,8 @@ This problem can be solved efficiently using a map and a set to store the occurr
 To get more information about this approach, check out the corresponding images with respect to this problem in 'Helping
 Images'.
 
+Time and space complexity are both of O(n) since the both strings are assumed to be of the same length.
+
 Time complexity: O(n)
 Space complexity: O(n)
 
@@ -42,6 +44,19 @@ Space complexity: O(n)
 /*
 -------------------------Other approaches
 
+1. Using an array instead of a map.
+
+If we know the kind of characters we will be dealing with, we can use an array instead of a map. That way we can optimize
+on space complexity. In this problem, we know that all characters are possible and since there are 128 characters with ASCII
+characters that's the size I have chosen. We initialize initially the values to -1. After going through each character in
+both the strings, we make sure to correspond both the characters in the strings to the same value since they are mapped
+to each other. If they don't match in value, it means that they have been mapped to differnt characters and so we return 
+false. If we get rhough the whole loop, we return true.
+
+Our space complexity here is O(1) since the array is of a constant fixed size and doesn't change with input.
+
+Time complexity: O(n)
+Space complexity: O(1)
 */
 
 
@@ -64,6 +79,29 @@ public:
                 sMap[s[i]] = t[i];
                 tSet.emplace(t[i]);
             }
+        }
+        return true;
+    }
+    
+    bool isIsomorphic(string s, string t) {
+        if(!s.size())
+            return true;
+        else
+            return checkIsomorphic(s, t);
+    }
+};
+
+
+// Other approaches(1)
+class Solution {
+public:
+    bool checkIsomorphic(string s, string t){
+        vector<int> sArray (128, -1), tArray(128, -1);
+        for(int i=0; i<s.size(); i++){
+            if(sArray[s[i]] != tArray[t[i]])
+                return false;
+            sArray[s[i]] = i;
+            tArray[t[i]] = i;
         }
         return true;
     }
