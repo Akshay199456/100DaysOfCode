a7cc9adefca8a1fd9fe3912c7a8febc48242804a
Akshay Mysoe <akshay.kum94@gmail.com>
2020-07-27 22:20:53 -0500

    Added other approaches (1) and 2 pb 128 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/128(Longest Consecutive Subsequence).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/128(Longest Consecutive Subsequence).cpp
@@ -30,6 +30,25 @@ Spacec complexity: O(1)
 /*
 -------------------------Other approaches
 
+1. Brute Force approach[Not coded]
+
+With the brute force approach, with each number, we try to find the largest subsequence we can encounter with each number
+and do that for each number in the array. We keep track of the largest subsequence we have encountered as we progress.
+Check in 'Helping Images' for more information on this approach.
+
+Time complexity: O(n^3)
+Space complexity: O(1)
+
+
+2. Optimization of the brute force appraoch
+
+We can optimize the brute force approach shown above by instead using a hashmap to keep track of the elements in the map.
+In addition, we only find the largest subsequence only for those elements in the array that are the beginning of the 
+subsequence instead of all the elements by checking for the previous element in the array. This way we will check all the
+elements in the array.
+
+Time complexity: O(n)
+Space complexity: O(n)
 */
 
 // My Approaches(1)
@@ -59,5 +78,47 @@ public:
             return nums.size();
         else
             return getLengthLongestConsecutive(nums);
+    }
+};
+
+
+// Other Approaches(2)
+class Solution {
+public:
+    void fillSet(vector<int> nums, unordered_set<int> & elementSet){
+        for(int i = 0; i < nums.size(); i++)
+            elementSet.emplace(nums[i]);
+    }
+    
+    bool arrayContains(vector<int> nums, unordered_set<int> elementSet, int element){
+        return !(elementSet.find(element) == elementSet.end());
+    }
+    
+    
+    
+    int getLengthLongestConsecutive(vector<int> nums, unordered_set<int> elementSet){
+        int maxLength = 1;
+        for(int i=0; i<nums.size(); i++){
+            if(elementSet.find(nums[i] - 1) == elementSet.end()){
+                int currentElement = nums[i];
+                int length = 1;
+                while(elementSet.find(currentElement + 1) != elementSet.end()){
+                    currentElement += 1;
+                    length += 1;
+                }
+                maxLength = max(length, maxLength);
+            }
+        }
+        return maxLength;
+    }
+    
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0 || nums.size() == 1)
+            return nums.size();
+        else{
+            unordered_set<int> elementSet;
+            fillSet(nums, elementSet);
+            return getLengthLongestConsecutive(nums, elementSet);
+        }
     }
 };
