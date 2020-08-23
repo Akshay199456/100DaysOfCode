c0057be74799fb25aee78fc11c2da584c16d208e
Akshay Mysoe <akshay.kum94@gmail.com>
2020-08-23 13:20:04 -0500

    Added other approaches(3) pb 567 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/567(Permutations in String).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/567(Permutations in String).cpp
@@ -53,6 +53,17 @@ not
 
 Time complexity: O(s1log(s1) + (s2-s1)(s1 + s1logs1))
 Space complexity: O(s1)
+
+
+3. Using maps/arrays to get frequency count of characters and comparing
+
+We can continue of the previous approach and iomprove it. If we notice in the previous approach, we were sorting the 
+s1 and substring of s2 of length s1 to check if they were eqal. At the end of the day, what we were doing was checking
+if the count and frequency of characters were the same. Hence, this is where we can optimize the previous approach. By using
+maps/arrays we can get frequency count of characters and use that information
+
+Time complexity: O(s1 + 26*s1*(s2-s1))
+Space complexity: O(1)
 */
 
 // Other Approaches(1)
@@ -100,6 +111,52 @@ public:
             string element = s2.substr(i, s1.size());
             sort(element.begin(), element.end());
             if(s1.compare(element) == 0)
+                return true;
+        }
+        return false;
+    }
+    
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size() < s1.size())
+            return false;
+        return findInclusion(s1, s2);
+    }
+};
+
+
+// Other Appraoches(3)
+class Solution {
+public:
+    void generateMap(string s, unordered_map<char, int> & map){
+        for(int i = 0; i < s.size(); i++){
+            if(map.find(s[i]) == map.end())
+                map[s[i]] = 1;
+            else
+                map[s[i]]++;
+        }
+    }
+    
+    bool compareMaps(unordered_map<char, int> s1Map, unordered_map<char,int> s2Map){
+        if(s1Map.size() != s2Map.size())
+            return false;
+        for(auto it1 = s1Map.begin(); it1!= s1Map.end(); it1++){
+            if(s2Map.find(it1->first) == s2Map.end())
+                return false;
+            else if(it1->second != s2Map[it1->first])
+                return false;
+        }
+        return true;
+    }
+    
+    bool findInclusion(string s1, string s2){
+        unordered_map<char, int> s1Map;
+        generateMap(s1, s1Map);
+        
+        for(int i = 0; i <= s2.size() - s1.size(); i++){
+            string element = s2.substr(i, s1.size());
+            unordered_map<char, int> s2Map;
+            generateMap(element, s2Map);
+            if(compareMaps(s1Map, s2Map))
                 return true;
         }
         return false;
