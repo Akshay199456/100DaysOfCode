40ccf966890d662cf92c9fd3aee607f96699aa91
Akshay Mysoe <akshay.kum94@gmail.com>
2020-09-14 18:48:28 -0500

    completed pb 98 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/98(Validate Binary Search Tree).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/98(Validate Binary Search Tree).cpp
@@ -32,6 +32,16 @@ Explanation: The root node's value is 5 but its right child's value is 4.
 */
 
 /*
+1. DFS
+
+This approach is very similar to Other Approaches(1) but is more concise
+
+Time complexity: O(n)
+Space complexity: O(n)
+
+*/
+
+/*
 -------------------------Other approaches
 
 1. DFS(Using a min and max for each node based on the nodes seen)[Best]
@@ -224,5 +234,39 @@ public:
         else
             return checkBST(root, empty);
         
+    }
+};
+
+
+// My Approaches(1)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkBST(TreeNode * root, long min, long max){
+        if(!root)
+            return true;
+        else{
+            //cout<<"Root: "<<root->val<<" min: "<<min<<" max: "<<max<<endl;
+            if(!(root->val > min && root->val < max))
+                return false;
+            else
+                return checkBST(root->left, min, root->val) && checkBST(root->right, root->val, max);
+        }
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        if(!root)
+            return true;
+        return checkBST(root, LONG_MIN, LONG_MAX);
     }
 };
