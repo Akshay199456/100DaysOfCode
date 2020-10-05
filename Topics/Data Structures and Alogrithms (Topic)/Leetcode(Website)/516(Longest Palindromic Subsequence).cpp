d3d7eba3903b4b1daed7855c2b0530feaf8cdefd
Akshay Mysoe <akshay.kum94@gmail.com>
2020-10-04 21:43:51 -0500

    Added my approaches(1) pb 516 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/516(Longest Palindromic Subsequence).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/516(Longest Palindromic Subsequence).cpp
@@ -29,6 +29,26 @@ s consists only of lowercase English letters.
 
 /*
 -------------------------My Approaches:
+1. Recursive Approach
+
+For palindrome based questions, the best way to find if a word is a palindrome or not is by using the two pointer approach.
+Since we are looking for subsequences here, one approach you can think of is list all set of subsequences of all possible
+lengths and then determine which one of them are palindromes. We can do this recursively which generates a tree based 
+structure. If the start and end character are the same, then we know that the length of the subsequence that will result
+from this will be atleast 2 and then we recursively check for the subsequence that results from the substring enclosed withion
+those indexes.
+If the start and end index characters don't match, then we know that we won't have any subsequences of that length. So, we
+need to check of subsequences of length-1. The way we can do that is by choosing one of two paths: either pushing beg index
+by 1 forward or by pushing the end indedx by 1 backwards. This results in a tree based structure.
+
+Time complexity: O(2^n)
+Space complexity: O(n)
+
+
+2. Bottom-up DP 
+
+Time complexity: O(n^2)
+Space complexity: O(n^2)
 
 */
 
@@ -58,5 +78,49 @@ public:
     
     int longestPalindromeSubseq(string s) {
         return getRecursive(s, 0, s.size()-1);
+    }
+};
+
+
+// My Approaches(2)
+class Solution {
+public:
+    vector<vector<int>> dpTable;
+    
+    void initializeDpTable(int size){
+        for(int i = 0; i < size; i++){
+            vector<int> list;
+            for(int j = 0; j < size; j++){
+                if(i == j)
+                    list.push_back(1);
+                else
+                    list.push_back(0);
+            }
+            dpTable.push_back(list);
+        }
+    }
+    
+    
+    void getRecursive(string s){
+        for(int i = s.size()-1; i >= 0; i--){
+            for(int j = i + 1; j < s.size(); j++){
+                // cout<<"i: "<<i<<" j: "<<j<<endl;
+                if(s[i] == s[j]){
+                    // cout<<"\ti+1: "<<i+1<<" j-1: "<<j-1<<endl;
+                    dpTable[i][j] = 2 + dpTable[i+1][j-1];
+                }
+                else{
+                    // cout<<"\ti: "<<i<<" j-1: "<<j-1<<endl;
+                    // cout<<"\ti+1: "<<i+1<<" j: "<<j<<endl;
+                    dpTable[i][j] = max(dpTable[i][j-1], dpTable[i+1][j]);
+                }
+            }
+        }
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        initializeDpTable(s.size());
+        getRecursive(s);
+        return dpTable[0][s.size()-1];
     }
 };
