73233c7478384810cc23d9ea8f19e859cd522b81
Akshay Mysoe <akshay.kum94@gmail.com>
2020-10-11 16:09:42 -0500

    Added other approaches(3) pb 32 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/32(Longest Valid Parantheses).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/32(Longest Valid Parantheses).cpp
@@ -67,6 +67,18 @@ to it on removal.
 Time complexity: O(n)
 Space complexity: O(n)
 
+
+3. Using two pointers
+
+We use balance checking specific to this problem in order to determine if we have a valid substring or not. Since we can't
+catach the longest substring from either the front or the back, we need to use a combination of the two to ctach the full
+solution. For every index, if we encounter a '(' we increament left by 1 else we increment right by 1. If left == right,
+then we have obtained a valid substring so we record its length. Else, if on the forward pass if right > left, we reset
+both to 0 since we won't get a valid substring including it. Similarly on the backward pass if left > right, then we need
+to reset left and right as we won't get a valid substring after it.
+
+Time complexity: O(n)
+Space complexity: O(1)
 */
 
 // Other Approaches(1)
@@ -148,5 +160,50 @@ public:
             indexStack.push(-1);
             return getResult(s);
         }
+    }
+};
+
+// Other Approaches(3)
+class Solution {
+public:
+    int getResult(string s){
+        // using balance checkinf for left to right
+        int left = 0, right = 0, maxLength = 0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(')
+                left++;
+            else
+                right++;
+            
+            if(right==left)
+                maxLength = max(maxLength, 2*right);
+            else if(right > left){
+                left =0;
+                right = 0;
+            }
+        }
+            
+        // using balance checkinf for right to left
+        left = 0, right = 0;
+        for(int i=s.size()-1; i>=0; i--){
+            if(s[i] == '(')
+                left++;
+            else
+                right++;
+            
+            if(right==left)
+                maxLength = max(maxLength, 2*right);
+            else if(left > right){
+                left =0;
+                right = 0;
+            }
+        }
+        return maxLength;
+    }
+    
+    int longestValidParentheses(string s) {
+        if(!s.size())
+            return 0;
+        return getResult(s);
     }
 };
