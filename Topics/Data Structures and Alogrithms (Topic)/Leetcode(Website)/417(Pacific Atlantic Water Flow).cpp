8507715914104b8af522e0cdc5fc928652064b0a
Akshay Mysoe <akshay.kum94@gmail.com>
2020-10-23 21:47:44 -0500

    added other approaches(2)

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/417(Pacific Atlantic Water Flow).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/417(Pacific Atlantic Water Flow).cpp
@@ -39,6 +39,15 @@ boolean variables. If there is a path for both, we then know that is a valid sol
 
 Time complexity: O(mn * (mn))
 Space complexity: O(max(m,n))
+
+2. Improved version of My Approaches(1)[Time limit exceeded]
+
+In this version of code, we only calculate the dfs of a path if it has not been calculated. Once we calculate the dfs of a
+path, any nodes that we exploreed as part of that dfs also have their path calculated. Thus, we don;t need to compute the 
+dfs again and again for thos nodes whos status we already know.
+
+Time complexity: O(mn * (mn))
+Space complexity: O(max(m,n))
 */
 
 /*
@@ -101,6 +110,84 @@ public:
                     bool isAtlantic = bfs(i,j,1, matrix);
                     // cout<<"isAtlantic: "<<isAtlantic<<endl;
                     if(isAtlantic && isPacific){
+                        vector<int> list{i,j};
+                        result.push_back(list);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+// My Approaches(2)
+class Solution {
+public:
+    vector<vector<int>> pacificMatrix;
+    vector<vector<int>> atlanticMatrix;
+    vector<vector<int>> result;
+    
+    void defineMatrix(int maxRows, int maxCols){
+        for(int i=0; i<maxRows; i++){
+            vector<int> list;
+            for(int j=0; j<maxCols; j++)
+                list.push_back(-1);
+            pacificMatrix.push_back(list);
+            atlanticMatrix.push_back(list);
+        }
+    }
+    
+    bool isValidNeighbor(int i, int j, int maxRows, int maxCols){
+        if(i>=0 && j>=0 && i<maxRows && j<maxCols)
+            return true;
+        return false;
+    }
+    
+    int fillMatrix(int i, int j, vector<vector<int>> matrix, int previousValue, int endI, int endJ, vector<vector<int>> & matrixGeneric){
+        if(i==endI || j==endJ || (matrixGeneric[i][j] == 1 && matrix[i][j] <= previousValue)){
+            matrixGeneric[i][j] = 1;
+            return 1;
+        }
+        else if(matrixGeneric[i][j] == 0 || (matrixGeneric[i][j] == 1 && matrix[i][j] > previousValue))
+            return 0;
+        else{
+            bool result = false;
+            int currValue = matrix[i][j];
+            matrix[i][j] = -1;
+            if(isValidNeighbor(i+1,j,matrix.size(),matrix[i].size()) && matrix[i+1][j]!=-1 && currValue >= matrix[i+1][j])
+                result = fillMatrix(i+1,j,matrix,currValue, endI, endJ, matrixGeneric);
+            if(!result && isValidNeighbor(i-1,j,matrix.size(),matrix[i].size()) && matrix[i-1][j]!=-1 && currValue >= matrix[i-1][j])
+                result = fillMatrix(i-1,j,matrix,currValue, endI, endJ, matrixGeneric);
+            if(!result && isValidNeighbor(i,j+1,matrix.size(),matrix[i].size()) && matrix[i][j+1]!=-1 && currValue >= matrix[i][j+1])
+                result = fillMatrix(i,j+1,matrix,currValue, endI, endJ, matrixGeneric);
+            if(!result && isValidNeighbor(i,j-1,matrix.size(),matrix[i].size()) && matrix[i][j-1]!=-1 && currValue >= matrix[i][j-1])
+                result = fillMatrix(i,j-1,matrix,currValue, endI, endJ, matrixGeneric);
+            
+            matrixGeneric[i][j] = result;
+            return result;
+        }
+    }
+    
+    void printMatrix(vector<vector<int>> matrix){
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[i].size(); j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) {
+        if(matrix.size()){
+            defineMatrix(matrix.size(), matrix[0].size());
+            for(int i=0; i<matrix.size(); i++){
+                for(int j=0; j<matrix[i].size(); j++){
+                    if(pacificMatrix[i][j] == -1)
+                        fillMatrix(i,j,matrix, -1,0,0, pacificMatrix);
+                    if(atlanticMatrix[i][j] == -1)
+                        fillMatrix(i,j, matrix, -1, matrix.size()-1, matrix[i].size()-1, atlanticMatrix);
+                    
+                    if(pacificMatrix[i][j] == 1 && atlanticMatrix[i][j] == 1){
                         vector<int> list{i,j};
                         result.push_back(list);
                     }
