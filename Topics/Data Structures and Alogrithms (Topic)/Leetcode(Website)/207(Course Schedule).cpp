194f1043571d8a4cf793cb555606b360ce22fa58
Akshay Mysoe <akshay.kum94@gmail.com>
2020-11-03 19:23:07 -0600

    Added my approaches(1) pb 207 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/207(Course Schedule).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/207(Course Schedule).cpp
@@ -1,11 +1,48 @@
 /*
 -------------------------Question:
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
 
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+ 
+
+Constraints:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+1 <= numCourses <= 10^5
 */
 
 /*
 -------------------------    My Approaches:
+1. Cycle Detection using BFS
 
+This problem is basically a cycle detection problem. We are testing whether another prerequisite results in a cycle and if
+so, then we know we wouldn't be able to complete it. To store the states, we use one of two values. 0 refers to a state
+where you have already visited the node while 1 refers to a state where you are still visiting the node. If we are in the
+middle of visiting a node and we come across a node which is still being visited, we know that a cycle exists, hence we 
+return true for the determination of a cycle. Also, we dont explore the nodes that we have already visited before.
+
+Time complexity: O(N^2)
+Space complexity: O(N^2)
+where N is the total number of buses.
 */
 
 /*
