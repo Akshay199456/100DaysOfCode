f4ccd4becce85da8f534f56dbcce92d196976b5e
Akshay Mysoe <akshay.kum94@gmail.com>
2020-11-03 22:01:55 -0600

    Completed pb 207 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/207(Course Schedule).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/207(Course Schedule).cpp
@@ -47,7 +47,30 @@ where N is the total number of buses.
 
 /*
 -------------------------    Other approaches:
+1. Toplogical sort with BFS
 
+We can use a topolical sort algorithm with BFS to solve this problem as well. However, in this approach, the way we construct
+the graph will be in a slightly different way. If a course is a prequisite for a certain course, we will store all the
+courses which require a particular course as the prequisite for aevery vertex in the graph. In addition, we are going to
+use an inDegreeList which will store the number of incoming edges to a particular vertex. So, if any vertex has the number
+of incoming edges as 0, it means that that course doesn't have any other courses as its prerquisite. In other words, it is
+a leaf. As a result, we trace back from the leaves to the rest of the graph and keep determining if there is a cycle along the
+way. If there is a cycle in the graph, we will eventually end up in a situation where the number of incoming edges of
+elements that are still remaining(those that don't have value -1) will have none as 0. If this is the case, we know that
+there is a cycle because of the remaining vertexes, every vertex depends on some other vertex as its prequisite and this 
+is a cycle by its definintion.
+
+
+Note: We have added an image with 'Additional notes'. This was use to prove why we would need to store the graph in a format
+where the vertexes mean other nodes are dependedn on this vertex. If we were to store it as shown in this image where vertex
+represents all the prerequisites the current vertex depends upon, when we would oncstruct the indegree list and go though
+the same process as Other Approaches(1), in order to reduce the indegree edges corresponding to the vertex we are currently
+at, we would have to go through all the remaining vertexes to see if the current vertex exists in that and if it does, reduce
+the incoming edges for that vertex. This would be highly inefficienct as compared to the way in Other approaches(1) where
+we use the current vertex and go through its neighbors to reduce the incoming edge counts for its neighbors
+
+Time complexity: O(n^2)
+Space complexity: O(n^2)
 */
 
 // My Approaches(1)
@@ -108,6 +131,53 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         createGraph(numCourses, prerequisites);
         // printGraph();
+        return finishAllCourses(numCourses);
+    }
+};
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    unordered_map<int, vector<int>> graph;
+    vector<int> inDegreeList;
+    
+    void createGraph(int numCourses, vector<vector<int>> prerequisites){
+        // initialize graph with empty lists and indegreeList
+        for(int i=0; i<numCourses; i++){
+            vector<int> list;
+            graph[i] = list;
+            inDegreeList.push_back(0);
+        }
+        
+        // initialize graph with edge relations
+        for(int i=0; i<prerequisites.size(); i++){
+            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            inDegreeList[prerequisites[i][0]]++;
+        }
+    }
+    
+    bool finishAllCourses(int numCourses){
+        for(int i=0; i<numCourses; i++){
+            int j=0;
+            for(; j < numCourses; j++){
+                if(!inDegreeList[j])
+                    break;
+            }
+            
+            if(j == numCourses)
+                return false;
+            inDegreeList[j]--;
+            
+            for(int k=0; k<graph[j].size(); k++)
+                inDegreeList[graph[j][k]]--;
+        }
+        return true;
+    }
+        
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        createGraph(numCourses, prerequisites);
         return finishAllCourses(numCourses);
     }
 };
