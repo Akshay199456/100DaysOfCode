87b001f4035fa61f964f67dbdfc041e8e51452eb
Akshay Mysoe <akshay.kum94@gmail.com>
2021-01-11 21:59:49 -0600

    Added other approaches(1) pb 435 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/435(Non-overlapping intervals).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/435(Non-overlapping intervals).cpp
@@ -31,12 +31,40 @@ Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap ea
 
 /*
 -------------------------    My Approaches:
+1. An approach towards greedy approach.
+
+We got most of this solution right, but seem to skip a few conditions mentioned in Other Approaches(1). While we decided to select the shortest interval(this helped us satisfy the (2) in Other Approaches(1) by finding the difference
+of each intervals and selecting the smallest, it made us miss a few conditions.
+
+Time complexity: O(nlogn)
+Space complexity: O(1)
 
 */
 
 /*
 -------------------------    Other approaches:
+1. Greedy Approach
 
+We can use the greedy apporach here to solve the problem. In my approach, I chose to sort the intervals by the start time but this can also be applied to the end times. Once we have the time intervals sorted by start time. there are a 
+few cases that arise. Comparing any two intervals, we have one of three choices:
+
+1. Two intervals dont intersect with each other
+
+If two intervals dont intersect with each other, it's easy. We just need to keep track using the last interval that we saw.
+
+2. Two intervals intersect with each other with the second fully contained within the first
+
+If the second interval is fully contained within the first, then we apply the greedy approach. Here, we want to select the second interval since selecting that interval allows us to slect more non-intersecting intervals int the future.
+Also, having the larger first interval meanis that we would be skipping more time intervals. However, since our goal is to minimize the amount of intervals to remove, we want to remove the larger interval.
+
+3. Two intervals intersect with each other with the second only intersecting with part of the first.
+
+Here, we want to remove th esecond interval as that ends the last. Similar to our logic in (2), we want to minmize the number of intervals to remove and this means we get rid of the interval that ends the last as it gives us the 
+opportunity to slelect the next interval and thus mroe intervals theerby removing fewer intervals.
+
+
+Time complexity: O(nlogn)
+Space complexity: O(1)
 */
 
 // My Approaches(1)[Not working]
@@ -61,6 +89,33 @@ public:
             }
         }
         
+        return minNumber;
+    }
+    
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.size() <= 1)
+            return 0;
+        return minIntervals(intervals);
+    }
+};
+
+// Other Approaches(1)
+class Solution {
+public:
+    int minIntervals(vector<vector<int>> intervals){
+        int minNumber = 0;
+        sort(intervals.begin(), intervals.end());
+        int lastEnd = intervals[0][1];
+        
+        for(int i=1; i<intervals.size(); i++){
+            if(intervals[i][0] < lastEnd){
+                minNumber++;
+                if(intervals[i][1] <= lastEnd)
+                    lastEnd = intervals[i][1];
+            }
+            else
+                lastEnd = intervals[i][1];
+        }
         return minNumber;
     }
     
