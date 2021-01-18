21f5e94077dac25917cce474b458b3ef1ffeea6e
Akshay Mysoe <akshay.kum94@gmail.com>
2021-01-18 15:51:32 -0600

    Added other approaches(1) pb 861 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/861(Score After Flipping Matrix).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/861(Score After Flipping Matrix).cpp
@@ -27,6 +27,16 @@ Toggled to [[1,1,1,1],[1,0,0,1],[1,1,1,1]].
 
 /*
 -------------------------    Other approaches:
+1. Greedy Approach
+
+Since the goal of the problem is to maximize the score, we can look at this problem as a greedy approach problem. If we want to maximize the score, we want to maximize each the value that is possible, so that the the toal score is
+maximized. If we obsserve the columns of the matrix, we notice that the first column on the left has more weight as compared to the other columns. For example, if we were to compare 0011 and 1011, the latter number is higher, because
+primarily of the 1 set on the first column in 1011. As a result, it contributes to more than half of the total of the value. As a result, our goal is to maximize the first column for each of the rows that allows us to maximize the score.
+Once we do that, for the other columns,we just want to maximize the total number of 1s we can obtain from those columns. We do this by calculating the number of 1s in that column that we have after toggling and comparing it to the
+coplement of it. Whichever is higher, that is what we want in that column across the matrix. We then multiply this with the value associated with that column and get the toal score that is the maximum
+
+Time complexity: O(mn)
+Space complexity: O(1)
 
 */
 
@@ -78,6 +88,42 @@ public:
         for(int i=0;i<A.size(); i++){
             int value = getRowValue(A[i]);
             total += (value > rowMax - value) ? value : rowMax - value;
+        }
+        
+        return total;
+    }
+};
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    void toggleRow(vector<int> & list){
+        for(int i=0; i<list.size(); i++){
+            list[i] = list[i] ^ 1;
+        }
+    }
+    
+    int matrixScore(vector<vector<int>>& A) {
+        int total = 0, stepIncrease = 1;
+        // maximize first column by converitng to all 1s in athat column.
+        for(int i=0; i < A.size(); i++){
+            if(!A[i][0]){
+                toggleRow(A[i]);
+            }
+        }
+        
+        // maximize the number of 1s from remaingin columns
+        for(int j=A[0].size()-1; j>=0; j--){
+            // get total number of 1s in a particular column
+            int oneNumber = 0;
+            for(int i=0; i<A.size(); i++){
+                if(A[i][j])
+                    oneNumber++;
+            }
+            
+            total +=  stepIncrease * max(oneNumber, (int)(A.size()) - oneNumber);
+            stepIncrease *= 2;
         }
         
         return total;
