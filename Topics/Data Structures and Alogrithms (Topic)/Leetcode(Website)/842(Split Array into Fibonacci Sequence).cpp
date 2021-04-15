c8c4f99e83c344be10d04c17080c7ea6968ca33a
Akshay Mysoe <akshay.kum94@gmail.com>
2021-04-14 19:38:59 -0500

    Added other approaches(1) pb 842 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/842(Split Array into Fibonacci Sequence).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/842(Split Array into Fibonacci Sequence).cpp
@@ -46,15 +46,24 @@ S contains only digits.
 1. Recursive, backtracking approach
 
 We can apply the recursive approach in order to solve this problem. If we notice the patterns in the problem, we see that at each step, there are one of 2 steps we can do: either start a new number from this index or extend the number we
-had for the previous number. This allows us to build the numbers as we are going along. It is one of these 2 steps that will gie a solution.
+had for the previous number. This allows us to build the numbers as we are going along. It is one of these 2 steps that will gie a solution. 
 
-Time complexdity: O(2^n)
-Space complexity: O(2^n)
+Time complexdity: O(n* 2^n)
+Space complexity: O(n)
 */
 
 /*
 -------------------------    Other approaches:
+1. Improved version of recursive approach
 
+If we observed My Approaches(1), there were two specific bottlenecks. 
+    (1) We were checking for the property of Fibonacci satisfaction at the end F(n-2) + F(n-1) == F(n) at the very end. This would build up solutions for which the condition was not all satisifed and only check it at the end.
+    (2) We were adding numbers greater than the max length of 10 digits that set up an int number. This would build really long numbers as a result even though they weren't valid.
+
+These two improvmeents were made in this approach. As a result, our solution here improved.
+
+Time complexity: O(n^2)
+Space complexity: O(n)
 */
 
 // My Approaches(1)
@@ -101,12 +110,45 @@ public:
             list.pop_back();
             
             // add this digit to previous number
-            list[list.size()-1] += s[index];
-            try{
-                if(!isSolutionFound && stoi(list[list.size() - 1]) >= 0 && stoi(list[list.size() - 1]) <= INT_MAX)
-                    findFibonacci(s, result, index+1, list, isSolutionFound);   
+            list[list.size()-1] += s[index];class Solution {
+public:
+    bool backtrack(string & s, vector<int> & result, int index){
+        if(index >= s.size()){
+            if(result.size() >= 3)
+                return true;
+        }
+        
+        int maxSize = (s[index] == '0') ? 1 : 10;
+        
+        for(int i=1; i<=maxSize && index+i <= s.size(); i++){
+            long long value = stoll(s.substr(index, i));
+            if(value > INT_MAX)
+                return false;
+            
+            int resultSize = result.size();
+            
+            if(result.size() >= 2){
+                long long sum = (long long)result[resultSize-2] + (long long) result[resultSize-1];
+                if(sum < value)
+                    return false;
             }
-            catch( const std::out_of_range &e){
+            
+            if(result.size() <= 1 || (((long long) result[resultSize-2] + (long long) result[resultSize-1]) == value)){
+                result.push_back(value);
+                if(backtrack(s, result, index+i))
+                    return true;
+                result.pop_back();
+            }
+        }
+        return false;
+    }
+    
+    vector<int> splitIntoFibonacci(string S) {
+        vector<int> result;
+        backtrack(S, result, 0);
+        return result;
+    }
+};
                 
             }
             list[list.size()-1].pop_back(); 
@@ -121,6 +163,48 @@ public:
         
         list.push_back(tempString);
         findFibonacci(S, result, 1, list, isSolutionFound);
+        return result;
+    }
+};
+
+
+// Other Approaches(1)
+class Solution {
+public:
+    bool backtrack(string & s, vector<int> & result, int index){
+        if(index >= s.size()){
+            if(result.size() >= 3)
+                return true;
+        }
+        
+        int maxSize = (s[index] == '0') ? 1 : 10;
+        
+        for(int i=1; i<=maxSize && index+i <= s.size(); i++){
+            long long value = stoll(s.substr(index, i));
+            if(value > INT_MAX)
+                return false;
+            
+            int resultSize = result.size();
+            
+            if(result.size() >= 2){
+                long long sum = (long long)result[resultSize-2] + (long long) result[resultSize-1];
+                if(sum < value)
+                    return false;
+            }
+            
+            if(result.size() <= 1 || (((long long) result[resultSize-2] + (long long) result[resultSize-1]) == value)){
+                result.push_back(value);
+                if(backtrack(s, result, index+i))
+                    return true;
+                result.pop_back();
+            }
+        }
+        return false;
+    }
+    
+    vector<int> splitIntoFibonacci(string S) {
+        vector<int> result;
+        backtrack(S, result, 0);
         return result;
     }
 };
