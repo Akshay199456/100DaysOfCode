cda307036bd49bdcee077da2faf750cb1355dc93
Akshay Mysoe <akshay.kum94@gmail.com>
2021-06-06 11:04:22 -0700

    Added my approaches(1) pb 16 leetcode

---
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/16(3Sum Closest).cpp
@@ -0,0 +1,62 @@
+/*
+-------------------------Question:
+
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ 
+
+Constraints:
+
+3 <= nums.length <= 10^3
+-10^3 <= nums[i] <= 10^3
+-10^4 <= target <= 10^4
+*/
+
+/*
+-------------------------    My Approaches:
+
+*/
+
+/*
+-------------------------    Other approaches:
+
+*/
+
+// My Approaches(1)
+class Solution {
+public:
+    int getClosest(vector<int> & nums, int target){
+        int minDistance = INT_MAX, dirn = 1;
+        for(int i = 0; i < nums.size() - 2; i++){
+            int beg = i+1, end = nums.size()-1;
+            while(beg < end){
+                int sum = nums[i] + nums[beg] + nums[end];
+                if(minDistance > abs(sum-target)){
+                    minDistance = abs(sum-target);
+                    if(sum >= target)
+                        dirn = 1;
+                    else
+                        dirn = -1;
+                }
+                
+                if(sum <= target)
+                    beg++;
+                else if( sum > target)
+                    end--;
+            }
+        }
+        return (dirn == 1) ? target + minDistance: target - minDistance;
+    }
+    
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        return getClosest(nums, target);
+    }
+};
