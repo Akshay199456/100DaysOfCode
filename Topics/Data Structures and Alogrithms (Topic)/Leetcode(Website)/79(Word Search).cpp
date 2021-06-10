59522b6514c71897438b6837417ddd7bcab648b9
Akshay Mysoe <akshay.kum94@gmail.com>
2021-06-09 23:03:46 -0500

    Added my approaches(1) pb 79 leetcode

--- Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/79(Word Search).cpp
+++ Topics/Data Structures and Alogrithms (Topic)/Leetcode(Website)/79(Word Search).cpp
@@ -32,25 +32,14 @@ board and word consists only of lowercase and uppercase English letters.
 /*
 -------------------------My Approaches:
 
-1. Backtracking using the UMPIRE method
+1. Backtracking
 
-We can use backtracking here to solve this problem. If the current character matches with the charcter in the board, we 
-continue finding path from it that only visits nodes that have not been visited before. At any stage if the character
-doesn't match, we return false instead and explore a different path as no solution can be obtained from the current path.
-If all the characters match then we have obtained a solution and we can say that it exists in the 2d array and return true. 
-Since the  only possible paths the next character can exist given the current character matches is left, right, top and bottom, 
-we explore those paths to find the match of the next character.
+We can apply the bracktracking approach to solve this problem. If we find a character that matches the current index, we can search its neighbors for the next character and so on. If at the end, we arrive at the end of word, then
+we have found the word else we have not.
 
-Time complexity: O(n^2)
-Space complexity: O(n^2)
+Time complexity: O(mnk) whre k is length of word
+Space complexity: O(max(m,n))
 
-A main reason I feel I did well on this problem is by following the UMPIRE method from codepath properly. Using it as a 
-guiding material while solving the problem helped me understand the different stages to the problem clearly without
-spending too much time on any one stage. A key aspect that I understood here is that the P - planning and I - implement
-stage and inversely proportional to each other. The better planned your approach is, the less time it takes to implement
-the code as you have planned approaches to the different scenarios that might occur. Therefore, you want to spend more time
-in planning th eproblem before actually implmenting the code. The less time you take in planning, the more time you will 
-take in implementing the code and you will fumble through the code as well.
 
 */
 
@@ -63,39 +52,24 @@ take in implementing the code and you will fumble through the code as well.
 // My Approaches(1)
 class Solution {
 public:
-    void createMatrix(vector<vector<bool>> & occurrenceMatrix, int r, int c){
-        for(int i = 0; i < r; i++){
-            vector<bool> list;
-            for(int j =0; j < c; j++)
-                list.push_back(false);
-            occurrenceMatrix.push_back(list);
-        }
-    }
-    
-    bool arrayDFS(int i, int j, string word, vector<vector<bool>> occurrenceMatrix, int curWordPos, vector<vector<char>> board){
-        if(i >= 0 && i < board.size() && j>=0 && j < board[i].size()){
-            if((word[curWordPos] == board[i][j]) && !occurrenceMatrix[i][j]){
-                occurrenceMatrix[i][j] = true;
-                if(curWordPos == word.size() - 1)
-                    return true;
-                return( arrayDFS(i,j+1, word, occurrenceMatrix, curWordPos + 1, board) || arrayDFS(i+1,j, word, occurrenceMatrix, curWordPos + 1, board) || arrayDFS(i,j-1, word, occurrenceMatrix, curWordPos + 1, board) || arrayDFS(i-1,j, word, occurrenceMatrix, curWordPos + 1, board));
-            }
-            return false;
+    bool findWord(vector<vector<char>> & board, string word, int index, int i, int j){
+        if(index == word.size())
+            return true;
+        else if(i >= 0 && i < board.size() && j>=0 && j < board[0].size() && board[i][j] == word[index]){
+            char beforeChar = board[i][j];
+            board[i][j] = '#';
+            bool result =  findWord(board, word, index+1, i-1, j) || findWord(board, word, index+1, i+1, j) || findWord(board, word, index+1, i, j-1) || findWord(board, word, index+1, i, j+1);
+            board[i][j] = beforeChar;
+            return result;
         }
         return false;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> occurrenceMatrix;
-        if(board.size() && word.size()){
-            createMatrix(occurrenceMatrix, board.size(), board[0].size());
-        
-            for(int i=0; i<board.size(); i++){
-                for(int j=0; j<board[i].size(); j++){
-                    bool status = arrayDFS(i,j, word, occurrenceMatrix, 0, board);
-                    if(status)
-                        return true;
-                }
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                if(board[i][j] == word[0] && findWord(board, word, 0, i, j))
+                    return true;
             }
         }
         return false;
