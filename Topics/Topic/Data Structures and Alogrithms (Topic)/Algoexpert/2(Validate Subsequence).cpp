e4b583851f0a2cb27c605b02de63ee5751edcd9c
Akshay Mysoe <akshay.kum94@gmail.com>
2022-01-04 21:05:38 -0800

    Reorganized content

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Algoexpert/2(Validate Subsequence).cpp
@@ -0,0 +1,24 @@
+/*
+
+/*
+-------------------------    My Approaches:
+
+1. using two pointer technique
+
+Time complexity: O(n)
+Space compleixty: O(1)
+*/
+
+/*
+-------------------------    Other Approaches 
+1.
+
+/*
+-------------------------    Video Explanation 
+
+*/
+
+
+// My approaches(1)
+
+// Other Approaches(1)
---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Algoexpert/Format.cpp
@@ -0,0 +1,21 @@
+/*
+
+/*
+-------------------------    My Approaches:
+
+1. 
+*/
+
+/*
+-------------------------    Other Approaches 
+1.
+
+/*
+-------------------------    Video Explanation 
+
+*/
+
+
+// My approaches(1)
+
+// Other Approaches(1)
