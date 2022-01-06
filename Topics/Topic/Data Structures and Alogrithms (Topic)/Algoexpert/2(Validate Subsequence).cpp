fcecbd6ea019d40d5e85b06ff5d256ae2d9d4e14
Akshay Mysoe <akshay.kum94@gmail.com>
2022-01-05 20:57:18 -0800

    added my approaches(1) pb 3 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Algoexpert/2(Validate Subsequence).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Algoexpert/2(Validate Subsequence).cpp
@@ -5,6 +5,13 @@
 
 1. using two pointer technique
 
+This approach uses the two pointer technique to solve the problem. we put one ppointer on the array and th eother on the sequence.
+we only move the pointer on the sequence move forward if there is a match with the eleement on the array.
+if no match, we move the pointer on the array forward. we continue this process ill either the pointer on the
+array or sequence has reached at the end. we then check if the pointer for the sequence has reached the end.
+if it has, we know that we saw all the element sin the sequence in the array in the intended order whcich
+validates it asa a sequence.
+
 Time complexity: O(n)
 Space compleixty: O(1)
 */
@@ -15,10 +22,36 @@ Space compleixty: O(1)
 
 /*
 -------------------------    Video Explanation 
+in subsequences, all of the numbers in the subsequence must appear in the array but they must also appear in the same order.
+have to traverse botha rrays.
+have to traverse the maina rray as well
+
+becasue the order of the elements in subsequence matters, we are lookinf for the first element and every element matters.
 
 */
 
 
 // My approaches(1)
+
+using namespace std;
+
+bool checkSubsequence(vector<int> & array, vector<int> & sequence){
+	int arrayPtr = 0, sequencePtr = 0;
+	while(arrayPtr < array.size() && sequencePtr < sequence.size()){
+		if(array[arrayPtr] == sequence[sequencePtr])
+			sequencePtr++;
+		arrayPtr++;
+	}
+	
+	if(sequencePtr == sequence.size())
+		return true;
+	return false;
+}
+
+bool isValidSubsequence(vector<int> array, vector<int> sequence) {
+  // Write your code here.
+	return checkSubsequence(array, sequence);
+}
+
 
 // Other Approaches(1)
---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Algoexpert/3(Sorted Squared Array).cpp
@@ -0,0 +1,62 @@
+/*
+
+/*
+-------------------------    My Approaches:
+
+1. Two pointer approach
+
+use the two pointer approach here in order to solve the problem. since we know that that integers cna be either
+positive or negative and we also lknow that the elements that wll contribute to the largest element will always be from the 
+ends of the array since the array will be in sorted order. so if the arrya has negative elements in the beginning
+and poisitive ekements int hee end, the competition between the two will decide whose square will be
+larger sinbce square of botha anegative and positive number are positive.
+
+Using think knowledge , we then fill an additional raays of the same size from the back of the array
+and keep filling it with larger of the two pointers. if the element with the left pointer has greater square,
+we push th left pointer forward else we push the right ptr backward. we contineue to do this till all the elements
+have been covered and the left pointer is ahead of the right pointer
+
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+
+/*
+-------------------------    Other Approaches 
+1.
+
+/*
+-------------------------    Video Explanation 
+
+*/
+
+
+// My approaches(1)
+#include <vector>
+using namespace std;
+
+vector<int> getResult(vector<int> & array){
+	vector<int> sortedArray(array.size());
+	int left = 0, right = array.size()-1, sortedPtr = array.size() - 1;
+	while(left <= right){
+		int leftValue = array[left] * array[left];
+		int rightValue = array[right] * array[right];
+		
+		if(leftValue > rightValue){
+			sortedArray[sortedPtr--] = leftValue;
+			left++;
+		}
+		else{
+			sortedArray[sortedPtr--] = rightValue;
+			right--;
+		}
+	}
+	return sortedArray;
+}
+
+vector<int> sortedSquaredArray(vector<int> array) {
+  // Write your code here.
+  return getResult(array);
+}
+
+
+// Other Approaches(1)
