6488303eb269d5c74e8461cd5dd639a1b200689e
Akshay Mysoe <akshay.kum94@gmail.com>
2022-01-19 20:56:32 -0800

    added my approaches(1) pb 2 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/2(Smallest subarray with a greater sum).py
@@ -0,0 +1,65 @@
+"""
+
+"""
+
+"""
+-------------------------    My Approaches:
+1. sliding window approach
+    Start: 8:37 pm
+    Pause: 9:27 pm
+
+    Resume: 7:56 pm
+    Pause: 7:59 pm
+
+    Resume: 8:06 pm
+    End: 8:32 pm 
+
+we can use the sliding wnow appraoch to solve this problem. as long as the sum is less than s, we add it and extend end. when the sum crosses s, we then take the minlength into consideration,
+subtract the beginning eleemnt from the total and move the begining forward. we continue this tillt eh first loop condition is met. after that, we might still have an answer
+reamining , so we run another loop until the total is greater than s which would give you an answer if an answer exissted there
+
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+"""
+
+"""
+/*
+-------------------------    Other Approaches 
+1.
+    Start: 
+    End: 
+"""
+
+
+
+#  My approaches(1)
+def get_minimum(min_length, beg, end, total, arr):
+  min_length = min(min_length, end - beg)
+  total -= arr[beg]
+  beg = beg + 1
+  return min_length, total, beg
+
+
+def get_min_length(arr, s):
+  beg, end, total, min_length = 0,0,0, len(arr) +1
+  while(beg <= end and end < len(arr)):
+    if total < s:
+      total += arr[end]
+      end = end + 1
+    else:
+      min_length, total, beg = get_minimum(min_length, beg, end, total, arr)
+  
+  while total >= s:
+    min_length, total, beg = get_minimum(min_length, beg, end, total, arr)
+
+  return 0 if min_length == len(arr)+1 else min_length
+
+
+
+def smallest_subarray_sum(s, arr):
+  # TODO: Write your code here
+  return get_min_length(arr, s)
+
+
+# Other Approaches(1)
