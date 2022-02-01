7061d6b40232b2583210db71fe7f72f45b8caf85
Akshay Mysoe <akshay.kum94@gmail.com>
2022-01-31 21:17:19 -0800

    completed pb 8 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/8(Remove Duplicates).py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/8(Remove Duplicates).py
@@ -49,7 +49,7 @@ Explanation: The first two elements after removing the duplicates will be [2, 11
 
 """
 -------------------------    Notes
-
+need to remove duolicates in place
 """
 
 
@@ -75,3 +75,24 @@ def remove_duplicates(arr):
 
 
 # Other Approaches(1)
+def remove_duplicates(arr):
+  # index of the next non-duplicate element
+  next_non_duplicate = 1
+
+  i = 1
+  while(i < len(arr)):
+    if arr[next_non_duplicate - 1] != arr[i]:
+      arr[next_non_duplicate] = arr[i]
+      next_non_duplicate += 1
+    i += 1
+
+  return next_non_duplicate
+
+
+def main():
+  print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+  print(remove_duplicates([2, 2, 2, 11]))
+
+
+main()
+
