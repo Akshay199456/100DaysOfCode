3cecdea0d4573682d63b2f9ef22448c98000f680
Akshay Mysoe <akshay.kum94@gmail.com>
2022-02-01 20:59:30 -0800

    completed pb 10 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/10(Squaring a sorted array).py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/10(Squaring a sorted array).py
@@ -1,19 +1,31 @@
 """
 Problem statement:
+Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
 
+Example 1:
+
+Input: [-2, -1, 0, 2, 3]
+Output: [0, 1, 4, 4, 9]
+Example 2:
+
+Input: [-3, -1, 0, 1, 2]
+Output: [0, 1, 1, 4, 9]
 
 """
 
 """
 -------------------------    My Approaches:
-1. 
-    Start: 
-    End: 
+1. two pointer approach
+    Start: 8:33 pm
+    End: 8:45 pm
+
+    we can use the two pointer approach here to solve the problem. since we have a sorted input and we are triung to find a condition ()find the max element,
+    we can use the two pointer approach. since the array is sorted, we know the max will only come fromt ehe ends of the array.
+    this means that we can add elements from the end of t original array into the new array depending on the squared value and move the pointers either forward or backward.
 
 
-
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(n)
+    Space complexity: O(n)
 */
 """
 
@@ -32,7 +44,14 @@ Problem statement:
 
 """
 -------------------------    Notes
+only trikck is that we can have negatgive numbers in the input array
 
+an easier approach could be to first find th eindex of first non-negative number in array. after that, can use two-pointer approach
+to iteratre the array. one pointer will mvoe forwards to iteratre non-negative numbers and other
+will move backward to iterate negative numbers. at any step whichever bigger square will be added to output arrrray
+
+alternate approach to use pointersstarting at both ends fo input array. whichever gives us the 
+bigger sqaure, we add it to result array and move to next/previous number according to pointer
 """
 
 
@@ -62,3 +81,29 @@ def make_squares(arr):
 
 
 # Other Approaches(1)
+def make_squares(arr):
+  n = len(arr)
+  squares = [0 for x in range(n)]
+  highestSquareIdx = n - 1
+  left, right = 0, n - 1
+  while left <= right:
+    leftSquare = arr[left] * arr[left]
+    rightSquare = arr[right] * arr[right]
+    if leftSquare > rightSquare:
+      squares[highestSquareIdx] = leftSquare
+      left += 1
+    else:
+      squares[highestSquareIdx] = rightSquare
+      right -= 1
+    highestSquareIdx -= 1
+
+  return squares
+
+
+def main():
+
+  print("Squares: " + str(make_squares([-2, -1, 0, 2, 3])))
+  print("Squares: " + str(make_squares([-3, -1, 0, 1, 2])))
+
+
+main()
