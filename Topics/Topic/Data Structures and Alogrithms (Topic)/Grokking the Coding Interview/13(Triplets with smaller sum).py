e028a9a05a473e1333848583207384f4f81e6c3b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-02-03 21:22:59 -0800

    completed pb 13 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/13(Triplets with smaller sum).py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/13(Triplets with smaller sum).py
@@ -51,7 +51,8 @@ Explanation: There are four triplets whose sum is less than the target:
 
 """
 -------------------------    Notes
-
+follows the two pointer pattern and shares similarities with triplet sum to zero.
+only differnece is we need to find triplets that satisfy condition
 """
 
 
@@ -78,3 +79,32 @@ def triplet_with_smaller_sum(arr, target):
 
 
 # Other Approaches(1)
+def triplet_with_smaller_sum(arr, target):
+  arr.sort()
+  count = 0
+  for i in range(len(arr)-2):
+    count += search_pair(arr, target - arr[i], i)
+  return count
+
+
+def search_pair(arr, target_sum, first):
+  count = 0
+  left, right = first + 1, len(arr) - 1
+  while (left < right):
+    if arr[left] + arr[right] < target_sum:  # found the triplet
+      # since arr[right] >= arr[left], therefore, we can replace arr[right] by any number between
+      # left and right to get a sum less than the target sum
+      count += right - left
+      left += 1
+    else:
+      right -= 1  # we need a pair with a smaller sum
+  return count
+
+
+def main():
+  print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
+  print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
+
+
+main()
+
