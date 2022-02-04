60aaa649010c719add302279a8e1f20db276d7af
Akshay Mysoe <akshay.kum94@gmail.com>
2022-02-03 19:43:46 -0800

    completed pb 12 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/12(Triplet sum close to target).py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/12(Triplet sum close to target).py
@@ -54,7 +54,10 @@ Explanation: The triplet [1, 1, 1] has the closest sum to the target.
 
 """
 -------------------------    Notes
-
+> solution follows the two pointers pattern aand is quite similar to triplet sum to zero problem.
+> similar approach to iterte through the array, taking one number at a time. at every step, we 
+will save the difference between the triplet and thr target number so in end, we cna return the triplet
+with closest sum.
 """
 
 
@@ -88,3 +91,36 @@ def triplet_sum_close_to_target(arr, target_sum):
 
 
 # Other Approaches(1)
+import math
+
+
+def triplet_sum_close_to_target(arr, target_sum):
+  arr.sort()
+  smallest_difference = math.inf
+  for i in range(len(arr)-2):
+    left = i + 1
+    right = len(arr) - 1
+    while (left < right):
+      target_diff = target_sum - arr[i] - arr[left] - arr[right]
+      if target_diff == 0:  # we've found a triplet with an exact sum
+        return target_sum  # return sum of all the numbers
+
+      # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
+      if abs(target_diff) < abs(smallest_difference) or (abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
+        smallest_difference = target_diff  # save the closest and the biggest difference
+
+      if target_diff > 0:
+        left += 1  # we need a triplet with a bigger sum
+      else:
+        right -= 1  # we need a triplet with a smaller sum
+
+  return target_sum - smallest_difference
+
+
+def main():
+  print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+  print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
+  print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
+
+
+main()
