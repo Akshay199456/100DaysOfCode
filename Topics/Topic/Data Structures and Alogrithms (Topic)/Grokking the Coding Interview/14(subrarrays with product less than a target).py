8c0e9b627e01ef3effa4006ad4af423249910f10
Akshay Mysoe <akshay.kum94@gmail.com>
2022-02-13 17:38:49 -0800

    completed pb 14 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/14(subrarrays with product less than a target).py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/14(subrarrays with product less than a target).py
@@ -55,7 +55,11 @@ Explanation: There are seven contiguous subarrays whose product is less than the
 
 """
 -------------------------    Notes
-
+problem follows sliding window and the two pointes pattern and shares similariteis with triplets
+with samller small with two differences:
+        input array is not sorted
+        instead of dinding triplets with sum less than a target, we need to find all subarrays having
+        a product less than the target
 """
 
 
@@ -93,3 +97,31 @@ def find_subarrays(arr, target):
 
 
 # Other Approaches(1)
+from collections import deque
+
+
+def find_subarrays(arr, target):
+  result = []
+  product = 1
+  left = 0
+  for right in range(len(arr)):
+    product *= arr[right]
+    while (product >= target and left < len(arr)):
+      product /= arr[left]
+      left += 1
+    # since the product of all numbers from left to right is less than the target therefore,
+    # all subarrays from left to right will have a product less than the target too; to avoid
+    # duplicates, we will start with a subarray containing only arr[right] and then extend it
+    temp_list = deque()
+    for i in range(right, left-1, -1):
+      temp_list.appendleft(arr[i])
+      result.append(list(temp_list))
+  return result
+
+
+def main():
+  print(find_subarrays([2, 5, 3, 10], 30))
+  print(find_subarrays([8, 2, 6, 5], 50))
+
+
+main()
