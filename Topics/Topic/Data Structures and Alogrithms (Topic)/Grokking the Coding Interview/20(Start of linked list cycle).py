0e2815d7b440790c2811a9ea6e12d6acdef5fbfe
Akshay Mysoe <akshay.kum94@gmail.com>
2022-02-24 21:40:28 -0800

    added my approaches(1) pb 20 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/20(Start of linked list cycle).py
@@ -0,0 +1,71 @@
+"""
+Problem statement:
+
+Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
+
+ head  
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+ Cycle start  
+ Examples:  
+ Cycle start  
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+    1   
+    2   
+    3   
+    4   
+    5   
+    6  
+"""
+
+"""
+-------------------------    My Approaches:
+1. slow and fast pointer technqiue
+    Start: 9:08 pm
+    End: 9:24 pm
+
+    we can first use the slow and fast pointer technique to find the post of intersection on
+    the cycle adn then the distance from the begging of the linked list to the start
+    of the cycle and from the intersection to the beginning of the cycle is the same. 
+    so we push the pointer till they intersect and that is the result.
+
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+"""
+
+"""
+-------------------------    Other Approaches 
+1.
+    Start: 
+    End: 
+
+    
+
+    Time complexity: O()
+    Space complexity: O()
+"""
+
+
+"""
+-------------------------    Notes
+
+"""
+
+
+
+#  My approaches(1)
+
+
+
+# Other Approaches(1)
