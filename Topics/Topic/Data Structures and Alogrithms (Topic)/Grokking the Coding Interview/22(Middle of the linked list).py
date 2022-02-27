1a0e7507837ddc9a3fe8bf2229d206c6f7cc1637
Akshay Mysoe <akshay.kum94@gmail.com>
2022-02-26 21:03:58 -0800

    Added my approaches(1) pb 22 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/22(Middle of the linked list).py
@@ -0,0 +1,63 @@
+"""
+Problem statement:
+
+Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+
+If the total number of nodes in the LinkedList is even, return the second middle node.
+
+Example 1:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> null
+Output: 3
+Example 2:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+Output: 4
+Example 3:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
+Output: 4
+"""
+
+"""
+-------------------------    My Approaches:
+1. slow and fast pointer approach
+
+    Start: 8:25 pm
+    End: 8:39 pm
+
+    we can use the slow and fast pointer approach to solve this problem. always, the fast pointer
+    is two steps ahead so whwen fast pointer is null or its next element is null, we are at the middle 
+    of hthe linked list
+
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+"""
+
+"""
+-------------------------    Other Approaches 
+1.
+    Start: 
+    End: 
+
+    
+
+    Time complexity: O()
+    Space complexity: O()
+"""
+
+
+"""
+-------------------------    Notes
+
+"""
+
+
+
+#  My approaches(1)
+
+
+
+# Other Approaches(1)
