3c72afe0d4c86f2711cb5a620d3d1499fdfe95b3
Akshay Mysoe <akshay.kum94@gmail.com>
2022-03-02 21:01:55 -0800

    completed pb 23 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/Format.py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/Format.py
@@ -30,7 +30,15 @@ Problem statement:
 
 """
 -------------------------    Notes
-
+if we didivde linked list inot two halves, the node values of the first half in the forward direction
+should be similar to the node values of the second half in the backward direction. as we
+are given a singly linked list, can't move in backward direction. to handle this:
+    1. we can use fast and slow pointers method to find the middle node of the linked l;ist
+    once we have middle of linked lis,t we will reverse second half
+    then, compare first half with reversed second half to see if the linked list represents
+    paldinrome
+    finally, we will reverse the second half of the linked lsit again to revert and brin the lnked
+    list back to its original form.
 """
 
 
@@ -109,3 +117,63 @@ main()
 
 
 # Other Approaches(1)
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+def is_palindromic_linked_list(head):
+  if head is None or head.next is None:
+    return True
+
+  # find middle of the LinkedList
+  slow, fast = head, head
+  while (fast is not None and fast.next is not None):
+    slow = slow.next
+    fast = fast.next.next
+
+  head_second_half = reverse(slow)  # reverse the second half
+  # store the head of reversed part to revert back later
+  copy_head_second_half = head_second_half
+
+  # compare the first and the second half
+  while (head is not None and head_second_half is not None):
+    if head.value != head_second_half.value:
+      break  # not a palindrome
+
+    head = head.next
+    head_second_half = head_second_half.next
+
+  reverse(copy_head_second_half)  # revert the reverse of the second half
+
+  if head is None or head_second_half is None:  # if both halves match
+    return True
+
+  return False
+
+
+def reverse(head):
+  prev = None
+  while (head is not None):
+    next = head.next
+    head.next = prev
+    prev = head
+    head = next
+  return prev
+
+
+def main():
+  head = Node(2)
+  head.next = Node(4)
+  head.next.next = Node(6)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(2)
+
+  print("Is palindrome: " + str(is_palindromic_linked_list(head)))
+
+  head.next.next.next.next.next = Node(2)
+  print("Is palindrome: " + str(is_palindromic_linked_list(head)))
+
+
+main()
