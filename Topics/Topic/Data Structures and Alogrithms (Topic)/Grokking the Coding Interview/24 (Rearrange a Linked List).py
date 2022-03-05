666145e360a3e916b07cc8825094c0b8b1355d7a
Akshay Mysoe <akshay.kum94@gmail.com>
2022-03-04 21:29:45 -0800

    Added my approaches(1) pb 24 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/24 (Rearrange a Linked List).py
@@ -0,0 +1,54 @@
+"""
+Problem statement:
+
+Given the head of a Singly LinkedList, write a method to modify the LinkedList such that the nodes from the second half of the LinkedList are inserted alternately to the nodes from the first half in reverse order. So if the LinkedList has nodes 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null, your method should return 1 -> 6 -> 2 -> 5 -> 3 -> 4 -> null.
+
+Your algorithm should not use any extra space and the input LinkedList should be modified in-place.
+
+Example 1:
+
+Input: 2 -> 4 -> 6 -> 8 -> 10 -> 12 -> null
+Output: 2 -> 12 -> 4 -> 10 -> 6 -> 8 -> null 
+Example 2:
+
+Input: 2 -> 4 -> 6 -> 8 -> 10 -> null
+Output: 2 -> 10 -> 4 -> 8 -> 6 -> null
+
+"""
+
+"""
+-------------------------    My Approaches:
+1. Fast ald slow pointer
+
+    We use the fast and slow pointer approach to find the middle of the linked list.
+    once we find the middle of the linked list, we then reverse the second half of the linked
+    list. at this point, the second half of the linked list either ahs the same number
+    of elements as the first half or one more. we make slight modifications if it is one more.
+    otherwise, we just move the pointers around.
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+"""
+
+"""
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+"""
+
+
+"""
+-------------------------    Notes
+
+"""
+
+
+
+#  My approaches(1)
+
+
+
+# Other Approaches(1)
--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/Format.py
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/Format.py
@@ -8,8 +8,8 @@ Problem statement:
 -------------------------    My Approaches:
 1. 
 
-    Time complexity: O(n)
-    Space complexity: O(1)
+    Time complexity: O()
+    Space complexity: O()
 */
 """
 
