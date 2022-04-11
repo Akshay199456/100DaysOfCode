311fac018ba12d0ef6f50e698fbec92ec44a27a6
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-10 19:35:55 -0700

    completed pb 31 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/31(Maximum CPU Load).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/31(Maximum CPU Load).cpp
@@ -50,7 +50,9 @@ Explanation: Maximum CPU load will be 8 as all jobs overlap during the time inte
 
 /*
 -------------------------    Notes
-
+follows merge intervals patterns and can eaasil be converted to minimum meeting rooms problem.
+for max cpu load, we need to find the max number of jobs runnoing at any time. will need to keep a running
+count of max cpu load at any time to find overall max load
 */
 
 
@@ -157,3 +159,67 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+class Job {
+ public:
+  int start = 0;
+  int end = 0;
+  int cpuLoad = 0;
+
+  Job(int start, int end, int cpuLoad) {
+    this->start = start;
+    this->end = end;
+    this->cpuLoad = cpuLoad;
+  }
+};
+
+class MaximumCPULoad {
+ public:
+  struct endCompare {
+    bool operator()(const Job &x, const Job &y) { return x.end > y.end; }
+  };
+
+  static int findMaxCPULoad(vector<Job> &jobs) {
+    if (jobs.empty()) {
+      return 0;
+    }
+
+    // sort the jobs by start time
+    sort(jobs.begin(), jobs.end(), [](const Job &a, const Job &b) { return a.start < b.start; });
+
+    int maxCPULoad = 0;
+    int currentCPULoad = 0;
+    priority_queue<Job, vector<Job>, endCompare> minHeap;
+    for (auto job : jobs) {
+      // remove all jobs that have ended
+      while (!minHeap.empty() && job.start > minHeap.top().end) {
+        currentCPULoad -= minHeap.top().cpuLoad;
+        minHeap.pop();
+      }
+
+      // add the current job into the minHeap
+      minHeap.push(job);
+      currentCPULoad += job.cpuLoad;
+      maxCPULoad = max(maxCPULoad, currentCPULoad);
+    }
+
+    return maxCPULoad;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<Job> input = {{1, 4, 3}, {7, 9, 6}, {2, 5, 4}};
+  cout << "Maximum CPU load at any time: " << MaximumCPULoad::findMaxCPULoad(input) << endl;
+
+  input = {{6, 7, 10}, {8, 12, 15}, {2, 4, 11}};
+  cout << "Maximum CPU load at any time: " << MaximumCPULoad::findMaxCPULoad(input) << endl;
+
+  input = {{1, 4, 2}, {3, 6, 5}, {2, 4, 1}};
+  cout << "Maximum CPU load at any time: " << MaximumCPULoad::findMaxCPULoad(input) << endl;
+}
