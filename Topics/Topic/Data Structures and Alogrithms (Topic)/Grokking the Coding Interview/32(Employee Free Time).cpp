38406acae47198931fff0d5793e05b7c116ebd1e
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-11 21:41:48 -0700

    completed pb 32 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/32(Employee Free Time).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/32(Employee Free Time).cpp
@@ -50,13 +50,41 @@ Explanation: All employees are free between [5,7].
 -------------------------    Other Approaches 
 1.
 
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(n log k ) where n is the toal no of itnervals and k is the toal no of employees.
+    this is becasue we are iterating through the intervals only once (taking O(n) time) and every time we
+    process an interval, we remove and can isnert one interval in the min heap (which will take
+    O(log K) time. at any time, heap will not have more than k elements
+ 
+    Space complexity: O(k) since heap will not have more than k elemets at a time.
 */
 
 
 /*
 -------------------------    Notes
+Follows the merge interval pattern
+
+one simple solutuion would be to put all employees working hours in list and sort them on the start
+time. then we can iteratre through the list to find the gaps. sorting th eintervals on the example
+gives us:
+[1,3], [2,4], [6,8], [9,12]
+
+we can now iterate through these intervals and whenever we find non-overlapping intervals,
+eg [2,4] and [6,8], we can calucalte a free interval (eg: [4,6]) this algorithm will
+take O(n log n) time where n is the toal nttal no of intervals. this time is needed becasue 
+we need to sort alll the iintervals. the speace complexity will be O(n) which ius needded for sorting.
+can we find a better solution?
+
+using a heap to sort the intervals
+
+one of the facts we are not utilizing is theat each employee list is indivudally sorted
+
+we can take the first interval of each employee and isnert it into a min heap. this min heap
+can always gives us the interval with the smallest start time. once we have the smallest start-time
+interval, we can compare it with the next smalelst start-time interval (again from heap) to find
+the gap.
+
+whenever we take an interval out of the min heap, we can isnert the same employee's next 
+interval. this also means that we need to know which interval belongs to which employee.
 
 */
 
@@ -177,3 +205,95 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+class Interval {
+ public:
+  int start = 0;
+  int end = 0;
+
+  Interval(int start, int end) {
+    this->start = start;
+    this->end = end;
+  }
+};
+
+class EmployeeFreeTime {
+ public:
+  struct startCompare {
+    bool operator()(const pair<Interval, pair<int, int>> &x,
+                    const pair<Interval, pair<int, int>> &y) {
+      return x.first.start > y.first.start;
+    }
+  };
+
+  static vector<Interval> findEmployeeFreeTime(const vector<vector<Interval>> &schedule) {
+    vector<Interval> result;
+    if (schedule.empty()) {
+      return result;
+    }
+
+    // PriorityQueue to store one interval from each employee
+    priority_queue<pair<Interval, pair<int, int>>, vector<pair<Interval, pair<int, int>>>,
+                   startCompare>
+        minHeap;
+
+    // insert the first interval of each employee to the queue
+    for (int i = 0; i < schedule.size(); i++) {
+      minHeap.push(make_pair(schedule[i][0], make_pair(i, 0)));
+    }
+
+    Interval previousInterval = minHeap.top().first;
+    while (!minHeap.empty()) {
+      auto queueTop = minHeap.top();
+      minHeap.pop();
+      // if previousInterval is not overlapping with the next interval, insert a free interval
+      if (previousInterval.end < queueTop.first.start) {
+        result.push_back({previousInterval.end, queueTop.first.start});
+        previousInterval = queueTop.first;
+      } else {  // overlapping intervals, update the previousInterval if needed
+        if (previousInterval.end < queueTop.first.end) {
+          previousInterval = queueTop.first;
+        }
+      }
+
+      // if there are more intervals available for the same employee, add their next interval
+      vector<Interval> employeeSchedule = schedule[queueTop.second.first];
+      if (employeeSchedule.size() > queueTop.second.second + 1) {
+        minHeap.push(make_pair(employeeSchedule[queueTop.second.second + 1],
+                               make_pair(queueTop.second.first, queueTop.second.second + 1)));
+      }
+    }
+
+    return result;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<vector<Interval>> input = {{{1, 3}, {5, 6}}, {{2, 3}, {6, 8}}};
+  vector<Interval> result = EmployeeFreeTime::findEmployeeFreeTime(input);
+  cout << "Free intervals: ";
+  for (auto interval : result) {
+    cout << "[" << interval.start << ", " << interval.end << "] ";
+  }
+  cout << endl;
+
+  input = {{{1, 3}, {9, 12}}, {{2, 4}}, {{6, 8}}};
+  result = EmployeeFreeTime::findEmployeeFreeTime(input);
+  cout << "Free intervals: ";
+  for (auto interval : result) {
+    cout << "[" << interval.start << ", " << interval.end << "] ";
+  }
+  cout << endl;
+
+  input = {{{1, 3}}, {{2, 4}}, {{3, 5}, {7, 9}}};
+  result = EmployeeFreeTime::findEmployeeFreeTime(input);
+  cout << "Free intervals: ";
+  for (auto interval : result) {
+    cout << "[" << interval.start << ", " << interval.end << "] ";
+  }
+}
