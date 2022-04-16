909e019d6baa5b0485eec555dfdf3fda976d5fcd
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-16 16:31:32 -0700

    Added m approaches(1) pb 34 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/34(Find the Missing Number).cpp
@@ -0,0 +1,49 @@
+/*
+Problem statement:
+
+We are given an array containing n distinct numbers taken from the range 0 to n. Since the array has only n numbers out of the total n+1 numbers, find the missing number.
+
+Example 1:
+
+Input: [4, 0, 3, 1]
+Output: 2
+Example 2:
+
+Input: [8, 3, 5, 2, 4, 6, 0, 1]
+Output: 7
+
+*/
+
+/*
+-------------------------    My Approaches:
+1. using the cyclic sort pattern and bit exor operation
+
+    we can use the bit exor operation to ifn dht eolution ot the problem, by exoring every number with the index, at the very end the missing 
+    number will be w=the only number that will remaing since every index will cancel off with its number.
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
