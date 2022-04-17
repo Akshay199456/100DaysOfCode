7ee5f3b020a03ae2b0e8da1e9bcc2f20bc9c477b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-17 11:21:32 -0700

    completed pb 35 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/35(Find All Missing Numbers).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/35(Find All Missing Numbers).cpp
@@ -45,7 +45,10 @@ Output: 4
 
 /*
 -------------------------    Notes
-
+follows cyclic sort pattern and shares similarities with find th emissing number with one difference. there canbe many
+duplicates.
+will follow similar approach as pb 34 grok to place numbers into correct indices. once done with cyclic sort, will iterate array
+ti find indices with missing correct numbers.
 */
 
 
@@ -91,3 +94,63 @@ class AllMissingNumbers {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class AllMissingNumbers {
+ public:
+  static vector<int> findNumbers(vector<int> &nums) {
+    int i = 0;
+    while (i < nums.size()) {
+      if (nums[i] != nums[nums[i] - 1]) {
+        swap(nums, i, nums[i] - 1);
+      } else {
+        i++;
+      }
+    }
+
+    vector<int> missingNumbers;
+    for (i = 0; i < nums.size(); i++) {
+      if (nums[i] != i + 1) {
+        missingNumbers.push_back(i + 1);
+      }
+    }
+
+    return missingNumbers;
+  }
+
+ private:
+  static void swap(vector<int> &arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<int> v1 = {2, 3, 1, 8, 2, 3, 5, 1};
+  vector<int> missing = AllMissingNumbers::findNumbers(v1);
+  cout << "Missing numbers: ";
+  for (auto num : missing) {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  v1 = {2, 4, 1, 2};
+  missing = AllMissingNumbers::findNumbers(v1);
+  cout << "Missing numbers: ";
+  for (auto num : missing) {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  v1 = {2, 3, 2, 1};
+  missing = AllMissingNumbers::findNumbers(v1);
+  cout << "Missing numbers: ";
+  for (auto num : missing) {
+    cout << num << " ";
+  }
+  cout << endl;
+}
