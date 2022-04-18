3b2fe295cb3cb141a0dc35c5260de6554e0aba4b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-17 19:35:12 -0700

    Added my approaches(1) pb 37 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/37(Find All Duplicate Numbers).cpp
@@ -0,0 +1,84 @@
+/*
+Problem statement:
+
+We are given an unsorted array containing n numbers taken from the range 1 to n. The array has some numbers appearing twice, find all these duplicate numbers using constant space.
+
+Example 1:
+
+Input: [3, 4, 4, 5, 5]
+Output: [4, 5]
+Example 2:
+
+Input: [5, 4, 7, 2, 3, 5, 3]
+Output: [3, 5]
+
+*/
+
+/*
+-------------------------    My Approaches:
+1. using cyclic sort approach
+
+    we can use the cyclic sort approach to solve this problem. we use the cyclic sort
+    approach to put the numbers in its place. while doing this, if we ever come across a number
+    that we are trying to insert into its place but its place already has a number with
+    the same value, we know we have encountered a duplicate so we add it to our results.
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+*/
+
+
+
+//  My approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class FindAllDuplicate {
+ public:
+  static void getDuplicateNumbers(vector<int> & nums, vector<int> & duplicateNumbers){
+    int start = 0;
+    while(start < nums.size()){
+      if(start + 1 == nums[start])
+        ++start;
+      else if(nums[start] == nums[nums[start]-1]){
+        duplicateNumbers.push_back(nums[start]);
+        ++start;
+      }
+      else{
+        int startIndex{start}, swappedIndex{nums[start]-1};
+        int startValue{nums[startIndex]}, swappedValue{nums[swappedIndex]};
+        nums[startIndex] = swappedValue;
+        nums[swappedIndex] = startValue;
+      }
+    }
+  }
+
+  static vector<int> findNumbers(vector<int> &nums) {
+    vector<int> duplicateNumbers;
+    // TODO: Write your code here
+    getDuplicateNumbers(nums, duplicateNumbers);
+    return duplicateNumbers;
+  }
+};
+
+
+
+
+//  Other Approaches(1)
