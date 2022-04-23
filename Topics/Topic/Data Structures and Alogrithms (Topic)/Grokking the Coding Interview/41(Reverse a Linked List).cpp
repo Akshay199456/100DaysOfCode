466eb7555905aed36c59588f971d80fb7d57d94b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-22 21:24:08 -0700

    completed pb 41 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/41(Reverse a Linked List).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/41(Reverse a Linked List).cpp
@@ -43,7 +43,13 @@ Given the head of a Singly LinkedList, reverse the LinkedList. Write a function 
 
 /*
 -------------------------    Notes
+to reverse, linked list, need to reverse one node at a time. will start with variable
+current which will initally point to the head of the linenked list and a variable previous
+which will point to the previous node that we have processed; initially previous will point to null
 
+in stepwise manner, will reverse current node by pointint it to previous before 
+moving on to th enext node. also, will updatee the previous to always point to the previoss
+node we have preocessed.
 */
 
 
@@ -104,3 +110,50 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+
+class ListNode {
+ public:
+  int value = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->value = value;
+    next = nullptr;
+  }
+};
+
+class ReverseLinkedList {
+ public:
+  static ListNode *reverse(ListNode *head) {
+    ListNode *current = head;      // current node that we will be processing
+    ListNode *previous = nullptr;  // previous node that we have processed
+    ListNode *next = nullptr;      // will be used to temporarily store the next node
+
+    while (current != nullptr) {
+      next = current->next;      // temporarily store the next node
+      current->next = previous;  // reverse the current node
+      previous = current;  // before we move to the next node, point previous to the current node
+      current = next;      // move on the next node
+    }
+    // after the loop current will be pointing to 'null' and 'previous' will be the new head
+    return previous;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListNode *head = new ListNode(2);
+  head->next = new ListNode(4);
+  head->next->next = new ListNode(6);
+  head->next->next->next = new ListNode(8);
+  head->next->next->next->next = new ListNode(10);
+
+  ListNode *result = ReverseLinkedList::reverse(head);
+  cout << "Nodes of the reversed LinkedList are: ";
+  while (result != nullptr) {
+    cout << result->value << " ";
+    result = result->next;
+  }
+}
