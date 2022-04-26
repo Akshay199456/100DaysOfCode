9c4e87530cefc033453f9f1be034c5aeafe84334
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-25 21:14:50 -0700

    completed pb 42 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/42(Reverse a sub-list).cpp
@@ -0,0 +1,48 @@
+/*
+Problem statement:
+
+Given the head of a LinkedList and two positions ‘p’ and ‘q’, reverse the LinkedList from position ‘p’ to ‘q’.
+
+*/
+
+/*
+-------------------------    My Approaches:
+1. In-place reversal of list and dummy node
+
+    We can use the in-place reveral and ummy node tcechnique to solve this problem.
+    using the dummy node as the first node helps us as even if we are reversing the whole
+    list, having the dummy node always makes it as a sub-list so it becomes easier to 
+    change head.
+
+    Dummy node is always a good option when you know that the state of the head node
+    might change based on in-place manipulation.
+
+    We then set pointers accordingly ro identify the sub beg and end as well as list beg and end
+    and then reverse the list
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
