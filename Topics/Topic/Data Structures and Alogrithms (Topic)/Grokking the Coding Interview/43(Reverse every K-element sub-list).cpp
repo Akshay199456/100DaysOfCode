b095a0b7dd88ebf850c46b7a4defad6975d9fe16
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-26 20:51:52 -0700

    completed pb 43 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/43(Reverse every K-element sub-list).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/43(Reverse every K-element sub-list).cpp
@@ -1,15 +1,21 @@
 /*
 Problem statement:
 
+Given the head of a LinkedList and a number ‘k’, reverse every ‘k’ sized sub-list starting from the head.
 
+If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
 */
 
 /*
 -------------------------    My Approaches:
-1. 
+1. in-place reversal of a linked list
 
-    Time complexity: O()
-    Space complexity: O()
+    this problem sues the same technique as pb 42 grok does. the only difference is that
+    it repeats itself continously until the very end so we must adjust the pointers 
+    accordingly to make that possible.
+
+    Time complexity: O(n)
+    Space complexity: O(1)
 */
 
 
@@ -24,7 +30,9 @@ Problem statement:
 
 /*
 -------------------------    Notes
-
+follows the in-place reversal of am linked list patterns and is quite to reverse a sub-list/..
+only difference is that we have to reverse all the sub-lists. can use the same approach starting
+with the first sub-list (i.e p=1, q=k) and keep reversing all the sublists of size 'k'.
 */
 
 
@@ -112,3 +120,78 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+
+class ListNode {
+ public:
+  int value = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->value = value;
+    next = nullptr;
+  }
+};
+
+class ReverseEveryKElements {
+ public:
+  static ListNode *reverse(ListNode *head, int k) {
+    if (k <= 1 || head == nullptr) {
+      return head;
+    }
+
+    ListNode *current = head, *previous = nullptr;
+    while (true) {
+      ListNode *lastNodeOfPreviousPart = previous;
+      // after reversing the LinkedList 'current' will become the last node of the sub-list
+      ListNode *lastNodeOfSubList = current;
+      ListNode *next = nullptr;  // will be used to temporarily store the next node
+      // reverse 'k' nodes
+      for (int i = 0; current != nullptr && i < k; i++) {
+        next = current->next;
+        current->next = previous;
+        previous = current;
+        current = next;
+      }
+
+      // connect with the previous part
+      if (lastNodeOfPreviousPart != nullptr) {
+        lastNodeOfPreviousPart->next =
+            previous;  // 'previous' is now the first node of the sub-list
+      } else {         // this means we are changing the first node (head) of the LinkedList
+        head = previous;
+      }
+
+      // connect with the next part
+      lastNodeOfSubList->next = current;
+
+      if (current == nullptr) {  // break, if we've reached the end of the LinkedList
+        break;
+      }
+      // prepare for the next sub-list
+      previous = lastNodeOfSubList;
+    }
+
+    return head;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListNode *head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(3);
+  head->next->next->next = new ListNode(4);
+  head->next->next->next->next = new ListNode(5);
+  head->next->next->next->next->next = new ListNode(6);
+  head->next->next->next->next->next->next = new ListNode(7);
+  head->next->next->next->next->next->next->next = new ListNode(8);
+
+  ListNode *result = ReverseEveryKElements::reverse(head, 3);
+  cout << "Nodes of the reversed LinkedList are: ";
+  while (result != nullptr) {
+    cout << result->value << " ";
+    result = result->next;
+  }
+}
