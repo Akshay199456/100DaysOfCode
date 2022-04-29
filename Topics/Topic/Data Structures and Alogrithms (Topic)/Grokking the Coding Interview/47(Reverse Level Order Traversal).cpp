7d849eaf78cede1663f3290dc0ac5559b18e3cce
Akshay Mysoe <akshay.kum94@gmail.com>
2022-04-29 07:03:38 -0700

    completed pb 47 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/47(Reverse Level Order Traversal).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/47(Reverse Level Order Traversal).cpp
@@ -29,7 +29,9 @@ Given a binary tree, populate an array to represent its level-by-level traversal
 
 /*
 -------------------------    Notes
-
+follows binary tree level order traversal pattrn. can follows hte same bfs approach. only 
+difference will be that instead of appending the current level at the end, we will
+append the current level at the beginning of the list
 */
 
 
@@ -99,3 +101,71 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <deque>
+#include <iostream>
+#include <queue>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class ReverseLevelOrderTraversal {
+ public:
+  static deque<vector<int>> traverse(TreeNode *root) {
+    deque<vector<int>> result = deque<vector<int>>();
+    if (root == nullptr) {
+      return result;
+    }
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+    while (!queue.empty()) {
+      int levelSize = queue.size();
+      vector<int> currentLevel;
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode *currentNode = queue.front();
+        queue.pop();
+        // add the node to the current level
+        currentLevel.push_back(currentNode->val);
+        // insert the children of current node to the queue
+        if (currentNode->left != nullptr) {
+          queue.push(currentNode->left);
+        }
+        if (currentNode->right != nullptr) {
+          queue.push(currentNode->right);
+        }
+      }
+      // append the current level at the beginning
+      result.push_front(currentLevel);
+    }
+
+    return result;
+  }
+};
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(9);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  auto result = ReverseLevelOrderTraversal::traverse(root);
+
+  cout << "Reverse level order traversal: ";
+  for (auto que : result) {
+    for (auto num : que) {
+      cout << num << " ";
+    }
+    cout << endl;
+  }
+}
