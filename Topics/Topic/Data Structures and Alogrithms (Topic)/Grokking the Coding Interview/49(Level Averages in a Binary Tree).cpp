556e6735a78e191b4123a623b30467b36c6b45f2
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-01 11:28:45 -0700

    completed pb 49 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/49(Level Averages in a Binary Tree).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/49(Level Averages in a Binary Tree).cpp
@@ -28,7 +28,9 @@ Given a binary tree, populate an array to represent the averages of all of its l
 
 /*
 -------------------------    Notes
-
+follows binary tree level order traversal problem. cna follow same bfs approach here. only didference is instead to keep track of all
+nodes in a level, we are only keeping track of running sum of all values in the nodes in each level . in the end, we append
+avg of current level to result array
 */
 
 
@@ -97,3 +99,68 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class LevelAverage {
+ public:
+  static vector<double> findLevelAverages(TreeNode *root) {
+    vector<double> result;
+    if (root == nullptr) {
+      return result;
+    }
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+    while (!queue.empty()) {
+      int levelSize = queue.size();
+      double levelSum = 0;
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode *currentNode = queue.front();
+        queue.pop();
+        // add the node's value to the running sum
+        levelSum += currentNode->val;
+        // insert the children of current node to the queue
+        if (currentNode->left != nullptr) {
+          queue.push(currentNode->left);
+        }
+        if (currentNode->right != nullptr) {
+          queue.push(currentNode->right);
+        }
+      }
+      // append the current level's average to the result array
+      result.push_back(levelSum / levelSize);
+    }
+
+    return result;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(9);
+  root->left->right = new TreeNode(2);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  vector<double> result = LevelAverage::findLevelAverages(root);
+  cout << "Level averages are: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+}
