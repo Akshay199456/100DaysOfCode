9efeb88b4c161788282f988e9ab1c47bed6e9722
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-01 17:31:13 -0700

    completed pb 50 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/50(Minimum Depth of Binary Tree).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/50(Minimum Depth of Binary Tree).cpp
@@ -29,7 +29,8 @@ Find the minimum depth of a binary tree. The minimum depth is the number of node
 
 /*
 -------------------------    Notes
-
+follows the binary tree level order traversal pattern. can follow the same approach as BFS. inly difference is that we are keeping trakc of the depth of the tree. as we ffind the first leaf ndoe
+that level will reperesent the minimum depth of the tree.
 */
 
 
@@ -101,3 +102,66 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class MinimumBinaryTreeDepth {
+ public:
+  static int findDepth(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    }
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+    int minimumTreeDepth = 0;
+    while (!queue.empty()) {
+      minimumTreeDepth++;
+      int levelSize = queue.size();
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode *currentNode = queue.front();
+        queue.pop();
+
+        // check if this is a leaf node
+        if (currentNode->left == nullptr && currentNode->right == nullptr) {
+          return minimumTreeDepth;
+        }
+
+        // insert the children of current node in the queue
+        if (currentNode->left != nullptr) {
+          queue.push(currentNode->left);
+        }
+        if (currentNode->right != nullptr) {
+          queue.push(currentNode->right);
+        }
+      }
+    }
+    return minimumTreeDepth;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  cout << "Tree Minimum Depth: " << MinimumBinaryTreeDepth::findDepth(root) << endl;
+  root->left->left = new TreeNode(9);
+  root->right->left->left = new TreeNode(11);
+  cout << "Tree Minimum Depth: " << MinimumBinaryTreeDepth::findDepth(root) << endl;
+}
