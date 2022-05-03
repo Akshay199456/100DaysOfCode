790cd634c7a62adef8a5820301fef17bbf78b945
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-02 21:26:05 -0700

    completed pb 54 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/54(Right View of Binary Tree).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/54(Right View of Binary Tree).cpp
@@ -31,7 +31,8 @@ Given a binary tree, return an array containing nodes in its right view. The rig
 
 /*
 -------------------------    Notes
-
+follows bonary tree level order traversal. can follow bfs approach. only additional 
+thing is to append the last node of each elvel to the result array
 */
 
 
@@ -99,3 +100,67 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class RightViewTree {
+ public:
+  static vector<TreeNode *> traverse(TreeNode *root) {
+    vector<TreeNode *> result;
+    if (root == nullptr) {
+      return result;
+    }
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+    while (!queue.empty()) {
+      int levelSize = queue.size();
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode *currentNode = queue.front();
+        queue.pop();
+        // if it is the last node of this level, add it to the result
+        if (i == levelSize - 1) {
+          result.push_back(currentNode);
+        }
+        // insert the children of current node in the queue
+        if (currentNode->left != nullptr) {
+          queue.push(currentNode->left);
+        }
+        if (currentNode->right != nullptr) {
+          queue.push(currentNode->right);
+        }
+      }
+    }
+
+    return result;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(9);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  root->left->left->left = new TreeNode(3);
+  vector<TreeNode *> result = RightViewTree::traverse(root);
+  for (auto node : result) {
+    cout << node->val << " ";
+  }
+}
