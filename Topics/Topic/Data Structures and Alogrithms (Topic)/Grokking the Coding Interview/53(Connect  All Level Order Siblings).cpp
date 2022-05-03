3ff4f0aa6ee9a4f0d5cf057e08c5112139586d17
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-02 21:03:49 -0700

    completed pb 53 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/53(Connect  All Level Order Siblings).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/53(Connect  All Level Order Siblings).cpp
@@ -29,7 +29,9 @@ Given a binary tree, connect each node with its level order successor. The last 
 
 /*
 -------------------------    Notes
-
+follows boinary tree level order. cam follow bfs approach. only differennce is that whoile traverseing
+we will remember (irrespective of level) the previous node to connect it with the ccirrent
+node.
 */
 
 
@@ -103,3 +105,70 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode *next;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = next = nullptr;
+  }
+
+  // tree traversal using 'next' pointer
+  virtual void printTree() {
+    TreeNode *current = this;
+    cout << "Traversal using 'next' pointer: ";
+    while (current != nullptr) {
+      cout << current->val << " ";
+      current = current->next;
+    }
+  }
+};
+
+class ConnectAllSiblings {
+ public:
+  static void connect(TreeNode *root) {
+    if (root == nullptr) {
+      return;
+    }
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+    TreeNode *currentNode = nullptr, *previousNode = nullptr;
+    while (!queue.empty()) {
+      currentNode = queue.front();
+      queue.pop();
+      if (previousNode != nullptr) {
+        previousNode->next = currentNode;
+      }
+      previousNode = currentNode;
+
+      // insert the children of current node in the queue
+      if (currentNode->left != nullptr) {
+        queue.push(currentNode->left);
+      }
+      if (currentNode->right != nullptr) {
+        queue.push(currentNode->right);
+      }
+    }
+  }
+};
+
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(9);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  ConnectAllSiblings::connect(root);
+  root->printTree();
+}
