bc2680547d816dee1555d97d2662ddab59bff6bb
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-02 19:48:37 -0700

    completed pb 51 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/51(Level Order Successor).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/51(Level Order Successor).cpp
@@ -29,7 +29,10 @@ Given a binary tree and a node, find the level order successor of the given node
 
 /*
 -------------------------    Notes
-
+follows the binary tree level order traversal pattern. can follow the same bfs approach.
+only difference willbe that we will not keep track of all the l;evels. instead we will keep
+inserting chi;d nodes the queu. as soon as we find the given node, we will return the next node
+from the queue as the level roder successor.
 */
 
 
@@ -113,3 +116,80 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class LevelOrderSuccessor {
+ public:
+  static TreeNode *findSuccessor(TreeNode *root, int key) {
+    if (root == nullptr) {
+      return nullptr;
+    }
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+    while (!queue.empty()) {
+      TreeNode *currentNode = queue.front();
+      queue.pop();
+      // insert the children of current node in the queue
+      if (currentNode->left != nullptr) {
+        queue.push(currentNode->left);
+      }
+      if (currentNode->right != nullptr) {
+        queue.push(currentNode->right);
+      }
+
+      // break if we have found the key
+      if (currentNode->val == key) {
+        break;
+      }
+    }
+
+    return queue.front();
+  }
+};
+
+int main(int argc, char *argv[]) {
+  
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(3);
+  root->left->left = new TreeNode(4);
+  root->left->right = new TreeNode(5);
+  
+  TreeNode *result = LevelOrderSuccessor::findSuccessor(root, 3);
+  if (result != nullptr) {
+    cout << result->val << " " << endl;
+  }
+  
+  root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(9);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  
+  result = LevelOrderSuccessor::findSuccessor(root, 9);
+  if (result != nullptr) {
+    cout << result->val << " " << endl;
+  }
+  
+  result = LevelOrderSuccessor::findSuccessor(root, 12);
+  if (result != nullptr) {
+    cout << result->val << " " << endl;
+  }
+}
