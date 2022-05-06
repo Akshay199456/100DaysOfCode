f74c2e98412d4d843f4ac9e427aa627e9663f44f
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-05 19:17:15 -0700

    completed pb 57 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/57(Sum of Path Numbers).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/57(Sum of Path Numbers).cpp
@@ -29,7 +29,10 @@ Given a binary tree where each node can only have a digit (0-9) value, each root
 
 /*
 -------------------------    Notes
+follows the binary tree path sum pattern/. can follow the same dfs appraoch. the additional
+thing we need to do is keep track of the number representing the current path.
 
+how to calculate the path number for a node.. 
 */
 
 
@@ -90,3 +93,53 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class SumOfPathNumbers {
+ public:
+  static int findSumOfPathNumbers(TreeNode *root) {
+    return findRootToLeafPathNumbers(root, 0);
+  }
+
+ private:
+  static int findRootToLeafPathNumbers(TreeNode *currentNode, int pathSum) {
+    if (currentNode == nullptr) {
+      return 0;
+    }
+
+    // calculate the path number of the current node
+    pathSum = 10 * pathSum + currentNode->val;
+
+    // if the current node is a leaf, return the current path sum.
+    if (currentNode->left == nullptr && currentNode->right == nullptr) {
+      return pathSum;
+    }
+    // traverse the left and the right sub-tree
+    return findRootToLeafPathNumbers(currentNode->left, pathSum) + 
+           findRootToLeafPathNumbers(currentNode->right, pathSum);
+  }
+};
+
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(0);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(1);
+  root->right->left = new TreeNode(6);
+  root->right->right = new TreeNode(5);
+  cout << "Total Sum of Path Numbers: " << SumOfPathNumbers::findSumOfPathNumbers(root) << endl;
+}
