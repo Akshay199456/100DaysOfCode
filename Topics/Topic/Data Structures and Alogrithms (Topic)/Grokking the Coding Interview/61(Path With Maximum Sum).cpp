c1d683e6ba16702c7606a42d0ec9614c2feccfc1
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-09 07:22:03 -0700

    added my approaches(1) pb 61 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/61(Path With Maximum Sum).cpp
@@ -0,0 +1,38 @@
+/*
+Problem statement:
+
+Find the path with the maximum sum in a given binary tree. Write a function that returns the maximum sum.
+
+A path can be defined as a sequence of nodes between any two nodes and doesn’t necessarily pass through the root. The path must contain at least one node.
+*/
+
+/*
+-------------------------    My Approaches:
+1. Does not work - Tree Depth First Search
+
+    Time complexity: O(n)
+    Space complexity: O(n)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
