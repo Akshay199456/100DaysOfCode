55cacda3301b2ed9be6beaaa32ac9637040e3de6
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-26 16:08:27 -0700

    completed pb 71 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/71(Minimum Difference Element).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/71(Minimum Difference Element).cpp
@@ -47,7 +47,15 @@ Output: 10
 
 /*
 -------------------------    Notes
+follows binary search pattern. since binary search hel[ps find a number in a sorted array efficiently,
+can use a mdofied version of it to find the number that has the minimum difference with the given
+key.
 
+can use similar approach to order agnsotic bianry search problem. will try to search for the 
+'key' in given array. if we find the 'key' we willr eturn it as the minimum difference number.
+if we can't ifoint he key(at the end of the loop), we can find the differences between the key
+and the nunbers pointed by indices start and end, as these two numbers will be closest to the 'key'.
+the number that gives minimum difference will be our required number.
 */
 
 
@@ -101,3 +109,46 @@ int main(int argc, char* argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class MinimumDifference {
+ public:
+  static int searchMinDiffElement(const vector<int>& arr, int key) {
+    if (key < arr[0]) {
+      return arr[0];
+    }
+    if (key > arr[arr.size() - 1]) {
+      return arr[arr.size() - 1];
+    }
+
+    int start = 0, end = arr.size() - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (key < arr[mid]) {
+        end = mid - 1;
+      } else if (key > arr[mid]) {
+        start = mid + 1;
+      } else {
+        return arr[mid];
+      }
+    }
+
+    // at the end of the while loop, 'start == end+1'
+    // we are not able to find the element in the given array
+    // return the element which is closest to the 'key'
+    if ((arr[start] - key) < (key - arr[end])) {
+      return arr[start];
+    }
+    return arr[end];
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cout << MinimumDifference::searchMinDiffElement(vector<int>{4, 6, 10}, 7) << endl;
+  cout << MinimumDifference::searchMinDiffElement(vector<int>{4, 6, 10}, 4) << endl;
+  cout << MinimumDifference::searchMinDiffElement(vector<int>{1, 3, 8, 10, 15}, 12) << endl;
+  cout << MinimumDifference::searchMinDiffElement(vector<int>{4, 6, 10}, 17) << endl;
+}
