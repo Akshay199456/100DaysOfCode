bc13576b436ffa0b983be9f5d836f6afe517981b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-27 16:14:41 -0700

    added my approaches(1) pb 74 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/74(Search in rotated array).cpp
@@ -0,0 +1,76 @@
+/*
+Problem statement:
+
+Given an array of numbers which is sorted in ascending order and also rotated by some arbitrary number, find if a given ‘key’ is present in it.
+
+Write a function to return the index of the ‘key’ in the rotated array. If the ‘key’ is not present, return -1. You can assume that the given array does not have any duplicates.
+
+Example 1:
+
+Input: [10, 15, 1, 3, 8], key = 15
+Output: 1
+Explanation: '15' is present in the array at index '1'.
+    1   
+    3   
+    8   
+    10   
+    15   
+ Original array:  
+ Array after 2 rotations:  
+    10   
+    15   
+    1   
+    3   
+    8   
+Example 2:
+
+Input: [4, 5, 7, 9, 10, -1, 2], key = 10
+Output: 4
+Explanation: '10' is present in the array at index '4'.
+ Original array:  
+    -1   
+    2   
+    4   
+    5   
+    7   
+    9   
+    10   
+    4   
+    5   
+    7   
+    9   
+    10   
+    -1   
+    2   
+*/
+
+/*
+-------------------------    My Approaches:
+1. 
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
