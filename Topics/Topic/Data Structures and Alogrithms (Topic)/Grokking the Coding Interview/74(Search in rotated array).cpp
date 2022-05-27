2d9d65e25d19cc0a8ea1a5ba8ecc6829146b0aaf
Akshay Mysoe <akshay.kum94@gmail.com>
2022-05-27 16:17:32 -0700

    added my approahces(1)-1 pb 74 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/74(Search in rotated array).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/74(Search in rotated array).cpp
@@ -46,10 +46,19 @@ Explanation: '10' is present in the array at index '4'.
 
 /*
 -------------------------    My Approaches:
-1. 
+1. Modified binary search [does not work]
 
-    Time complexity: O()
-    Space complexity: O()
+    the tactic to this problem is simple. we need to find either hte min element or max
+    element in the aray. for either of the two element indexes known, we can divide the 
+    array into two sorted parts and we can then find the element we are looking for in the respective sorted 
+    half.
+
+    while i was able to do the lattere, i had trouible coming up with an algorithm to 
+    determine the max/min element in the sorted rotated array in O(log n) tiome. if i 
+    could have got that, i wouild have been able to solve the problem fully.
+
+    Time complexity: O(logn)
+    Space complexity: O(1)
 */
 
 
@@ -69,8 +78,78 @@ Explanation: '10' is present in the array at index '4'.
 
 
 
-//  My approaches(1)
+//  My approaches(1) [Does not work]
 
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class SearchRotatedArray {
+ public:
+  static int getMaxIndex(const vector<int>& arr){
+    int beg = 0, end = arr.size()-1;
+    while(beg <= end){
+      int mid = beg + (end-beg)/2;
+      bool leftComplete = false, rightComplete = false;
+
+      if(mid == beg || arr[mid] > arr[beg])
+        leftComplete = true;
+      if(mid == end || arr[mid] > arr[end])
+        rightComplete = true;
+      
+      // if in proper ascending order
+      if(leftComplete && rightComplete)
+        return mid;
+      
+      if(leftComplete)
+        beg = mid+1;
+      else
+        end = mid-1;
+
+    }
+    return -1;
+  }
+
+  static int findElement(const vector<int>& arr, int key, int beg, int end){
+    while(beg <= end){
+      int mid = beg + (end-beg)/2;
+      
+      if(arr[mid] == key)
+        return mid;
+      else if(key < arr[mid])
+        end = mid-1;
+      else
+        beg = mid+1;
+    }
+    return -1;
+  }
+
+  static int search(const vector<int>& arr, int key) {
+    // TODO: Write your code here
+    if(arr.size()){
+      int maxIndex = getMaxIndex(arr);
+      cout<<"Max index: "<<maxIndex<<endl;
+      int leftSearch = findElement(arr, key, 0, maxIndex);
+      if(leftSearch != - 1)
+        return leftSearch;
+      return findElement(arr, key, maxIndex+1, arr.size()-1);   
+    }
+    return -1;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cout << SearchRotatedArray::search(vector<int>{10,15,1,3,8}, 15) << endl;
+  cout << SearchRotatedArray::search(vector<int>{4,5,7,9,10,-1,2}, 10) << endl;
+  cout << SearchRotatedArray::search(vector<int>{10,15,1,3,8}, 11) << endl;
+  cout << SearchRotatedArray::search(vector<int>{1,3,8,10,15}, 8) << endl;
+  cout << SearchRotatedArray::search(vector<int>{15,1,3,8,10}, 8) << endl;
+  cout << SearchRotatedArray::search(vector<int>{10,15,1,3,8}, 8) << endl;
+  cout << SearchRotatedArray::search(vector<int>{8,10,15,1,3}, 8) << endl;
+  cout << SearchRotatedArray::search(vector<int>{3,8,10,15,1}, 8) << endl;
+
+}
 
 
 //  Other Approaches(1)
