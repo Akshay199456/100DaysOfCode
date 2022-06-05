c72fe7a0913468fbcc65a02c7a90655cb8364a93
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-05 08:28:01 -0700

    added my approaches(1) pb 77 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/77(Two Single Numbers).cpp
@@ -0,0 +1,60 @@
+/*
+Problem statement:
+
+n a non-empty array of numbers, every number appears exactly twice except two numbers that appear only once. Find the two numbers that appear only once.
+
+Example 1:
+
+Input: [1, 4, 2, 1, 3, 5, 6, 2, 3, 5]
+Output: [4, 6]
+Example 2:
+
+Input: [2, 1, 3, 2]
+Output: [1, 3]
+*/
+
+/*
+-------------------------    My Approaches:
+1. [not coded]
+
+    tbf, we cam yup with two approaches but none of those approaches are the exor approach.
+
+    approach 1
+
+    thee first apporach was using hashmap or hashset to store orccurences. when it is not present, we insert it. when it is
+    present, we remove it. at the end, the ones that remain are the ones that have count 1.
+
+    time complexity: O(n)
+    Space complexity: O(n)
+
+
+    approach 2
+
+    we can sort the arry and then use two poitners to find the one which dont hae a duplicate occurrence
+
+    Time complexity: O(nlogn)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
