bcca794d12f47738de300686b68046a7165e1fa1
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-16 07:41:53 -0700

    completed pb 78 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/78(Complement of base 10 number).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/78(Complement of base 10 number).cpp
@@ -35,14 +35,42 @@ Explanation: 10 is 1010 in binary, its complement is 0101 in binary, which is 5 
 -------------------------    Other Approaches 
 1.
 
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(b) where b s the number of bits required to store the given number
+    Space complexity: O(1)
 */
 
 
 /*
 -------------------------    Notes
+the following properties of xor:
+    1. it will return 1 if we take xor of two different bits i.e 1^0 = 0^1 = 1
+    2. it will return 0 if we take xor of two same bits i.e 0^0 = 1^1 = 0. in other words, xor of two
+    same numbers is 0.
+    3. it returns the same number if we xor with 0.
 
+from the above mentioned firt property, we can conclude that xor of a number with its complement
+will resukt in an number that has all of its bit set to 1. for example, the binary complement of 101 is 010 and if
+we take xor of these two numbers, we will get a number wil all bits set to 1, i.e 101 ^ 010 = 111
+
+we can write this fact in the following operation:
+number ^ complement = all_bits_set
+
+lets add 'number' on both sides
+number ^ number ^ complement = number ^ all_bits_set
+
+from the above mentioned second property:
+0^complement = number ^ all_bits_set
+
+from the above mentioned third proerty:
+complement = number ^ all_bits_set
+
+can use this fact to find the complement of anyu number
+
+how do we calculate all_bits_set? One way to calculate all_bits_set will be to first count the bits
+required to store the given number. we can then use the fact that that for a number which is a compleet power of 2
+i.e can be written as pow(2,n), if we subtract '1' from such a s number, we get a number which has 'n'
+lease significant bits se to 1. for example, '4' which is a cmplement powerof '2', and '3' (which is one less than 4)
+has a bonary representation of '11' i'e it has '2' least significant bits set to 1.
 */
 
 
@@ -71,3 +99,34 @@ int main(int argc, char* argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <math.h>
+
+class CalculateComplement {
+public:
+  static int bitwiseComplement(int num) {
+    // count number of total bits in 'num'
+    int bitCount = 0;
+    int n = num;
+    while (n > 0) {
+      bitCount++;
+      n = n >> 1;
+    }
+
+    // for a number which is a complete power of '2' i.e., it can be written as pow(2, n), if we
+    // subtract '1' from such a number, we get a number which has 'n' least significant bits set to
+    // '1'. For example, '4' which is a complete power of '2', and '3' (which is one less than 4)
+    // has a binary representation of '11' i.e., it has '2' least significant bits set to '1'
+    int all_bits_set = pow(2, bitCount) - 1;
+
+    // from the solution description: complement = number ^ all_bits_set
+    return num ^ all_bits_set;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  cout << "Bitwise complement is: " << CalculateComplement::bitwiseComplement(8) << endl;
+  cout << "Bitwise complement is: " << CalculateComplement::bitwiseComplement(10) << endl;
+}
