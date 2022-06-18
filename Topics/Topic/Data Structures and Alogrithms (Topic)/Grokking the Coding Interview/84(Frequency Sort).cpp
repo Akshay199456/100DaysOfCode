1b93abc6d657455eae768dde43f1e84d7a8bfe0b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-18 13:22:00 -0700

    completed pb 84 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/84(Frequency Sort).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/84(Frequency Sort).cpp
@@ -38,7 +38,10 @@ Explanation: 'b' appeared three times, 'a' appeared twice, and 'c' appeared only
 
 /*
 -------------------------    Notes
+follows top k elements pattern and shares similarities with top k frequent numbers.
 
+can follow the same approach as dicussed in top k frequent numbers. first will need the frequencies of all characters
+then use a max heap to find the most occuring characters
 
     Time complexity: O()
     Space complexity: O()
@@ -107,3 +110,53 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+
+class FrequencySort {
+ public:
+  struct valueCompare {
+    bool operator()(const pair<char, int> &x, const pair<char, int> &y) {
+      return y.second > x.second;
+    }
+  };
+
+  static string sortCharacterByFrequency(const string &str) {
+    // find the frequency of each character
+    unordered_map<char, int> characterFrequencyMap;
+    for (char chr : str) {
+      characterFrequencyMap[chr]++;
+    }
+
+    priority_queue<pair<char, int>, vector<pair<char, int>>, valueCompare> maxHeap;
+
+    // add all characters to the max heap
+    for (auto entry : characterFrequencyMap) {
+      maxHeap.push(entry);
+    }
+
+    // build a string, appending the most occurring characters first
+    string sortedString = "";
+    while (!maxHeap.empty()) {
+      auto entry = maxHeap.top();
+      maxHeap.pop();
+      for (int i = 0; i < entry.second; i++) {
+        sortedString += entry.first;
+      }
+    }
+
+    return sortedString;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  string result = FrequencySort::sortCharacterByFrequency("Programming");
+  cout << "Here is the given string after sorting characters by frequency: " << result << endl;
+
+  result = FrequencySort::sortCharacterByFrequency("abcbab");
+  cout << "Here is the given string after sorting characters by frequency: " << result << endl;
+}
