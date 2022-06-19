01ffb8b06cc8b97b6e623aa8f55f0b64f3a62c6c
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-19 14:50:47 -0700

    completed pb 87 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/87(Maximum Distinct Elements).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/87(Maximum Distinct Elements).cpp
@@ -54,7 +54,14 @@ Explanation: We can remove one occurrence of '4' to get three distinct numbers.
 
 /*
 -------------------------    Notes
+follows top k numbers pattern and shares similarities with top k frequent numbers. can follow a similar approach as dicssued in top k frequent numbers problem:
+1. first, will need to find the frequencies of all th enumbers
+2. then need to push all numbers that are not distinct (i.e have a frequency higher than one) in a min hea[ based ] on otheir frequencies. at the same time, will keep a running count of all the docint numbers
+3. following a greedy approach, in stepwise fashion, will remove the least frequent number from heap (i.e top element of min heap) and try to make it distinct. will see
+if we can remove all occurrence os a number except one. if we can, we will increment our riunning couint of dotinct numbers. also have to keep a coiun t of how many removals we have done.
+4. if after removing elements from the heap, we are still left with some deletion, have to remoe some disticnt elements
 
+pretty much the same approach as mine
 
     Time complexity: O()
     Space complexity: O()
@@ -137,3 +144,73 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+class MaximumDistinctElements {
+ public:
+  struct valueCompare {
+    bool operator()(const pair<int, int> &x, const pair<int, int> &y) {
+      return x.second > y.second;
+    }
+  };
+
+  static int findMaximumDistinctElements(const vector<int> &nums, int k) {
+    int distinctElementsCount = 0;
+    if (nums.size() <= k) {
+      return distinctElementsCount;
+    }
+
+    // find the frequency of each number
+    unordered_map<int, int> numFrequencyMap;
+    for (auto i : nums) {
+      numFrequencyMap[i]++;
+    }
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, valueCompare> minHeap;
+
+    // insert all numbers with frequency greater than '1' into the min-heap
+    for (auto entry : numFrequencyMap) {
+      if (entry.second == 1) {
+        distinctElementsCount++;
+      } else {
+        minHeap.push(entry);
+      }
+    }
+
+    // following a greedy approach, try removing the least frequent numbers first from the min-heap
+    while (k > 0 && !minHeap.empty()) {
+      auto entry = minHeap.top();
+      minHeap.pop();
+      // to make an element distinct, we need to remove all of its occurrences except one
+      k -= entry.second - 1;
+      if (k >= 0) {
+        distinctElementsCount++;
+      }
+    }
+
+    // if k > 0, this means we have to remove some distinct numbers
+    if (k > 0) {
+      distinctElementsCount -= k;
+    }
+
+    return distinctElementsCount;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  int result =
+      MaximumDistinctElements::findMaximumDistinctElements(vector<int>{7, 3, 5, 8, 5, 3, 3}, 2);
+  cout << "Maximum distinct numbers after removing K numbers: " << result << endl;
+
+  result = MaximumDistinctElements::findMaximumDistinctElements(vector<int>{3, 5, 12, 11, 12}, 3);
+  cout << "Maximum distinct numbers after removing K numbers: " << result << endl;
+
+  result = MaximumDistinctElements::findMaximumDistinctElements(
+      vector<int>{1, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5}, 2);
+  cout << "Maximum distinct numbers after removing K numbers: " << result << endl;
+}
