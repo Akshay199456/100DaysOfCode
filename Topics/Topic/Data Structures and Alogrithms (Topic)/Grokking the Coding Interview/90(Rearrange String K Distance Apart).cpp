1f9a62716a9c6b8adfd5d43292eb4770eea35acf
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-21 07:01:28 -0700

    completed pb 90 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/90(Rearrange String K Distance Apart).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/90(Rearrange String K Distance Apart).cpp
@@ -60,7 +60,11 @@ Explanation: We cannot find an arrangement of the string where any two 'a' are 3
 
 /*
 -------------------------    Notes
+problem follows the top k numbers pattern and is quite similar to rearrage string problem. only difference is that in the 'rearrange string', the same chracters need not be adjacent i.e they should be at least 2 distance aprt (in other words, there should be at least one character betweeen the two same characters) while in the 
+current prb;em,. the same characters should be 'K' distance appart.
 
+following similar approach, since we were inserting a character back in the hea[ n the next iteration, in this proble,m, we will
+re-insert the chracter after k iterations. we can keep track of the previous characters ni a quueeu to insert them back in the heap after k iterations
 
     Time complexity: O()
     Space complexity: O()
@@ -147,3 +151,67 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class RearrangeStringKDistanceApart {
+ public:
+  struct valueCompare {
+    char operator()(const pair<int, int> &x, const pair<int, int> &y) {
+      return y.second > x.second;
+    }
+  };
+
+  static string reorganizeString(const string &str, int k) {
+    if (k <= 1) return str;
+    
+    unordered_map<char, int> charFrequencyMap;
+    for (char chr : str) {
+      charFrequencyMap[chr]++;
+    }
+
+    priority_queue<pair<char, int>, vector<pair<char, int>>, valueCompare> maxHeap;
+
+    // add all characters to the max heap
+    for (auto entry : charFrequencyMap) {
+      maxHeap.push(entry);
+    }
+
+    queue<pair<char, int>> queue;
+    string resultString;
+    while (!maxHeap.empty()) {
+      auto currentEntry = maxHeap.top();
+      maxHeap.pop();
+      // append the current character to the result string and decrement its count
+      resultString += currentEntry.first;
+      currentEntry.second--;
+      queue.push(currentEntry);
+      if (queue.size() == k) {
+        auto entry = queue.front();
+        queue.pop();
+        if (entry.second > 0) {
+          maxHeap.push(entry);
+        }
+      }
+    }
+
+    // if we were successful in appending all the characters to the result string, return it
+    return resultString.length() == str.length() ? resultString : "";
+  }
+};
+
+int main(int argc, char *argv[]) {
+  cout << "Reorganized string: "
+       << RearrangeStringKDistanceApart::reorganizeString("Programming", 3) << endl;
+  cout << "Reorganized string: "
+       << RearrangeStringKDistanceApart::reorganizeString("mmpp", 2) << endl;
+  cout << "Reorganized string: "
+       << RearrangeStringKDistanceApart::reorganizeString("aab", 2) << endl;
+  cout << "Reorganized string: " << RearrangeStringKDistanceApart::reorganizeString("aappa", 3)
+       << endl;
+}
