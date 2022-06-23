ac701f88bf473fe49a13fbaa6a35ed49da37bb4b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-22 20:37:23 -0700

    completed pb 91 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/91(Scheduling Tasks).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/91(Scheduling Tasks).cpp
@@ -41,7 +41,13 @@ Explanation: a -> b -> idle -> idle -> a
 
 /*
 -------------------------    Notes
+problem followss the top k elements pattern and is quuite similar to rearrange string k distance apart.
+need to rearrange tasks such \that same tasks are k distance apart.
 
+following similar approach, will use a max heap to execute the highest frequency task first. after executing tasks, we decrease its frequency and put it in a 
+waiting list. in each iteration, we will try to excute as many as k+1 tasks. for the next
+iteration, we will put all the waiting tasks back in the max heap. if for any iteration, we are not  abel toe execute
+k+1 tasks, the cpu has to remain idel for the reamingin time in the next iteration.
 
     Time complexity: O()
     Space complexity: O()
@@ -115,7 +121,7 @@ class TaskScheduler {
 };
 
 int main(int argc, char *argv[]) {
-  vector<char> tasks = {'a'};
+  vector<char> tasks = {'a', 'a', 'a', 'b', 'c', 'c','d','d','d','e','f'};
   cout << "Minimum intervals needed to execute all tasks: "
        << TaskScheduler::scheduleTasks(tasks, 0) << endl;
 
@@ -128,3 +134,66 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+class TaskScheduler {
+ public:
+  struct valueCompare {
+    char operator()(const pair<int, int> &x, const pair<int, int> &y) {
+      return y.second > x.second;
+    }
+  };
+
+  static int scheduleTasks(vector<char> &tasks, int k) {
+    int intervalCount = 0;
+    unordered_map<char, int> taskFrequencyMap;
+    for (char chr : tasks) {
+      taskFrequencyMap[chr]++;
+    }
+
+    priority_queue<pair<char, int>, vector<pair<char, int>>, valueCompare> maxHeap;
+
+    // add all tasks to the max heap
+    for (auto entry : taskFrequencyMap) {
+      maxHeap.push(entry);
+    }
+
+    while (!maxHeap.empty()) {
+      vector<pair<char, int>> waitList;
+      int n = k + 1;  // try to execute as many as 'k+1' tasks from the max-heap
+      for (; n > 0 && !maxHeap.empty(); n--) {
+        intervalCount++;
+        auto currentEntry = maxHeap.top();
+        maxHeap.pop();
+        if (currentEntry.second > 1) {
+          currentEntry.second--;
+          waitList.push_back(currentEntry);
+        }
+      }
+      // put all the waiting list back on the heap
+      for (auto it = waitList.begin(); it != waitList.end(); it++) {
+        maxHeap.push(*it);
+      }
+      if (!maxHeap.empty()) {
+        intervalCount += n;  // we'll be having 'n' idle intervals for the next iteration
+      }
+    }
+
+    return intervalCount;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<char> tasks = {'a', 'a', 'a', 'b', 'c', 'c'};
+  cout << "Minimum intervals needed to execute all tasks: "
+       << TaskScheduler::scheduleTasks(tasks, 2) << endl;
+
+  tasks = vector<char>{'a', 'b', 'a'};
+  cout << "Minimum intervals needed to execute all tasks: "
+       << TaskScheduler::scheduleTasks(tasks, 3) << endl;
+}
