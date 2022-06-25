25844fd6543d6d3de859dbd46187ad87bb996a91
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-24 21:31:28 -0700

    completed pb 94 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/94(Kth smallest number in m sorted lists).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/94(Kth smallest number in m sorted lists).cpp
@@ -41,10 +41,24 @@ Explanation: The 3rd smallest number among all the arrays is 7.
 
 /*
 -------------------------    Notes
+problem follows k way merge pattern and can follow a smilar appraoch as dficussed in merge k sorted lists.
 
+can start merging all the arrays, but instead of isnerting numbers into a merged list, will keep count to see how many elemnts have been isnertde int he merged
+list. once that count is equal to k, we have found our answer
+
+big difference from merge k sorted lists is that in this proble,., the input is a lit of arrays compared to linked lists. this means that when we want to push
+the next number in th eheap, we need to know what the index of the current number in the current array was. to handle this, we will ned to keep track of the
+array and the element indices
 
     Time complexity: O()
     Space complexity: O()
+
+
+Additional notes:
+
+while both of the k way merge pattern problems we have solved till now have been easy, we have taken a bit of time in solving them.
+the main issue hasnt been solving th eproblem. we actually do tht pretty quick. the main time consuming operation has been in diagnosign issues or not 
+abiding by all the constrainst s ot eh rpoblem.  need to solve more problems for the first case and read the problem more carefully for the secfond.
 */
 
 
@@ -106,3 +120,55 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+class KthSmallestInMSortedArrays {
+ public:
+  struct valueCompare {
+    bool operator()(const pair<int, pair<int, int>> &x, const pair<int, pair<int, int>> &y) {
+      return x.first > y.first;
+    }
+  };
+
+  static int findKthSmallest(const vector<vector<int>> &lists, int k) {
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, valueCompare>
+        minHeap;
+
+    // put the 1st element of each array in the min heap
+    for (int i = 0; i < lists.size(); i++) {
+      if (!lists[i].empty()) {
+        minHeap.push(make_pair(lists[i][0], make_pair(i, 0)));
+      }
+    }
+
+    // take the smallest (top) element form the min heap, if the running count is equal to k return
+    // the number if the array of the top element has more elements, add the next element to the
+    // heap
+    int numberCount = 0, result = 0;
+    while (!minHeap.empty()) {
+      auto node = minHeap.top();
+      minHeap.pop();
+      result = node.first;
+      if (++numberCount == k) {
+        break;
+      }
+      node.second.second++;
+      if (lists[node.second.first].size() > node.second.second) {
+        node.first = lists[node.second.first][node.second.second];
+        minHeap.push(node);
+      }
+    }
+
+    return result;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<vector<int>> lists = {{2, 6, 8}, {3, 6, 7}, {1, 3, 4}};
+  int result = KthSmallestInMSortedArrays::findKthSmallest(lists, 5);
+  cout << "Kth smallest number is: " << result;
+}
