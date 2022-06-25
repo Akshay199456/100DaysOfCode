020dcf72632619d9a3d047cc285d558df65f4e25
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-25 11:49:25 -0700

    completed pb 95 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/95(Kth smallest number in sorted matrix).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/95(Kth smallest number in sorted matrix).cpp
@@ -39,7 +39,8 @@ Explanation: The 5th smallest number in the matrix is 7.
 
 /*
 -------------------------    Notes
-
+follows k way merge pattern and ca be easily converted to kth samllest number in m sorted lists problem. as each row(or column) of the given matrix
+can be seen as a sorted list, we essentially need to find the kth smallest number in n sorted lists
 
     Time complexity: O()
     Space complexity: O()
@@ -97,3 +98,55 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+class KthSmallestInSortedMatrix {
+ public:
+  struct numCompare {
+    bool operator()(const pair<int, pair<int, int>> &x, const pair<int, pair<int, int>> &y) {
+      return x.first > y.first;
+    }
+  };
+
+  static int findKthSmallest(vector<vector<int>> &matrix, int k) {
+    int n = matrix.size();
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, numCompare>
+        minHeap;
+
+    // put the 1st element of each row in the min heap
+    // we don't need to push more than 'k' elements in the heap
+    for (int i = 0; i < n && i < k; i++) {
+      minHeap.push(make_pair(matrix[i][0], make_pair(i, 0)));
+    }
+
+    // take the smallest element form the min heap, if the running count is equal to k return the
+    // number if the row of the top element has more elements, add the next element to the heap
+    int numberCount = 0, result = 0;
+    while (!minHeap.empty()) {
+      auto heapTop = minHeap.top();
+      minHeap.pop();
+      result = heapTop.first;
+      if (++numberCount == k) {
+        break;
+      }
+
+      heapTop.second.second++;
+      if (n > heapTop.second.second) {
+        heapTop.first = matrix[heapTop.second.first][heapTop.second.second];
+        minHeap.push(heapTop);
+      }
+    }
+    return result;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<vector<int>> matrix2 = {vector<int>{2, 6, 8}, vector<int>{3, 7, 10},
+                                 vector<int>{5, 8, 11}};
+  int result = KthSmallestInSortedMatrix::findKthSmallest(matrix2, 5);
+  cout << "Kth smallest number is: " << result << endl;
+}
