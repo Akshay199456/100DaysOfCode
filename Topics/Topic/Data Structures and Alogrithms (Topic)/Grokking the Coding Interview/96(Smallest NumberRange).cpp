4837db9d64fe9187a031c95485a84ce77345990c
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-25 14:10:42 -0700

    completed pb 96 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/96(Smallest NumberRange).cpp
@@ -0,0 +1,54 @@
+/*
+Problem statement:
+
+Given ‘M’ sorted arrays, find the smallest range that includes at least one number from each of the ‘M’ lists.
+
+Example 1:
+
+Input: L1=[1, 5, 8], L2=[4, 12], L3=[7, 8, 10]
+Output: [4, 7]
+Explanation: The range [4, 7] includes 5 from L1, 4 from L2 and 7 from L3.
+Example 2:
+
+Input: L1=[1, 9], L2=[4, 12], L3=[7, 10, 16]
+Output: [9, 12]
+Explanation: The range [9, 12] includes 9 from L1, 12 from L2 and 10 from L3.
+*/
+
+/*
+-------------------------    My Approaches:
+1. k way merge [not coded]
+
+    we can use the k way merge pattern in order to create a list of all the elemnts in the arrays in theire sorted order. once we have that list,
+    we can then use the two pointer technique to go through the intervals and see if we have a number in each of the lists within that interval.
+    using the two pointer approach then would solve this problem
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
