dc9ee8059640a6956e452d57b29d17e211c35df1
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-27 08:29:38 -0700

    completed pb 98 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/98(Subsets).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/98(Subsets).cpp
@@ -36,7 +36,7 @@ Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
 
 /*
 -------------------------    Notes
-
+to generate all subsets of given set, can use the BFS appproach. we can start with empty set, iterate through all numbers one-by-one, andd them to existing sets to create new subsets.
 
     Time complexity: O()
     Space complexity: O()
@@ -96,3 +96,48 @@ int main(int argc, char* argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class Subsets {
+ public:
+  static vector<vector<int>> findSubsets(const vector<int>& nums) {
+    vector<vector<int>> subsets;
+    // start by adding the empty subset
+    subsets.push_back(vector<int>());
+    for (auto currentNumber : nums) {
+      // we will take all existing subsets and insert the current number in them to create new
+      // subsets
+      int n = subsets.size();
+      for (int i = 0; i < n; i++) {
+        // create a new subset from the existing subset and insert the current element to it
+        vector<int> set(subsets[i]);
+        set.push_back(currentNumber);
+        subsets.push_back(set);
+      }
+    }
+    return subsets;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  vector<vector<int>> result = Subsets::findSubsets(vector<int>{1, 3});
+  cout << "Here is the list of subsets: " << endl;
+  for (auto vec : result) {
+    for (auto num : vec) {
+      cout << num << " ";
+    }
+    cout << endl;
+  }
+
+  result = Subsets::findSubsets(vector<int>{1, 5, 3});
+  cout << "Here is the list of subsets: " << endl;
+  for (auto vec : result) {
+    for (auto num : vec) {
+      cout << num << " ";
+    }
+    cout << endl;
+  }
+}
