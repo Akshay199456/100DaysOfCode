9fa4088ee1de67e5be93620afa26121a0f05f15e
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-26 17:35:34 -0700

    added my approaches(1) pb 97 gtok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/97(K pairs with largest sum).cpp
@@ -0,0 +1,52 @@
+/*
+Problem statement:
+
+Given two sorted arrays in descending order, find ‘K’ pairs with the largest sum where each pair consists of numbers from both the arrays.
+
+Example 1:
+
+Input: L1=[9, 8, 2], L2=[6, 3, 1], K=3
+Output: [9, 3], [9, 6], [8, 6] 
+Explanation: These 3 pairs have the largest sum. No other pair has a sum larger than any of these.
+Example 2:
+
+Input: L1=[5, 2, 1], L2=[2, -1], K=3
+Output: [5, 2], [5, -1], [2, 2] 
+*/
+
+/*
+-------------------------    My Approaches:
+1. using max heap to store sum by going through all elementsof all arrays [not coded]
+
+    we can use a double for loop to go through all of th eelements in the arrays and push therir sum into a max heap. we later retireve k pairs from it
+
+
+    Time complexity: O(mn logmn)
+    Space complexity: O(mn)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
