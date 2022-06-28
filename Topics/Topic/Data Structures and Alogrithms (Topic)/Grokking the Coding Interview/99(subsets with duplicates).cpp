58e34d8d8d09752bca07611bdcc1fe43459e8430
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-27 17:35:54 -0700

    added my approaches(1) pb 9 grok

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/99(subsets with duplicates).cpp
@@ -0,0 +1,52 @@
+/*
+Problem statement:
+
+Given a set of numbers that might contain duplicates, find all of its distinct subsets.
+
+Example 1:
+
+Input: [1, 3, 3]
+Output: [], [1], [3], [1,3], [3,3], [1,3,3]
+Example 2:
+
+Input: [1, 5, 3, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3], [3,3], [1,3,3], [3,3,5], [1,5,3,3] 
+*/
+
+/*
+-------------------------    My Approaches:
+1. use map and custom hash to store each vector [not coded
+2. go through all n to see if it matches with any exisitng solution [not coded]
+
+now we came up with 2 approaches to solve this problem neither of which we have coded since they both are not using the subset technqiue we learnt.
+as a result, we can use any of those two approaches to solve the problem.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
