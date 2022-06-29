6bb2d8a6528c4cdaaade2a06f7d38c1297236796
Akshay Mysoe <akshay.kum94@gmail.com>
2022-06-28 21:30:59 -0700

    completed pb 101 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/101(String Permutations by changing case).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/101(String Permutations by changing case).cpp
@@ -45,6 +45,10 @@ Output: "ab7c", "Ab7c", "aB7c", "AB7c", "ab7C", "Ab7C", "aB7C", "AB7C"
 
 /*
 -------------------------    Notes
+problem follows the subsets pattern and can be mapped to the permutations problem.
+
+following a bfs approach, we will consider one character at a time. since we need to preserve the acharacter sequence, we can start with the actual string and [rpcess] each character (i.e make it upper-case or lower-case)
+one by one
 
 
     Time complexity: O()
@@ -169,3 +173,54 @@ int main(int argc, char* argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class LetterCaseStringPermutation {
+ public:
+  static vector<string> findLetterCaseStringPermutations(const string& str) {
+    vector<string> permutations;
+    if (str == "") {
+      return permutations;
+    }
+
+    permutations.push_back(str);
+    // process every character of the string one by one
+    for (int i = 0; i < str.length(); i++) {
+      if (isalpha(str[i])) {  // only process characters, skip digits
+        // we will take all existing permutations and change the letter case appropriately
+        int n = permutations.size();
+        for (int j = 0; j < n; j++) {
+          vector<char> chs(permutations[j].begin(), permutations[j].end());
+          // if the current character is in upper case change it to lower case or vice versa
+          if (isupper(chs[i])) {
+            chs[i] = tolower(chs[i]);
+          } else {
+            chs[i] = toupper(chs[i]);
+          }
+          permutations.push_back(string(chs.begin(), chs.end()));
+        }
+      }
+    }
+    return permutations;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  vector<string> result = LetterCaseStringPermutation::findLetterCaseStringPermutations("ad52");
+  cout << "String permutations are: ";
+  for (auto str : result) {
+    cout << str << " ";
+  }
+  cout << endl;
+
+  result = LetterCaseStringPermutation::findLetterCaseStringPermutations("ab7c");
+  cout << "String permutations are: ";
+  for (auto str : result) {
+    cout << str << " ";
+  }
+  cout << endl;
+}
