a19dbd8f13f605d57e119f3b605dedab5008ab0f
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-23 21:37:10 -0700

    completed pb 2 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/2(First Eleemnt Not Smaller Than Target).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/2(First Eleemnt Not Smaller Than Target).cpp
@@ -44,8 +44,9 @@ Explanation: the first element larger than 6 is 7 which has index 3.
 
 /*
 -------------------------    Notes
+problem is equivalent to finding the boundary of elements < 3 and elements >=3. imagine if we apply a filter of arr[i]< target, we would get:
 
-
+now the problem is reduced to finding the first true element in a boolean array. and we already know how to do this from find the boundary problem.
     Time complexity: O()
     Space complexity: O()
 */
@@ -104,3 +105,47 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int first_not_smaller(std::vector<int> arr, int target) {
+    int left = 0, right = arr.size() - 1, boundary_index = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] >= target) {
+            boundary_index = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return boundary_index;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = first_not_smaller(arr, target);
+    std::cout << res << '\n';
+}
