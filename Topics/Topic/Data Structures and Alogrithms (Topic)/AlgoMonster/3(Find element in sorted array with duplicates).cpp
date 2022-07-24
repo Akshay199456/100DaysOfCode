c9024ef1950dc2bb961f0a4a4a672b6f86eadae4
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-24 09:57:11 -0700

    completed pb 3 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/3(Find element in sorted array with duplicates).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/3(Find element in sorted array with duplicates).cpp
@@ -102,3 +102,49 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int find_first_occurrence(std::vector<int> arr, int target) {
+    int left = 0, right = arr.size() - 1, target_index = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            target_index = mid;
+            right = mid - 1;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return target_index;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = find_first_occurrence(arr, target);
+    std::cout << res << '\n';
+}
