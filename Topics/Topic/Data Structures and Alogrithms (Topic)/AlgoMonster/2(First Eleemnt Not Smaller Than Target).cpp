401bd8a593eb0b472164c3d4d7f27eb15ce5fba3
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-23 21:32:57 -0700

    added my approaches(1) pb 2 algo

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/2(First Eleemnt Not Smaller Than Target).cpp
@@ -0,0 +1,106 @@
+/*
+Problem statement:
+
+iven an array of integers sorted in increasing order and a target, find the index of the first element in the array that is larger than or equal to the target. Assume that it is guaranteed to find a satisfying number.
+
+Input:
+
+arr = [1, 3, 3, 5, 8, 8, 10]
+target = 2
+Output: 1
+
+Explanation: the first element larger than 2 is 3 which has index 1.
+
+Input:
+
+arr = [2, 3, 5, 7, 11, 13, 17, 19]
+target = 6
+Output: 3
+
+Explanation: the first element larger than 6 is 7 which has index 3.
+
+
+*/
+
+/*
+-------------------------    My Approaches:
+1. binary search
+
+    we can use the same binary search approach we applied in the previous problem to narrow down our search
+
+    Time complexity: O(logn)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int first_not_smaller(std::vector<int> arr, int target) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    int beg{0}, end{arr.size()-1}, boundaryIndex{-1};
+    while(beg <= end)
+    {
+        int mid =beg + (end-beg)/2;
+        if(arr[mid] >= target){
+            boundaryIndex = mid;
+            end = mid-1;
+        }
+        else
+            beg = mid+1;
+    }
+    return boundaryIndex;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = first_not_smaller(arr, target);
+    std::cout << res << '\n';
+}
+
+
+
+
+//  Other Approaches(1)
