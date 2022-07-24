12b402efecc64124c7bc7934b74a3d982d9d1f36
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-23 21:10:37 -0700

    completed pb 1 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/1(Finding the boundary).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/1(Finding the boundary).cpp
@@ -33,6 +33,29 @@ Explanation: first true's index is 2.
 
 /*
 -------------------------    Notes
+binary decision we have to make when we look at an element is:
+    1. if the element is false, we discard everything to the lft and the current element itself.
+    2. if the element is true, the current element could be the first true although there may be other true to the left. we discard everything to the right but what about the current element?
+
+    we can either keep the current element in the range or record it somewhere and then discard it. here we choose the latter.
+
+    we keep a variable boundary_undex that represents the elftmost true's index currently recorded. if the current element is true, then we update boundar_index
+    with its index and discard everything to the right inclduing the current element itself since its index has been recorded by the variable
+
+    good thing with this approach is that we dont have to moduft the while loop logic in the vanilla binary search from the last module besides introducing a variable
+
+
+
+Alternative approach
+
+another approach to handle case 2 above is to keep the current element in the search range instead of sicarding it i.e if arr[mid]: right = mid instead
+of right = mid-1. however, doing this without modifying the while condition will result in an infinte loop. this is because
+when left == right, right = mid will not modify right and this, not shrink search range and we will bne stuck in the while loop forever. to makee this worl
+we have to remove teh euality in the while condition.
+
+in addition, a while loop without eaulity will miss the single-element edge case so we have to add an additional hcck after the loop to handle this case.
+overall, we have to make three modifications to the vanilla binary search to make it work
+
 
 
     Time complexity: O()
@@ -84,3 +107,38 @@ int main() {
 
 
 //  Other Approaches(1)
+include <algorithm> // copy
+#include <iostream> // cin, cout
+#include <iterator> // back_inserter, istream_iterator
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int find_boundary(std::vector<bool> arr) {
+    int left = 0;
+    int right = arr.size() - 1;
+    int boundaryIndex = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid]) {
+            boundaryIndex = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return boundaryIndex;
+}
+
+int main() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<bool> arr;
+    bool b;
+    while (ss >> std::boolalpha >> b) {
+        arr.push_back(b);
+    }
+    int res = find_boundary(arr);
+    std::cout << res << '\n';
+}
