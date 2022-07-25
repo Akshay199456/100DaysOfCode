90808d79be26b0fff44f96a3fb7c72b97dfc5c87
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-25 12:46:29 -0700

    added my approaches(1

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/8(Max Depth of a tree).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/8(Max Depth of a tree).cpp
@@ -30,7 +30,16 @@ Input:
 
 /*
 -------------------------    Notes
+Explantation:
 
+1. decide on the return value
+    the problem asks the total max depth, so we return the depth for the current subtree after we visit a node
+2. idenitfy states
+    to devide the depth of current node, we only need depth from its children and dont neeed any additional state
+
+having decided on the state and reutnr value, we can now write the DFS.
+
+there are n nodes and n-1 edges in a tree so if we traverse each one then the total traversal is O(2n-1) which is O(n)
 
     Time complexity: O()
     Space complexity: O()
@@ -104,3 +113,61 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy, max
+#include <iostream> // cin, cout
+#include <iterator> // back_inserter, istream_iterator
+#include <sstream> // istringstream
+#include <string> // getline, stoi, string
+#include <vector> // vector
+
+template <typename T>
+struct Node {
+    T val;
+    Node<T>* left;
+    Node<T>* right;
+
+    explicit Node(T val, Node<T>* left = nullptr, Node<T>* right = nullptr)
+        : val{val}, left{left}, right{right} {}
+
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+int tree_max_depth(Node<int>* root) {
+    // Null node adds no depth
+    if (root == nullptr) return 0;
+    // Depth of current node's subtree = max depth of the two subtrees + 1 provided by current node
+    return std::max(tree_max_depth(root->left), tree_max_depth(root->right)) + 1;
+}
+
+// this function build a tree from input
+// learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+template<typename T, typename Iter, typename F>
+Node<T>* build_tree(Iter& it, F f) {
+    std::string val = *it;
+    ++it;
+    if (val == "x") return nullptr;
+    Node<T>* left = build_tree<T>(it, f);
+    Node<T>* right = build_tree<T>(it, f);
+    return new Node<T>{f(val), left, right};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+int main() {
+    std::vector<std::string> root_vec = get_words<std::string>();
+    auto root_it = root_vec.begin();
+    Node<int>* root = build_tree<int>(root_it, [](auto s) { return std::stoi(s); });
+    int res = tree_max_depth(root);
+    std::cout << res << '\n';
+}
---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/9(Visible Tree Node).cpp
@@ -0,0 +1,117 @@
+/*
+Problem statement:
+
+n a binary tree, we define a node "visible" when no node on the root-to-itself path (inclusive) has a greater value. The root is always "visible" since there are no other nodes between the root and itself. Given a binary tree, count the number of "visible" nodes.
+
+Input:
+
+
+*/
+
+/*
+-------------------------    My Approaches:
+1. dfs approach
+
+    we can use the dfs approach in order to solve this problem. we know that if we run into a null node, then we should return 0 as a null node cant be a visible node.
+    the other thing we want to check is if the current node is the nax value that we have seen in this path till now.
+    if it is, then it is a visible node and so we increment that count. we can then go to the left and right nodes and make the same decision by passing in the max value that we ahve seen till now
+
+    Time complexity: O(n)
+    Space complexity: O(n)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout
+#include <iterator> // back_inserter, istream_iterator
+#include <sstream> // istringstream
+#include <string> // getline, stoi, string
+#include <vector> // vector
+
+template <typename T>
+struct Node {
+    T val;
+    Node<T>* left;
+    Node<T>* right;
+
+    explicit Node(T val, Node<T>* left = nullptr, Node<T>* right = nullptr)
+        : val{val}, left{left}, right{right} {}
+
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+int getCount(Node<int> * root, int maxValue){
+    if(!root)
+        return 0;
+    
+    maxValue = std::max(maxValue, root->val);
+    int totalCount = getCount(root->left, maxValue) + getCount(root->right, maxValue);
+    
+    if(root->val >= maxValue)
+        totalCount +=1;
+    return totalCount;
+}
+
+int visible_tree_node(Node<int>* root) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    if(!root)
+        return 0;
+    return getCount(root, root->val);
+}
+
+// this function build a tree from input
+// learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+template<typename T, typename Iter, typename F>
+Node<T>* build_tree(Iter& it, F f) {
+    std::string val = *it;
+    ++it;
+    if (val == "x") return nullptr;
+    Node<T>* left = build_tree<T>(it, f);
+    Node<T>* right = build_tree<T>(it, f);
+    return new Node<T>{f(val), left, right};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+int main() {
+    std::vector<std::string> root_vec = get_words<std::string>();
+    auto root_it = root_vec.begin();
+    Node<int>* root = build_tree<int>(root_it, [](auto s) { return std::stoi(s); });
+    int res = visible_tree_node(root);
+    std::cout << res << '\n';
+}
+
+
+
+//  Other Approaches(1)
