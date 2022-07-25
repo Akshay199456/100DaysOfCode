c62676abfc3ae28eedd548b7e89c155bce02dcc4
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-25 12:50:44 -0700

    completed pb 9 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/9(Visible Tree Node).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/9(Visible Tree Node).cpp
@@ -32,7 +32,18 @@ Input:
 
 /*
 -------------------------    Notes
+we can dfs on the tree and keep track of the max value we have seen as we go.
 
+1. decide on the return value
+    the problem asks for the total no of visible nodes so we return the total no of visisible nodes for the current subtree after we visit a node
+
+2. identify states
+    the definition for a 'visible' node is its value is greater than any other node's value on the root-to-itself path. to determine whether the current node is visilbe or not, we need to lknow
+    the max value from the root to it. we can carry this as a state as we traverse down the tree.
+
+having decided on the state and return value, we can now write the dfs
+
+there are n nodes and n-1 edges in a tree so if we traverse each one then the total traversal is O(2n-1) which is O(n)
 
     Time complexity: O()
     Space complexity: O()
@@ -115,3 +126,69 @@ int main() {
 
 
 //  Other Approaches(1)
+include <algorithm> // copy
+#include <iostream> // cin, cout
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, stoi, string
+#include <vector> // vector
+
+template <typename T>
+struct Node {
+    T val;
+    Node<T>* left;
+    Node<T>* right;
+
+    explicit Node(T val, Node<T>* left = nullptr, Node<T>* right = nullptr)
+        : val{val}, left{left}, right{right} {}
+
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+int dfs(Node<int>* root, int max_sofar) {
+    if (!root) return 0;
+    int total = 0;
+    if (root->val >= max_sofar) total++;
+    total += dfs(root->left, std::max(max_sofar, root->val));
+    total += dfs(root->right, std::max(max_sofar, root->val));
+    return total;
+}
+
+int visible_tree_node(Node<int>* root) {
+    // start max_sofar with smallest number possible so any value root has is greater than it
+    return dfs(root, std::numeric_limits<int>::min());
+}
+
+// this function build a tree from input
+// learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+template<typename T, typename Iter, typename F>
+Node<T>* build_tree(Iter& it, F f) {
+    std::string val = *it;
+    ++it;
+    if (val == "x") return nullptr;
+    Node<T>* left = build_tree<T>(it, f);
+    Node<T>* right = build_tree<T>(it, f);
+    return new Node<T>{f(val), left, right};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+int main() {
+    std::vector<std::string> root_vec = get_words<std::string>();
+    auto root_it = root_vec.begin();
+    Node<int>* root = build_tree<int>(root_it, [](auto s) { return std::stoi(s); });
+    int res = visible_tree_node(root);
+    std::cout << res << '\n';
+}
