a99779fdaa2293fbc8faeeda71ab98cf4b296ed2
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-25 20:44:36 -0700

    completed pb 12 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/12(Insert into BST).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/12(Insert into BST).cpp
@@ -176,3 +176,94 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, stoi, string, to_string
+#include <vector> // vector
+
+template <typename T>
+struct Node {
+    T val;
+    Node<T>* left;
+    Node<T>* right;
+
+    explicit Node(T val, Node<T>* left = nullptr, Node<T>* right = nullptr)
+        : val{val}, left{left}, right{right} {}
+
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+Node<int>* insert_bst(Node<int>* bst, int val) {
+    if (bst == nullptr) return new Node<int>(val);
+    if (bst->val < val) {
+        bst->right = insert_bst(bst->right, val);
+    } else if (bst->val > val) {
+        bst->left = insert_bst(bst->left, val);
+    }
+    return bst;
+}
+
+// this function build a tree from input
+// learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+template<typename T, typename Iter, typename F>
+Node<T>* build_tree(Iter& it, F f) {
+    std::string val = *it;
+    ++it;
+    if (val == "x") return nullptr;
+    Node<T>* left = build_tree<T>(it, f);
+    Node<T>* right = build_tree<T>(it, f);
+    return new Node<T>{f(val), left, right};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+template<typename T, typename F>
+void format_tree(Node<T>* node, F f, std::vector<std::string>& out) {
+    if (node == nullptr) {
+        out.emplace_back("x");
+        return;
+    }
+    out.emplace_back(f(node->val));
+    format_tree(node->left, f, out);
+    format_tree(node->right, f, out);
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<std::string> bst_vec = get_words<std::string>();
+    auto bst_it = bst_vec.begin();
+    Node<int>* bst = build_tree<int>(bst_it, [](auto s) { return std::stoi(s); });
+    int val;
+    std::cin >> val;
+    ignore_line();
+    Node<int>* res = insert_bst(bst, val);
+    std::vector<std::string> res_vec;
+    format_tree(res, [](auto v) { return std::to_string(v); }, res_vec);
+    put_words(res_vec);
+}
