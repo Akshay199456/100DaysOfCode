f7e938909b8c78202fa0592f4fd29ee7fd7e21d6
Akshay Mysoe <akshay.kum94@gmail.com>
2022-07-26 20:53:26 -0700

    completed pb 13 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/13(Invert Binary Tree).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/13(Invert Binary Tree).cpp
@@ -43,7 +43,17 @@ Inverted tree:
 -------------------------    Other Approaches 
 1.
 
-    Time complexity: O()
+to invert a binary tree, can simply switch the left and right nodes.
+
+1. decide on the return value
+    for the return value, his questions asks for an inverted version of the original tree, so each step we return the 
+    inverted version of the current subtree
+
+2. identify states
+    no additional states are carried other than the current tree that we are inverting
+
+
+    Time complexity: O(n)
     Space complexity: O()
 */
 
@@ -157,3 +167,80 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <sstream> // istringstream
+#include <string> // getline, stoi, string, to_string
+#include <vector> // vector
+
+template <typename T>
+struct Node {
+    T val;
+    Node<T>* left;
+    Node<T>* right;
+
+    explicit Node(T val, Node<T>* left = nullptr, Node<T>* right = nullptr)
+        : val{val}, left{left}, right{right} {}
+
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+Node<int>* invert_binary_tree(Node<int>* tree) {
+    if (tree == nullptr) return nullptr;
+    return new Node<int>(tree->val, invert_binary_tree(tree->right), invert_binary_tree(tree->left));
+}
+
+// this function build a tree from input
+// learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+template<typename T, typename Iter, typename F>
+Node<T>* build_tree(Iter& it, F f) {
+    std::string val = *it;
+    ++it;
+    if (val == "x") return nullptr;
+    Node<T>* left = build_tree<T>(it, f);
+    Node<T>* right = build_tree<T>(it, f);
+    return new Node<T>{f(val), left, right};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+template<typename T, typename F>
+void format_tree(Node<T>* node, F f, std::vector<std::string>& out) {
+    if (node == nullptr) {
+        out.emplace_back("x");
+        return;
+    }
+    out.emplace_back(f(node->val));
+    format_tree(node->left, f, out);
+    format_tree(node->right, f, out);
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<std::string> tree_vec = get_words<std::string>();
+    auto tree_it = tree_vec.begin();
+    Node<int>* tree = build_tree<int>(tree_it, [](auto s) { return std::stoi(s); });
+    Node<int>* res = invert_binary_tree(tree);
+    std::vector<std::string> res_vec;
+    format_tree(res, [](auto v) { return std::to_string(v); }, res_vec);
+    put_words(res_vec);
