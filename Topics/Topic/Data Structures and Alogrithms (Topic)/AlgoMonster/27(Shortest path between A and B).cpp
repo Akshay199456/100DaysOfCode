f47e3750947a6681c603734df9b618876f552f1b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-08-04 18:20:44 -0700

    cmpleted pb 27 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/27(Shortest path between A and B).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/27(Shortest path between A and B).cpp
@@ -40,9 +40,12 @@ since the problem asks for the shortest path between the two locations a and b, 
 
 /*
 -------------------------    Notes
+bfs is best for finding the distance between two nodes sice it traverses level by level. since the graph is already built for us get_neighbors function retirieves the adjacency list
 
+n is the no of nodes in the graph and m the number of edges. the time spent isequal to the nummber of nodes and edges in the worst
+case. consider for example a linerar graph 0->1->2->3 and so on where we want to get from end to end, we would traverse every node and edge excatly once.
 
-    Time complexity: O()
+    Time complexity: O(n+m)
     Space complexity: O()
 */
 
@@ -130,3 +133,80 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <queue> // queue
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <unordered_set> // unordered_set
+#include <vector> // vector
+
+std::vector<int> get_neighbors(std::vector<std::vector<int>> graph, int node) {
+    return graph[node];
+}
+
+// BFS template
+int bfs(std::vector<std::vector<int>> graph, int root, int target) {
+    std::queue<int> q;
+    q.push(root);
+    std::unordered_set<int> visited;
+    visited.emplace(root);
+    int level = 0;
+    while (q.size() > 0) {
+        int n = q.size();
+        for (int i = 0; i < n; i++) {
+            int node = q.front();
+            if (node == target) return level;
+            for (int neighbor : get_neighbors(graph, node)) {
+                if (visited.count(neighbor)) continue;
+                q.push(neighbor);
+                visited.emplace(neighbor);
+            }
+            q.pop();
+        }
+        // increment level after we have processed all nodes of the level
+        level++;
+    }
+    return level;
+}
+
+int shortest_path(std::vector<std::vector<int>> graph, int a, int b) {
+    return bfs(graph, a, b);
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    int graph_length;
+    std::cin >> graph_length;
+    ignore_line();
+    std::vector<std::vector<int>> graph;
+    for (int i = 0; i < graph_length; i++) {
+        graph.emplace_back(get_words<int>());
+    }
+    int a;
+    std::cin >> a;
+    ignore_line();
+    int b;
+    std::cin >> b;
+    ignore_line();
+    int res = shortest_path(graph, a, b);
+    std::cout << res << '\n';
+}
+A quick note on "unweighted". Sometimes, graph's edges can have weight. For example, a graph that represents city connections and edges' weights represent the distance between cities.
+
+BFS can find the shortest path for unweighted graphs. For weighted graphs, we need algorithms like Dijkstra's algorithm. Dijkstra's algorithm rarely comes up in coding interviews so we won't get into details here.
