dd09fa9cbd07822b53a28cd676e27ad60a532177
Akshay Mysoe <akshay.kum94@gmail.com>
2022-08-30 17:52:24 -0700

    completed pb 36 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/36(Task Scheduling 2).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/36(Task Scheduling 2).cpp
@@ -33,10 +33,15 @@ requirements = [["a", "b"], ["c", "b"], ["b", "d"]]
 
 /*
 -------------------------    Notes
+since this problem is similat to task scheduling, we still use the tempalte for kahn's algorithm. the basic ideas is to perform
+a topolgical sort ecept we also keep track of the largest amount of time expended for a partcular path through the graph. the time it takes to complete a task in inhibited by
+the most time-consuming task in its dependencies. we make sure that we update every node such that it contains the maximum time needed to complete 
+every prerequisite task. we then make sure to update every children such that it contains the distance as the final answer is essentially the maximal path through the graph
 
-
-    Time complexity: O()
+    Time complexity: O(n+m)
     Space complexity: O()
+
+time complexity is equal to n the number of nodes in the graph plsu m the number of edges in the graph. this is because we have to go through every connection and node once when we sort the graph.
 */
 
 
@@ -142,3 +147,104 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <limits> // numeric_limits
+#include <queue> // queue
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <unordered_map> // unordered_map
+#include <vector> // vector
+
+template <typename T> std::unordered_map<T, int> count_parents(std::unordered_map<T, std::vector<T>> graph) {
+    std::unordered_map<T, int> counts;
+    for (auto entry : graph) {
+        counts[entry.first] = 0;
+    }
+    for (auto entry : graph) {
+        for (auto node : entry.second) {
+            counts[node] += 1;
+        }
+    }
+    return counts;
+}
+
+int topo_sort(std::unordered_map<std::string, std::vector<std::string>> graph, std::unordered_map<std::string, int> task_times) {
+    int ans = 0;
+    std::queue<std::string> q;
+    std::unordered_map<std::string, int> distances;
+    for (auto entry : graph) {
+        distances[entry.first] = 0;
+    }
+    std::unordered_map<std::string, int> counts = count_parents(graph);
+    for (auto entry : counts) {
+        if (entry.second == 0) {
+            q.push(entry.first);
+            distances[entry.first] = task_times[entry.first];
+            ans = std::max(ans, distances[entry.first]);
+        } 
+    }
+    while (q.size() > 0) {
+        std::string node = q.front();
+        for (std::string child : graph[node]) {
+            counts[child] -= 1;
+            distances[child] = std::max(distances[child], distances[node] + task_times[child]);
+            ans = std::max(ans, distances[child]);
+            if (counts[child] == 0) q.push(child);
+        }
+        q.pop();
+    }
+    return ans;
+}
+
+int task_scheduling_2(std::vector<std::string> tasks, std::vector<int> times, std::vector<std::vector<std::string>> requirements) {
+    std::unordered_map<std::string, std::vector<std::string>> graph;
+    std::unordered_map<std::string, int> task_times;
+    for (int i = 0; i < tasks.size(); i++) {
+        graph[tasks[i]] = std::vector<std::string>();
+        task_times[tasks[i]] = times[i];
+    }
+    for (std::vector<std::string> req : requirements) {
+        graph[req[0]].emplace_back(req[1]);
+    }
+
+    return topo_sort(graph, task_times);
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<std::string> tasks = get_words<std::string>();
+    std::vector<int> times = get_words<int>();
+    int requirements_length;
+    std::cin >> requirements_length;
+    ignore_line();
+    std::vector<std::vector<std::string>> requirements;
+    for (int i = 0; i < requirements_length; i++) {
+        requirements.emplace_back(get_words<std::string>());
+    }
+    int res = task_scheduling_2(tasks, times, requirements);
+    std::cout << res << '\n';
+}
