321ba473857bd54f27454d1c711ea243d50445ff
Akshay Mysoe <akshay.kum94@gmail.com>
2022-09-03 13:55:17 -0700

    completed pb 39 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/39(Minimum Spanning Tree | Forests).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/39(Minimum Spanning Tree | Forests).cpp
@@ -51,7 +51,8 @@ We can erect fencing between trees for the following pairs, 1 and 2, 2 and 4, 3 
 
 /*
 -------------------------    Notes
-
+main idea is to run kruska's algorithm with some tweaked aspects. instead of terminating when -1 nodes have been mered, we are notified that it may not be oossible to merge every node. therefore, we simply just let the algorithm run until every ede is checked. one can also run prim's but make sure that every single node
+is calculated. every connected componmnet needs to have prim's algorithm run on it.
 
     Time complexity: O()
     Space complexity: O()
@@ -172,3 +173,35 @@ int main() {
 
 
 //  Other Approaches(1)
+from typing import List, Tuple
+
+class UnionFind:
+    def __init__(self):
+        self.id = {}
+
+    def find(self, x):
+        y = self.id.get(x, x)
+        if y != x:
+            self.id[x] = y = self.find(y)
+        return y
+
+    def union(self, x, y):
+        self.id[self.find(x)] = self.find(y)
+
+def mst_forest(trees : int, pairs : List[Tuple[int]]) -> int:
+    # sort list, make sure to define custom comparator class cmp to sort edge based on weight from lowest to highest
+    pairs.sort(key = lambda pair: pair[2])
+    dsu = UnionFind()
+    ret = 0
+    for pair in pairs:
+      # check if pairs belong to same set before merging and adding edge to mst
+      if dsu.find(pair[0]) != dsu.find(pair[1]):
+        dsu.union(pair[0], pair[1])
+        ret += pair[2]
+    return ret
+
+if __name__ == '__main__':
+    trees = int(input())
+    pairs = [[int(x) for x in input().split()] for _ in range(int(input()))]
+    res = mst_forest(trees, pairs)
+    print(res)
