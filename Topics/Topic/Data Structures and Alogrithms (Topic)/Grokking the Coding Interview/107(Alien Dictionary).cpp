32cdb102c4758fc88941057f1d7ed843b1cdfad8
Akshay Mysoe <akshay.kum94@gmail.com>
2022-09-06 18:24:32 -0700

    completed pb 107 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/107(Alien Dictionary).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/107(Alien Dictionary).cpp
@@ -64,10 +64,29 @@ From the above five points, we can conclude that the correct character order is:
 
 /*
 -------------------------    Notes
+since the given words are sorted lexicographically by the rules of the alien language, we ccan alwasys compare two adjacent words to determine the ordering of the acharacters.
+take example-1 above: [ba,bc,ac,cab]
 
+    1. tkae th first two words ba and bc. starting from the begnniong of the words, find the first character that is
+    different in bopth words: it would be 'a' from 'ba' and 'c' from 'bc'. because of the sorted order of words i.e dictioanry),
+    we can conclude that 'a' comes before 'c' in the alien language.
+    2. similarly, from 'bc' and 'ac', we can conclude that 'b' comes before 'a'
 
-    Time complexity: O()
-    Space complexity: O()
+these two points tell us that we are actually asked to find the topoliogical ordering of the characters and that the ordering 
+rules should be inferred from adjacent words from the alien dictionary
+
+this makes the current problem similar to tasks scheduling order, the only difference being that we need to build th egraph
+of the characters by comparing adjacent words first and then perform the topological sort for the graph to determine the order fo the 
+characters
+
+in step 'd', each task can becpme a source onyl once and each edge (a rule) will be accessed and removed ocne.
+therefore, the time complexity of the above algorithm will be O(v+e_ whwere 'v' is the total number of different
+charactrers and 'e' is the total number of rules in the alien languuage. since at most, eacg pair of words can give us one rule,
+therefore, we cna conlclude that the upper bound fot he riules is O(n) where n is the number of words in the input. so we can say the time
+complexity of our aglorithm is O(v+n)
+
+    Time complexity: O(v+n)
+    Space complexity: O(v+n)
 */
 
 
@@ -196,3 +215,87 @@ int main(int argc, char *argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class AlienDictionary {
+ public:
+  static string findOrder(const vector<string> &words) {
+    if (words.empty() || words.empty()) {
+      return "";
+    }
+
+    // a. Initialize the graph
+    unordered_map<char, int> inDegree;        // count of incoming edges for every vertex
+    unordered_map<char, vector<char>> graph;  // adjacency list graph
+    for (auto word : words) {
+      for (char character : word) {
+        inDegree[character] = 0;
+        graph[character] = vector<char>();
+      }
+    }
+
+    // b. Build the graph
+    for (int i = 0; i < words.size() - 1; i++) {
+      string w1 = words[i], w2 = words[i + 1];  // find ordering of characters from adjacent words
+      for (int j = 0; j < min(w1.length(), w2.length()); j++) {
+        char parent = w1[j], child = w2[j];
+        if (parent != child) {             // if the two characters are different
+          graph[parent].push_back(child);  // put the child into it's parent's list
+          inDegree[child]++;               // increment child's inDegree
+          break;  // only the first different character between the two words will help us find the
+                  // order
+        }
+      }
+    }
+
+    // c. Find all sources i.e., all vertices with 0 in-degrees
+    queue<char> sources;
+    for (auto entry : inDegree) {
+      if (entry.second == 0) {
+        sources.push(entry.first);
+      }
+    }
+
+    // d. For each source, add it to the sortedOrder and subtract one from all of its children's
+    // in-degrees if a child's in-degree becomes zero, add it to the sources queue
+    string sortedOrder;
+    while (!sources.empty()) {
+      char vertex = sources.front();
+      sources.pop();
+      sortedOrder += vertex;
+      vector<char> children =
+          graph[vertex];  // get the node's children to decrement their in-degrees
+      for (char child : children) {
+        inDegree[child]--;
+        if (inDegree[child] == 0) {
+          sources.push(child);
+        }
+      }
+    }
+
+    // if sortedOrder doesn't contain all characters, there is a cyclic dependency between
+    // characters, therefore, we will not be able to find the correct ordering of the characters
+    if (sortedOrder.length() != inDegree.size()) {
+      return "";
+    }
+
+    return sortedOrder;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  string result = AlienDictionary::findOrder(vector<string>{"ba", "bc", "ac", "cab"});
+  cout << "Character order: " << result << endl;
+
+  result = AlienDictionary::findOrder(vector<string>{"cab", "aaa", "aab"});
+  cout << "Character order: " << result << endl;
+
+  result = AlienDictionary::findOrder(vector<string>{"ywx", "wz", "xww", "xz", "zyy", "zwz"});
+  cout << "Character order: " << result << endl;
+}
