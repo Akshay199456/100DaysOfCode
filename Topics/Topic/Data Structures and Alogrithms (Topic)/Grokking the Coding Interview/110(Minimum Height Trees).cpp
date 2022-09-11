424eaaa2c735846230a1e770df246eec4f2fc9a9
Akshay Mysoe <akshay.kum94@gmail.com>
2022-09-11 14:39:54 -0700

    completed pb 110 grok

--- Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/110(Minimum Height Trees).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/Grokking the Coding Interview/110(Minimum Height Trees).cpp
@@ -114,10 +114,31 @@ Output:[1]s
 
 /*
 -------------------------    Notes
+from the above mentioned examples, can clearly see that any leaf node (i.e node with only one edge) can never give us an MHT because its adjacent non-leaf nodes will always give us an MHT with a smaller height.
+all the adjacent non-leadnodes will consider the leaf node as a subtree
+
+lets understand this with another example. suppose we have a tree with root 'M' and height '5'. now if we take another node, say 'P' and make the 'M'
+tree as its subtree, then the height of the overall treee with root 'p' will be '6'(5+1). now this while tree can be considered a graph, where 'p' is a leaf as it has only one 
+edge (connection with 'm'). this clearly shows that the lead node 'p') gives us a tree of height '6' whereas its adjacent non-leaf node ('m') gives  us a tree with smaller
+height '5' since 'p' will be a child of 'm'.
+
+this gives us a stretegy to find MHTs. since leaves can't give us MHTs, we can remove them from the graph and remove their edges too. once we remove the leaves, we will have new leaves. since these new leaves
+cant give us mht, we will repeat hte process and remove them formt he graph too. we will prune the leaves untitl we are left with one or two nodes which will be our answer and the roots for MHTs.
+
+we can implement the above process using the topological sort. any node with only one edge (i.e a leaf) can be our source end
+and in a stepwise fashion, we can remove lal sources from the graph to find new sources, we will repeat htis process untutl we are left with one or two nodes in the graph which will be our answer
 
 
-    Time complexity: O()
-    Space complexity: O()
+Additional notes:
+
+as soonas they had mentioned that leaf nodes will never be mst because adjacent non-leaf nodes will always hjave 1 less node, i knoew they would use the stretegy to keep removeing
+leaf by leaf. that is a stretegy we have used a while ago in a few problems to get the answer to the one with th eleast/most umber of something. and they used the same stretgy here as well.
+
+to follow the topoligical sort, there is just one difference. since it is an undirected graph, we add both nodes to each other. so the one with the leaves have indegree of 1 insteaf of 0 in a typical directed grpah. and they use that
+to get rid of the leaves layer by layer till we get to the smallest layer we can have
+
+    Time complexity: O(v+e)
+    Space complexity: O(v+e)
 */
 
 
@@ -240,3 +261,103 @@ int main(int argc, char* argv[]) {
 
 
 //  Other Approaches(1)
+using namespace std;
+
+#include <deque>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class MinimumHeightTrees {
+ public:
+  static vector<int> findTrees(int nodes, vector<vector<int>>& edges) {
+    vector<int> minHeightTrees;
+    if (nodes <= 0) {
+      return minHeightTrees;
+    }
+
+    // with only one node, since its in-degree will be 0, therefore, we need to handle it separately
+    if (nodes == 1) {
+      minHeightTrees.push_back(0);
+      return minHeightTrees;
+    }
+
+    // a. Initialize the graph
+    unordered_map<int, int> inDegree;       // count of incoming edges for every vertex
+    unordered_map<int, vector<int>> graph;  // adjacency list graph
+    for (int i = 0; i < nodes; i++) {
+      inDegree[i] = 0;
+      graph[i] = vector<int>();
+    }
+
+    // b. Build the graph
+    for (int i = 0; i < edges.size(); i++) {
+      int n1 = edges[i][0], n2 = edges[i][1];
+      // since this is an undirected graph, therefore, add a link for both the nodes
+      graph[n1].push_back(n2);
+      graph[n2].push_back(n1);
+      // increment the in-degrees of both the nodes
+      inDegree[n1]++;
+      inDegree[n2]++;
+    }
+
+    // c. Find all leaves i.e., all nodes with only 1 in-degree
+    deque<int> leaves;
+    for (auto entry : inDegree) {
+      if (entry.second == 1) {
+        leaves.push_back(entry.first);
+      }
+    }
+
+    // d. Remove leaves level by level and subtract each leave's children's in-degrees.
+    // Repeat this until we are left with 1 or 2 nodes, which will be our answer.
+    // Any node that has already been a leaf cannot be the root of a minimum height tree, because
+    // its adjacent non-leaf node will always be a better candidate.
+    int totalNodes = nodes;
+    while (totalNodes > 2) {
+      int leavesSize = leaves.size();
+      totalNodes -= leavesSize;
+      for (int i = 0; i < leavesSize; i++) {
+        int vertex = leaves.front();
+        leaves.pop_front();
+        vector<int> children = graph[vertex];
+        for (auto child : children) {
+          inDegree[child]--;
+          if (inDegree[child] == 1) {  // if the child has become a leaf
+            leaves.push_back(child);
+          }
+        }
+      }
+    }
+
+    std::move(std::begin(leaves), std::end(leaves), std::back_inserter(minHeightTrees));
+    return minHeightTrees;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  vector<vector<int>> vec = {{0, 1}, {1, 2}, {1, 3}, {2, 4}};
+  vector<int> result = MinimumHeightTrees::findTrees(5, vec);
+  cout << "Roots of MHTs: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  vec = {{0, 1}, {0, 2}, {2, 3}};
+  result = MinimumHeightTrees::findTrees(4, vec);
+  cout << "Roots of MHTs: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  vec = {{0, 1}, {1, 2}, {1, 3}};
+  result = MinimumHeightTrees::findTrees(4, vec);
+  cout << "Roots of MHTs: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+  cout << endl;
+}
