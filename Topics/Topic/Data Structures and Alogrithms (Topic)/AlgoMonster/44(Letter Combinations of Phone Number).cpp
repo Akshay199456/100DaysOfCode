e313e82868af00b8e7005eb80558d50008d683ab
Akshay Mysoe <akshay.kum94@gmail.com>
2022-09-14 07:53:58 -0700

    completed pb 44 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/44(Letter Combinations of Phone Number).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/44(Letter Combinations of Phone Number).cpp
@@ -39,7 +39,25 @@ Output:
 
 /*
 -------------------------    Notes
+this is essentially asking for all permutations with the constraint of a  number to letter mapping.
 
+1. identify state(s)
+
+    to construct a letter combination we need
+
+    1. the letter we have selected so far
+
+    to make a choice when we viist the current node's children, we dont need to maintain any additional state since the next possible letters
+    are defined by the number to letter mapping
+
+2. Draw the tree
+
+3. DFS on the tree
+
+    since state makes at most 4 recursive calls, we have O(4^n) states. in our base case, we retuire O(n) o build string. so, final runtime is 
+    O(4^n * n)
+
+    n is the length of the input string. worse case we have 4 choices for every numbner, average case we have 3 choices so it should be closer to O(3^n *n)
 
     Time complexity: O()
     Space complexity: O()
@@ -111,3 +129,47 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout
+#include <iterator> // ostream_iterator, prev
+#include <string> // getline, string
+#include <unordered_map> // unordered_map
+#include <vector> // vector
+
+std::unordered_map<char, std::string> keyboard = {{'2',"abc"}, {'3',"def"}, {'4',"ghi"},
+    {'5',"jkl"}, {'6',"mno"}, {'7',"pqrs"}, {'8',"tuv"}, {'9',"wxyz"}};
+
+void dfs(std::vector<char> path, std::vector<std::string>& res, std::string digits, int i) {
+    if (path.size() == digits.size()) {
+        std::string s(path.begin(), path.end());
+        res.emplace_back(s);
+        return;
+    }
+    for (char& c: keyboard[digits[i]]) {
+        path.emplace_back(c);
+        dfs(path, res, digits, i + 1);
+        path.pop_back();
+    }
+}
+
+std::vector<std::string> letter_combinations_of_phone_number(std::string digits) {
+    std::vector<std::string> res;
+    dfs({}, res, digits, 0);
+    return res;
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::string digits;
+    std::getline(std::cin, digits);
+    std::vector<std::string> res = letter_combinations_of_phone_number(digits);
+    put_words(res);
+}
