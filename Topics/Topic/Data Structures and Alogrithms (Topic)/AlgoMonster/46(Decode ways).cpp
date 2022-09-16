31eefa8c117904da8c6d2c1c1b2e1fe46d9beb8c
Akshay Mysoe <akshay.kum94@gmail.com>
2022-09-15 21:27:05 -0700

    completed pb 46 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/46(Decode ways).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/46(Decode ways).cpp
@@ -35,7 +35,16 @@ Explanation: "123" can be decoded as "ABC", "LC", "AW"
     we reutrn 0 as no combination is possible. 
 
     Time complexity: O(2^n)
-    Space complexity: O(2^n)
+    Space complexity: O(n)
+
+
+2. memoization
+
+    we can improve upon our original approach by using memoization instead. whenever we come across a statee that we have already evalaueted before, we return the value.
+    that way, we dont repeat calculations that have beend one before.
+
+    Time complexity: O(n)
+    Space complexity: O(n)
 */
 
 
@@ -108,6 +117,63 @@ int main() {
 }
 
 
+// My Approaches(2)
+#include <iostream> // cin, cout
+#include <string> // getline, string
+#include <unordered_map>
+#include <utility>
+
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const std::pair<T1, T2>& p) const
+    {
+        auto hash1 = std::hash<T1>{}(p.first);
+        auto hash2 = std::hash<T2>{}(p.second);
+ 
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;             
+        }
+         
+        // If hash1 == hash2, their XOR is zero.
+          return hash1;
+    }
+};
+
+
+int dfs(std::string & digits, int start, int step, std::unordered_map<std::pair<int,int>, int, hash_pair> & memo){
+    if(start + step > digits.size())
+        return 0;
+    else if(memo.find(std::make_pair(start, step)) != memo.end())
+        return memo[std::make_pair(start, step)];
+    else{
+        std::string sub = digits.substr(start, step);
+        int intValue = std::stoi(sub);        
+        if(intValue < 0 || intValue > 26)
+            return 0;
+        else if(start + step == digits.size())
+            return 1;
+            
+        int returnValue = dfs(digits, start+step, 1, memo) + dfs(digits, start+step, 2, memo);
+        memo[std::make_pair(start, step)] = returnValue;
+        return returnValue;
+    }
+}
+
+int decode_ways(std::string digits) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    std::unordered_map<std::pair<int,int>, int, hash_pair> memo;
+    return dfs(digits, 0, 1, memo) + dfs(digits, 0, 2, memo);
+}
+
+int main() {
+    std::string digits;
+    std::getline(std::cin, digits);
+    int res = decode_ways(digits);
+    std::cout << res << '\n';
+}
+
+
+
 
 //  Other Approaches(1)
 int dfs(int start_index, std::string digits, std::string letters[26]) {
@@ -133,3 +199,40 @@ int decode_ways(std::string digits) {
     
     return dfs(0, digits, letters);
 }
+
+
+// Other Approaches(2)
+nt dfs(int start_index, std::string digits, std::string letters[26]) {
+int dfs(int start_index, int memo[], std::string digits, std::string letters[26]) {
+    if (start_index == digits.length()) return 1;
+    
+    if (memo[start_index] == -1) {
+    int ways = 0;
+        int ways = 0;
+    std::string remaining = digits.substr(start_index);
+        std::string remaining = digits.substr(start_index);
+    for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < 26; i++) {
+        std::string prefix = letters[i];
+            std::string prefix = letters[i];
+        if (remaining.find(prefix) == 0) {
+            if (remaining.find(prefix) == 0) {
+            ways += dfs(start_index + prefix.length(), digits, letters);
+                ways += dfs(start_index + prefix.length(), memo, digits, letters);
+            }
+        }
+        memo[start_index] = ways;
+    }
+    return ways;
+    return memo[start_index];
+}
+int decode_ways(std::string digits) {
+    std::string letters[26];
+    for (int i = 0; i < 26; i++) {
+        letters[i] = std::to_string(i + 1);
+    }
+    
+    return dfs(0, digits, letters);
+    int memo[digits.length()];
+    std::fill_n(memo, digits.length(), -1);
+    return dfs(0, memo, digits, letters);
