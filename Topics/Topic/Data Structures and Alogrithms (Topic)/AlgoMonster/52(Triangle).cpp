75aaca54d3d28fe06ab439c05736556ddd1c9235
Akshay Mysoe <akshay.kum94@gmail.com>
2022-10-04 21:02:28 -0700

    completed pb 52 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/52(Triangle).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/52(Triangle).cpp
@@ -50,10 +50,46 @@ The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11.
 
 /*
 -------------------------    Notes
+trickiest part of this part is understanding the indexing and which ones correspond to traversing the left branch or the right branch
 
+once you finish playing around with the examples given to you, you might notice that for any 'node' at position triangle[i][j],
+the left branch is triangle[i+1][j] and the right branch is trinagle[i+1][j+1]. this becomes extremely apprarent if we left-align the nodes:
+
+1. brute force
+
+    a brute force enumerates through all possibilities such that at each step we rety the left branch and right branch, then figuring out which branch
+    gives us the minimum sum. this can be done with a recursive combinatorial search with a time complexity of O(2^n).
+
+    runtime is O(2^n) since for each state we have two choices - go left or go right. the space complexity is O(n) since we have at most O(n) function calls in the 
+    memory stack at any given time
+
+2. DFS + memoization
+
+    idea for memoization for this problem comes from the idea that for certain values in triangle, it may be useful for more than one
+    future computation. For example, in the diagram, we see that the nodes labelled A and B depend on node C. So intead, of recomputing the value correspondong to node C every time, we can store our result for node C in a memo table.
+
+    since each node can be uniquely identified based on their row and comlum value, memo will be a 2d array where memo[row][col] represents the min path sum up to the node corresponding.
+
+    runtime is O(n^2) since there are O(n^2) states and each state takles O(1) to compute. the space complexity is O(n^2) too due to the use of hte n by n memo table/
+
+
+3. Bottom up
+
+    Alternatively you can use a bottom-up approach. again the idea of bottom-up is to build our solution from the smallest solutions
+    up to the one we want. so, we can start from the last tow and build our way up to the first row. our state and transition remains the same
+
+    State:
+        dp[r][c] is the min path sum starting from rth row and cth column
+    Transition
+        dp[r][c] = min(dp[r-1][c], dp[r-1][c-1]) + triangle[r][c]
 
     Time complexity: O()
     Space complexity: O()
+
+4. Memory optimization )Optional)
+
+    lastly, we can optimize our memory usage by noticing that our transition only uses information from the row below it. this, for every row, we only need to keep track fo thr row
+    below and everything else beyond that is useless. this takes our memory from O(n^2) to O(n)
 */
 
 
@@ -74,6 +110,9 @@ void getMin(std::vector<std::vector<int>> & triangle, int & minValue, int sum, i
             minValue = sum;
         return;
     }
+    // if any branch has a sum greater than minValue after it has been set (pruning)
+    else if((sum != std::numeric_limits<int>::max()) && (sum > minValue))
+        return;
     
     sum  += triangle[level][index];
     getMin(triangle, minValue, sum, index, level+1);
@@ -115,7 +154,153 @@ int main() {
     int res = minimum_total(triangle);
     std::cout << res << '\n';
 }
+}
 
 
 
 //  Other Approaches(1)
+int min_path_sum(vector<vector<int>> &triangle, int row, int col) {
+  if (row == (int) triangle.size()) {
+    return 0;
+  }
+  int left_sum = min_path_sum(triangle, row + 1, col);
+  int right_sum = min_path_sum(triangle, row + 1, col + 1);
+  return triangle[row][col] + min(left_sum, right_sum);
+
+}
+int minimum_total(vector<vector<int>> &triangle) {
+  return min_path_sum(triangle, 0, 0);
+}
+
+
+// Other Approaches(2)
+#include <algorithm> // copy, min
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int dfs(std::vector<std::vector<int>>& triangle, int level, int column, std::vector<std::vector<int>>& memo) {
+    if (level == triangle.size()) return 0;
+    if (memo[level][column] != std::numeric_limits<int>::max()) {
+        return memo[level][column];
+    }
+    int left = dfs(triangle, level + 1, column, memo);
+    int right = dfs(triangle, level + 1, column + 1, memo);
+    memo[level][column] = std::min(left, right) + triangle[level][column];
+    return memo[level][column];
+}
+
+int minimum_total(std::vector<std::vector<int>> triangle) {
+    int n = triangle.size();
+    std::vector<std::vector<int>> memo(n, std::vector<int>(n, std::numeric_limits<int>::max()));
+    return dfs(triangle, 0, 0, memo);
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    int triangle_length;
+    std::cin >> triangle_length;
+    ignore_line();
+    std::vector<std::vector<int>> triangle;
+    for (int i = 0; i < triangle_length; i++) {
+        triangle.emplace_back(get_words<int>());
+    }
+    int res = minimum_total(triangle);
+    std::cout << res << '\n';
+}
+
+
+// Other Approaches(3)
+int minimum_total(vector<vector<int>> &triangle) {
+  int n = (int) triangle.size();
+
+  vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+  for (int i = 0; i < n; i++) { // first deal with last row
+    dp[n - 1][i] = triangle[n - 1][i];
+  }
+
+  for (int i = n - 2; i >= 0; i--) { // start from second last row and build up to (0, 0)
+    for (int j = 0; j <= i; j++) {
+      dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+    }
+  }
+
+  return dp[0][0];
+}
+
+// Other Approaches(4)
+include <algorithm> // copy, min
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+using namespace std;
+
+int minimum_total(vector<vector<int>> triangle) {
+  int n = (int) triangle.size();
+  // dp[0][...] is current row
+  // dp[1][...] is row below
+  vector<vector<int>> dp(2, vector<int>(n + 1, 0));
+  
+  for (int i = 0; i < n; i++) { // first deal with last row
+    dp[1][i] = triangle[n - 1][i];
+  }
+
+  for (int i = n - 2; i >= 0; i--) {
+    for (int j = 0; j <= i; j++) {
+      dp[0][j] = min(dp[1][j], dp[1][j + 1]) + triangle[i][j];
+    }
+
+    for (int j = 0; j <= i; j++) { // swap rows - now dp[0][...] becomes row below
+      dp[1][j] = dp[0][j];
+    }
+  }
+
+  return dp[1][0]; // last swap makes our answer on the second row
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    int triangle_length;
+    std::cin >> triangle_length;
+    ignore_line();
+    std::vector<std::vector<int>> triangle;
+    for (int i = 0; i < triangle_length; i++) {
+        triangle.emplace_back(get_words<int>());
+    }
+    int res = minimum_total(triangle);
+    std::cout << res << '\n';
+}
