031deae4b5428a6e382d770a5eeffd0120604d3c
Akshay Mysoe <akshay.kum94@gmail.com>
2022-10-06 08:33:19 -0700

    added my approaches(2) pb 53 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/53(Coin change).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/53(Coin change).cpp
@@ -27,6 +27,11 @@ Output: -1
 
     Time complexity: O()
     Space complexity: O()
+
+2. top-down DP
+    because, we use combinatorics to generate all possible combination there can be states that are repeated during that recursion.
+    as a result, we make use of a set to store the two states which represent whether an element has alreay occurred or not.
+    those are the (level of recursion, sum). both of these togehther, let us know if we have already come across this state and help us get rid of branches whoich have been calculated before.
 */
 
 
@@ -111,6 +116,89 @@ int main() {
     int res = coin_change(coins, amount);
     std::cout << res << '\n';
 }
+
+
+// My Approaches(2)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+#include <unordered_set>
+
+// Hash function 
+struct hashFunction
+{
+  size_t operator()(const std::pair<int,int> &x) const
+  {
+    return x.first ^ x.second;
+  }
+};
+
+void getMinCoins(std::vector<int> & coins, int amount, int & minCoins, int sum, int count, std::unordered_set<std::pair<int,int>, hashFunction> & visited){
+    // if sum > amount, we stop
+    std::pair<int,int> currPair = std::make_pair(sum,count);
+    
+    if(sum > amount)
+        return;
+    else if(sum == amount){
+        // if sum == amount
+        if(count < minCoins)
+            minCoins = count;
+        return;
+    }
+    // if minCoins is set and number of coins exceeds it
+    else if((minCoins != std::numeric_limits<int>::max()) && (count > minCoins))
+        return;
+    // if we have already come across this pair before
+    else if(visited.find(currPair) != visited.end())
+        return;
+    
+    visited.emplace(currPair);
+    for(int i=0; i<coins.size(); i++)
+        getMinCoins(coins, amount, minCoins, sum + coins[i], count+1, visited);
+}
+
+
+int coin_change(std::vector<int> coins, int amount) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    int minCoins = std::numeric_limits<int>::max();
+    std::unordered_set<std::pair<int,int>, hashFunction> visited;
+    
+    if(!amount)
+        return 0;
+    
+    getMinCoins(coins, amount, minCoins,0,0, visited);
+    if(minCoins == std::numeric_limits<int>::max())
+        return -1;
+    return minCoins;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> coins = get_words<int>();
+    int amount;
+    std::cin >> amount;
+    ignore_line();
+    int res = coin_change(coins, amount);
+    std::cout << res << '\n';
+}
+
 
 
 
