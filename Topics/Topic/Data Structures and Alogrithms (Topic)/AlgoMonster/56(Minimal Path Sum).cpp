5be5181a510163ab77dc3baecc7e3ffef1f8607b
Akshay Mysoe <akshay.kum94@gmail.com>
2022-10-19 16:40:25 -0700

    added my approaches(2) pb 56 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/56(Minimal Path Sum).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/56(Minimal Path Sum).cpp
@@ -27,8 +27,19 @@ Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
     which is the clsoest to destination adn then going to 0,0 from there to build up a solution piece by piece.
     if both the rightsum and bottomum are INT_MAX, then we know that we jave arroved at the answer, so we return 0 instead.
 
+    Time complexity: O(2^max(m*n))
+    Space complexity: O(max(m*n))
+
+
+2. top-down approach
+
+    we can improve upon the brute force approach by storing duplicae calucluations that have already been done before in a two d matrix. that way we can skip through
+    duplicate calucluations
+
     Time complexity: O(m*n)
     Space complexity: O(m*n)
+
+
 */
 
 
@@ -104,6 +115,70 @@ int main() {
     int res = min_path_sum(grid);
     std::cout << res << '\n';
 }
+
+
+// My Approaches(2)
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int getMinSum(std::vector<std::vector<int>> & grid, int currX, int currY, int maxSizeX, int maxSizeY, std::vector<std::vector<int>> & memo){
+    if(currX == maxSizeX || currY == maxSizeY)
+        return std::numeric_limits<int>::max();
+    else if(memo[currX][currY] != std::numeric_limits<int>::max())
+        return memo[currX][currY];
+    
+    int rightSum = getMinSum(grid, currX, currY+1, maxSizeX, maxSizeY, memo);
+    int bottomSum = getMinSum(grid, currX+1, currY, maxSizeX, maxSizeY, memo);
+    
+    if(rightSum == std::numeric_limits<int>::max() && bottomSum == std::numeric_limits<int>::max())
+        memo[currX][currY] = grid[currX][currY];
+    else
+        memo[currX][currY] = grid[currX][currY] + std::min(rightSum, bottomSum);
+    
+    return memo[currX][currY];
+}
+
+int min_path_sum(std::vector<std::vector<int>> grid) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    std::vector<std::vector<int>> memo(grid.size(), std::vector<int>(grid[0].size(), std::numeric_limits<int>::max()));
+    if(!grid.size())
+        return 0;
+    return getMinSum(grid,0,0,grid.size(),grid[0].size(), memo);
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    int grid_length;
+    std::cin >> grid_length;
+    ignore_line();
+    std::vector<std::vector<int>> grid;
+    for (int i = 0; i < grid_length; i++) {
+        grid.emplace_back(get_words<int>());
+    }
+    int res = min_path_sum(grid);
+    std::cout << res << '\n';
+}
+
+
+
 
 
 
