5aa11ed5e728649322cb0347037d0476bc0b8c1e
Akshay Mysoe <akshay.kum94@gmail.com>
2023-01-25 20:59:47 -0800

    added other approaches(1) pb 23-1 algomonster

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/23-1(Subarray sum).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/23-1(Subarray sum).cpp
@@ -42,10 +42,28 @@ Explanation: -20 - 3 + 30 = 7. The indices for subarray [-20,-3,30] is 1 and 4 (
     number of elements in the array. there are n subarrays with size 1, n-1 subarrays with size 2 and 1 subarray with size n.
     time complexity is O(n^2)
 
-    key observation is that
+    key observation is that the sum of a subarray [i,j] is equal to the sum of [0,j] minus the sum of [0,i-1]
+    The sum of elements from index 0 to i is called thre prefix sum. if we have the subarray sum for each index, we can find the 
+    sum of any subarray in constant time.
 
-    Time complexity: O()
+    with the knowledge of the prefix sum under our belt, the problem boils down to two sum. we keep a dictionary of prefix_sum: index while
+    going through the array calculating prefix_sum. if at any point, prefix_sum - target is in the dictionary, we have
+    found our subarray.
+
+
+    Time complexity: O(n)
     Space complexity: O()
+
+
+2. Prefix sum
+
+    So, it took me a while to understnd this approach. if you look at the image Additional Notes(1)-7, what the problem asks us is to finmd
+    a subarray with sum k. now, we can keep a cumulative sum from the beginning of the array and lets name that sum. in that case, if we want to find whether
+    k exists for the sum of the subarray, what we just need to check is if sum-k exists in the map wheree sum-k is the sum from the beginning 
+    og the array to point where the subarray with sum k exists. this is because if k exists and we know sum exists, then if we are able to find sum-k, we can infer k exists. and that
+    means a subarray with sum k exists.
+
+    that's how the technique gets applied here for prefix_sum.
 */
 
 
@@ -116,6 +134,125 @@ int main() {
 }
 
 
+// My Approaches(2)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+#include <unordered_map>
+
+std::vector<int> subarray_sum(std::vector<int> arr, int target) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    std::unordered_map<int,int> elementMap;
+    int cumSum = 0;
+    elementMap[0] = 0;
+    
+    for(int i=0; i< arr.size(); i++){
+        cumSum += arr[i];
+        int complement =  cumSum - target;
+        if(elementMap.find(complement) != elementMap.end())
+            return {elementMap[complement], i+1};        
+        elementMap[cumSum] = i+1;
+    }
+    
+    return {};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    std::vector<int> res = subarray_sum(arr, target);
+    put_words(res);
+}
+
+
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <unordered_map> // unordered_map
+#include <vector> // vector
+
+std::vector<int> subarray_sum(std::vector<int> arr, int target) {
+    std::unordered_map<int, int> prefix_sums;
+    // an empty subarray has a sum of 0
+    prefix_sums[0] = 0;
+    int cur_sum = 0;
+    for (int i = 0 ; i < arr.size(); i++) {
+        cur_sum += arr[i];
+        int complement = cur_sum - target;
+        if (prefix_sums.count(complement)) {
+            return {prefix_sums[complement], i + 1};
+        }
+        prefix_sums[cur_sum] = i + 1;
+    }
+    // return no indices if arr is empty
+    return {};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    std::vector<int> res = subarray_sum(arr, target);
+    put_words(res);
+}
