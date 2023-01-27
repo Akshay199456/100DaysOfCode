f09b7eb87b4d9a59ba6523b5bd568069c8009a2a
Akshay Mysoe <akshay.kum94@gmail.com>
2023-01-26 20:22:59 -0800

    completed pb 61 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/61(Fixed Size Sliding Window).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/61(Fixed Size Sliding Window).cpp
@@ -30,7 +30,11 @@ For example, if the input is nums = [1, 2, 3, 7, 4, 1], k = 3, then the output w
 
 /*
 -------------------------    Notes
-
+the systematic approach is to check the sum of each subarray with size k and find the largest one. we start frm the leftmost
+window of size k in the array, and we can keep track of a window_sum variable that stores the sum of the current window.
+when we shift the window to the right, we add a new element to the right of the window and remove the leftmost element from the window.
+to obtain the window_sum of the new window, we can add the value of the new elmeent and subtract the value we removed on the left. then, for each window, we wish 
+to check whether the window_sum is at least largest where largest is the current largest sum among all processed windows.
 
     Time complexity: O()
     Space complexity: O()
@@ -97,3 +101,50 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int subarray_sum_fixed(std::vector<int> nums, int k) {
+    int window_sum = 0;
+    for (int i = 0; i < k; ++i) {
+      window_sum = window_sum + nums[i];
+    }
+    int largest = window_sum;
+    for (int right = k; right < nums.size(); ++right) {
+        int left = right - k;
+        window_sum = window_sum - nums[left];
+        window_sum = window_sum + nums[right];
+        largest = std::max(largest, window_sum);
+    
+    }
+    return largest;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> nums = get_words<int>();
+    int k;
+    std::cin >> k;
+    ignore_line();
+    int res = subarray_sum_fixed(nums, k);
+    std::cout << res << '\n';
+}
