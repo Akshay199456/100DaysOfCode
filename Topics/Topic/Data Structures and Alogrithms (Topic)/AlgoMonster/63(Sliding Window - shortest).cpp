82474b6edac750329e8beb27832ef638f09a86ad
Akshay Mysoe <akshay.kum94@gmail.com>
2023-01-29 17:25:11 -0800

    added my approaches(1) pb 63 algo

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/63(Sliding Window - shortest).cpp
@@ -0,0 +1,99 @@
+/*
+Problem statement:
+
+Let's continue on finding the sum of subarrays. This time given a positive integer array nums, we want to find the length of the shortest subarray such that the subarray sum is at least target. Recall the same example with input nums = [1, 4, 1, 7, 3, 0, 2, 5] and target = 10, then the smallest window with the sum >= 10 is [7, 3] with length 2. So the output is 2.
+*/
+
+/*
+-------------------------    My Approaches:
+1. Sliding window - varying size
+
+
+    we can solve this problem with slidign window of verying size just like pb 62. the only idfference here is that we want t
+    'gind a window that is o he samllest size that satisfies the condition. as a result, as long as currSum + nums[end] satieisfies the condition, 
+    we keep eanding the window. when it satisfies the conditoion, there is no point expanding it after since any other window after that will only be larger.
+    so, we push beg forward and get the minlength set.
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int subarray_sum_shortest(std::vector<int> nums, int target) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    int beg = 0, end = 0, currSum = 0, minLength = std::numeric_limits<int>::max();
+    if(!nums.size())
+        return 0;
+    
+    while(end < nums.size()){
+        if(currSum + nums[end] < target)
+            currSum += nums[end++];
+        else{
+            minLength = std::min(minLength, end-beg+1);
+            currSum -= nums[beg++];
+        }
+    }
+    
+    if(!beg && end == nums.size())
+        return nums.size();
+    else if(currSum >= target)
+        minLength = std::min(minLength, end-beg+1);
+    
+    return minLength;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> nums = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = subarray_sum_shortest(nums, target);
+    std::cout << res << '\n';
+}
+
+
+
+//  Other Approaches(1)
