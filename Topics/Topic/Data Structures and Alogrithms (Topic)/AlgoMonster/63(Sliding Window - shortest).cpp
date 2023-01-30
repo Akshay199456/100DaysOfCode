3f6dee8fcdff221ad6ca95a821fcffab4a5907f8
Akshay Mysoe <akshay.kum94@gmail.com>
2023-01-29 17:39:20 -0800

    completed pb 63 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/63(Sliding Window - shortest).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/63(Sliding Window - shortest).cpp
@@ -30,7 +30,14 @@ Let's continue on finding the sum of subarrays. This time given a positive integ
 
 /*
 -------------------------    Notes
+a brute force is to use largest_sum_subarray_k as a subroutine to check whether there exists a subarray of each possiblle length k with sum larger
+than target adnd find the smallest k. but that woul be too slow. so we will alter the longest sliding window approach
+to now search for the shortest subarray. only this time, our goal is to shrink the window, we we will check whether a window is valid
+anmds shrink the valid iwndows to find th smalelst one.
 
+we want to find the shortest subarray that sumps up to at least target. if the sum of the current window is less than target, we must extend the window so that 
+we can increase the window_sum. if window_sum >= target, we want to shrink the window from the left side so that we could explore a smaller window that potentially
+adds up to target.
 
     Time complexity: O()
     Space complexity: O()
@@ -97,3 +104,48 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int subarray_sum_shortest(std::vector<int> nums, int target) {
+    int windowSum = 0, length = nums.size();
+    int left = 0;
+    for (int right = 0; right < nums.size(); ++right) {
+        windowSum = windowSum + nums[right];
+        while (windowSum >= target) {
+            length = std::min(length, right-left+1);
+            windowSum = windowSum - nums[left];
+            ++left;
+        }
+    }
+    return length;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> nums = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = subarray_sum_shortest(nums, target);
+    std::cout << res << '\n';
+}
