5771241636753399906b322d32705c31e980f5e1
Akshay Mysoe <akshay.kum94@gmail.com>
2023-01-30 19:40:58 -0800

    completed pb 64 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/64(Lease Consecutive Cards to MAtch).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/64(Lease Consecutive Cards to MAtch).cpp
@@ -34,7 +34,23 @@ For example, given cards = [3, 4, 2, 3, 4, 7], then picking up [3, 4, 2, 3] make
 
 /*
 -------------------------    Notes
+1. burte foce
 
+    can use brute force to find the next appearence of a card for every card in cards. the tuntime of this algorithm
+    will be O(n^2) as there are n cards and finding its match each takes O(n) time.
+
+
+2. Sliding Window
+
+    a faster algorithm is to use a sliding window on the cards. essentuakky, this question is asking for the shortest subarray that 
+    contains a duplicate. we start frm an emptyu window and continue adding cards to the right until we find a match, then we try to see whether we can discard some cards form the elft.
+    wheilt he window is valid, we caompare for the shorter length and find the least number of consecutive cards that contains a match.
+
+    we apply the flexible sliding window template to dinf the shortest subarray. since we are looking to match a pair, the deck is valid
+    when the card that was just added intot he winodow matches with a previous card within the window. that is, the number value of
+    cards[right[ appeared in our window twice , windowpcardspright]] == 2. then since we are looking for the shortest
+    window, we increasmetn the elft pointer until cards[right[ no longer make a match. followig that, we will continue to search for other valued windows towards the right and find the shortest
+    length/
 
     Time complexity: O()
     Space complexity: O()
@@ -94,3 +110,41 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout
+#include <iterator> // back_inserter, istream_iterator
+#include <map> // map
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int least_consecutive_cards_to_match(std::vector<int>& cards) {
+    std::map<int, int> window;
+    int left = 0, shortest = cards.size()+1;
+    for (int right = 0; right < cards.size(); ++right) {
+        window[cards[right]]++;
+        while (window[cards[right]] == 2) {
+            shortest = std::min(shortest, right-left+1);
+            window[cards[left]]--;
+            ++left;
+        }
+    }
+    return shortest != cards.size()+1 ? shortest : -1;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+int main() {
+    std::vector<int> cards = get_words<int>();
+    int res = least_consecutive_cards_to_match(cards);
+    std::cout << res << '\n';
+}
