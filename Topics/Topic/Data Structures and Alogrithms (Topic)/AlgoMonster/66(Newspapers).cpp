e1e66b3f27b25ace3e1170445fac7a0323ac3b66
Akshay Mysoe <akshay.kum94@gmail.com>
2023-02-01 16:28:58 -0800

    added my approaches(1) pbb 66 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/66(Newspapers).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/66(Newspapers).cpp
@@ -54,16 +54,169 @@ as that is the bottleneck.
 
 /*
 -------------------------    Notes
+at minimum, the amount of time required to go through all the newspapers has be >= max(newspapers_read_times) to be able to finish reading the longest newspaper.
+At maximum, if the amoiunt of time taken to go through all the newspapers is sum(newspapers_read_times), then it would reeequire only 1 coworker to finish the work. since we want to split thhe work of going 
+through all the newspapers amoung num_coworkers workers, the minimum amount of time require to finish reading all the newspapers is somewhere is ebtween max(newspapers_read_times) and 
+sum(newspapers_read_times). now, we simply need to perform binary search on the possible time values, checking if each time limit is enough to finish the work.
 
+we check if a particular time works by trying to split the work among the coworkers and see if the total time taken 
+exceeds the given time limit. we do so by moving from left to right in the newspapers array while keeping tack of the time that the 
+current coworker spent. when the time that the current coworker spent exceeds the given time limit, we reset the current time tracker and add 1 to our coworker
+count. when we finish checking the newspapers array, we check if the required coworker count exceeds the one we are given. if it does, then we know that the 
+given time limit doesnt work and we must try a higher value. otherwise, we want to try a lower value to look for the minimum time limit that works.
 
-    Time complexity: O()
+Setting the inital low and high values takes O(n) to find out the maximum value and the sum of newspapers_read_times.
+then performing binary search takes O(logn) and the helper function feasible() that is called inside the binary search loop is O(n). Overall the binary
+search takes O(nlogn) which is more significant than O(n) so the time complexity of our solution is O(nlogn)
+
+    Time complexity: O(n logn)
     Space complexity: O()
 */
 
 
 
 //  My approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+void getMinMax(std::vector<int> newspapers_read_times, int & maxTime, int & minTime){
+    for(int i=0; i<newspapers_read_times.size(); i++){
+        maxTime += newspapers_read_times[i];
+        minTime = std::max(minTime, newspapers_read_times[i]);
+    }
+}
+
+bool feasible(std::vector<int> newspapers_read_times, int num_coworkers, int mid){
+    int n_workers = 0, time = 0;
+    for(int i=0; i<newspapers_read_times.size(); i++){
+        if(time + newspapers_read_times[i] > mid){
+            time = 0;
+            n_workers +=1;
+        }
+        time += newspapers_read_times[i];
+    }
+    
+    if(time)
+        n_workers +=1;
+    return n_workers <= num_coworkers;
+}
+
+
+int newspapers_split(std::vector<int> newspapers_read_times, int num_coworkers) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    int maxTime = 0, minTime = std::numeric_limits<int>::min();
+    int result = -1;
+    getMinMax(newspapers_read_times, maxTime, minTime);
+    
+    while(minTime <= maxTime){
+        int mid = minTime + (maxTime-minTime)/2;
+        if(feasible(newspapers_read_times, num_coworkers, mid)){
+            result = mid;
+            maxTime = mid-1;
+        }
+        else
+            minTime = mid+1;
+    }
+    
+    return result;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> newspapers_read_times = get_words<int>();
+    int num_coworkers;
+    std::cin >> num_coworkers;
+    ignore_line();
+    int res = newspapers_split(newspapers_read_times, num_coworkers);
+    std::cout << res << '\n';
+}
 
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy, max_element
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <numeric> // accumulate
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+bool feasible(std::vector<int> newspapers_read_times, int num_coworkers, int limit) {
+    // time to keep track of the current worker's time spent, num_workers to keep track of the number of coworkers used
+    int time = 0, num_workers = 0;
+    for (int read_time : newspapers_read_times) {
+        // check if current time exceeds the given time limit
+        if (time + read_time > limit) {
+            time = 0;
+            num_workers++;
+        }
+        time += read_time;
+    }
+    // edge case to check if we needed an extra worker at the end
+    if (time != 0) {
+        num_workers++;
+    }
+    // check if the number of workers we need is more than what we have
+    return num_workers <= num_coworkers;
+}
+
+int newspapers_split(std::vector<int> newspapers_read_times, int num_coworkers) {
+    int low = *std::max_element(newspapers_read_times.begin(), newspapers_read_times.end());
+    int high = std::accumulate(newspapers_read_times.begin(), newspapers_read_times.end(), 0);
+    int ans = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (feasible(newspapers_read_times, num_coworkers, mid)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> newspapers_read_times = get_words<int>();
+    int num_coworkers;
+    std::cin >> num_coworkers;
+    ignore_line();
+    int res = newspapers_split(newspapers_read_times, num_coworkers);
+    std::cout << res << '\n';
+}
