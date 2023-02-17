8dd20455c570c52eb647b6b47467059f7354d2d4
Akshay Mysoe <akshay.kum94@gmail.com>
2023-02-16 20:55:48 -0800

    completed pb 70 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/70(Combinatorial Problem).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/70(Combinatorial Problem).cpp
@@ -39,10 +39,21 @@ Output: ["aaaa", "aaab", "aaba", "aabb", "abaa", "abab", "abba", "abbb", "baaa",
 
 /*
 -------------------------    Notes
+we can start from an empoty string and add letters repatedly until we get to the n length. each time we add a letter, we can
+pich from eithe a or b
 
+implementation
+
+applying the backtracking template to get the solution
+
+is_leaf: when word is n letters, we have reache a leaf(solution)
+get_edges: each letter is either 'a' or 'b'
 
     Time complexity: O()
+        for each node there are a maximum of 2 children. the height of the tree is n. the no of nodes i 2^n-1. it takes O(n) to join the n
+        characters at eac leaf node. so the overall time complexity is O(2^n *n)
     Space complexity: O()
+        the height of the tree is O(n) so the space complexity is O(n)
 */
 
 
@@ -94,3 +105,40 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // sort
+#include <iostream> // cin, cout, streamsize
+#include <limits> // numeric_limits
+#include <string> // string
+#include <vector> // vector
+
+void dfs(int n, std::vector<std::string> &res, int startIndex, std::string path) {
+    if (startIndex == n) {
+      std::string s(path.begin(), path.end());
+      res.emplace_back(s);
+      return;
+    }
+    for (std::string letter : {"a", "b"}) {
+      dfs(n, res, startIndex+1, path+letter);
+    }
+}
+
+std::vector<std::string> letter_combination(int n) {
+    std::vector<std::string> res;
+    dfs(n, res, 0, "");
+    return res;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    ignore_line();
+    std::vector<std::string> res = letter_combination(n);
+    std::sort(res.begin(), res.end());
+    for (const std::string& line : res) {
+        std::cout << line << '\n';
+    }
+}
