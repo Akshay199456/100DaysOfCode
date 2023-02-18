1037c2bd88a2876f9f93e9e00f8740502971bd9f
Akshay Mysoe <akshay.kum94@gmail.com>
2023-02-17 19:28:29 -0800

    modified pb 47 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/47(Palindrome Partioning).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/47(Palindrome Partioning).cpp
@@ -17,7 +17,7 @@ Output:
 
 /*
 -------------------------    My Approaches:
-1. 
+1.  [Coded in second round after understanding main code]
 
     Time complexity: O()
     Space complexity: O()
@@ -40,8 +40,13 @@ to be a palindrome)
 
 we prune the tree by not branching out when the prefix is not a palindrome
 
-    Time complexity: O(2^n)
-    Space complexity: O()
+    Time complexity: O(2^n *n)
+        estimating ity of backtrackong on a pruned tree is tricky because pruned branches should be excluded from the overall time
+        complexity. one way to estimate is to look at the operation we have done on the input. for each letter in the input string,
+        we can either include it as a previous string or start a new string with it. with these two choices, the toal number of operations
+        is 2^n. we also need O(n) to check oif the string is a palindrome. in total, the complexity is O(2^n * n)
+    Space complexity: O(n)
+        space complexity depends on the eight of the tree a dn the height of the state-sapce tree is worst case O(n)
 
 this is because once we determine a paldinrome we work backwards to consider the other palindromes and enumerate them accordingly.
 
@@ -61,6 +66,65 @@ therefore, i think these approaches are pretty similar.
 
 
 //  My approaches(1)
+#include <algorithm> // copy
+#include <iostream> // cin, cout
+#include <iterator> // ostream_iterator, prev
+#include <string> // getline, string
+#include <vector> // vector
+
+bool isPalindrome(std::string element){
+    int startIndex = 0, endIndex = element.size()-1;
+    while(startIndex < endIndex){
+        if(element[startIndex] != element[endIndex])
+            return false;
+        startIndex++;
+        endIndex--;
+    }
+    retuurn true;
+}
+
+
+void dfs(std::vector<std::vector<std::string>> & result, std::vector<std::string> & currPath, int startIndex, std::string s){
+    if(startIndex == s.size()){
+        result.push_back(currPath);
+        return;
+    }
+    
+    for(int endIndex=startIndex; endIndex < s.size(); endIndex++){
+        std::string currSubstring = s.substr(startIndex, endIndex-startIndex+1);
+        if(isPalindrome(currSubstring)){
+            currPath.push_back(currSubstring);
+            dfs(result, currPath, endIndex+1, s);
+            currPath.pop_back();
+        }
+    }
+}
+
+std::vector<std::vector<std::string>> partition(std::string s) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    std::vector<std::vector<std::string>> result;
+    std::vector<std::string> currPath;
+    dfs(result, currPath, 0, s);
+    return result;
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::string s;
+    std::getline(std::cin, s);
+    std::vector<std::vector<std::string>> res = partition(s);
+    for (const std::vector<std::string>& row : res) {
+        put_words(row);
+    }
+}
 
 
 
