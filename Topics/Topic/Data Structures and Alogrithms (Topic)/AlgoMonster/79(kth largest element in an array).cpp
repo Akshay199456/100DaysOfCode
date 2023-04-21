06194e67b13929138315800b54c8f605ad426279
Akshay Mysoe <akshay.kum94@gmail.com>
2023-04-21 07:14:31 -0700

    added my approaches(1) pb 80 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/79(kth largest element in an array).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/79(kth largest element in an array).cpp
@@ -31,16 +31,33 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
 
 /*
 -------------------------    Other Approaches 
-1.
+1. Solution
+
+    orting th earray before indexing gives us a O(nlogn) solutiopn. we can do better with a max heap. heapify
+    is O(n) and poppingthe first k ele,menmt is O(klogn) so combined is O(n+klogn)
+
+    Assuming thaat we used heapigy in place, then the space complexity ios O(1). if we had allocated a new max heap, then
+    the space complexity s On) 
 
     Time complexity: O()
     Space complexity: O()
+
+2. Quick Select
+    
+    alternatively we could use quick select for this particular problem. worst case sceaniro, it runs on O(n^2), but on average, it runs on O(n).
+    
+    quick slkeect uses the principole of quick sort, only during each process, we only need to care about the middle point compared to k-1 (because the question uses 1 indexing) and
+    depending on the result, we either stop the progrtam, recurse to the left side or recurse to the right side.
+
+    note that it is easy to get the indices of quick select wrong, so in a real coding interview, its often better to implement
+    the ehap solution and have less chance of error.
+
 */
 
 
 /*
 -------------------------    Notes
-
+s
 
     Time complexity: O()
     Space complexity: O()
@@ -105,3 +122,104 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <queue> // priority_queue
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int find_kth_largest(std::vector<int> nums, int k) {
+    std::priority_queue<int> heap(nums.begin(), nums.end());
+    for (int i = 0; i < k - 1; i++) {
+        heap.pop();
+    }
+    return heap.top();
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> nums = get_words<int>();
+    int k;
+    std::cin >> k;
+    ignore_line();
+    int res = find_kth_largest(nums, k);
+    std::cout << res << '\n';
+}
+
+// Other Approaches(2)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int find_kth_largest(std::vector<int> nums, int k) {
+    int min_ptr = 0, max_ptr = nums.size() - 1;
+    while (min_ptr < max_ptr) {
+        int pivot = nums[max_ptr];
+        int swap_left = min_ptr, swap_right = max_ptr;
+        while (swap_left < swap_right) {
+            while (swap_left < swap_right && nums[swap_left] > pivot) {
+                swap_left++;
+            }
+            while (swap_left < swap_right && nums[swap_right] <= pivot) {
+                swap_right--;
+            }
+            if (swap_left < swap_right) {
+                std::swap(nums[swap_left], nums[swap_right]);
+            }
+        }
+        std::swap(nums[swap_left], nums[max_ptr]);
+        if (swap_left == k - 1) {
+            return nums[swap_left];
+        } else if (swap_left < k - 1) {
+            min_ptr = swap_left + 1;
+        } else {
+            max_ptr = swap_left - 1;
+        }
+    }
+    return nums[min_ptr];
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> nums = get_words<int>();
+    int k;
+    std::cin >> k;
+    ignore_line();
+    int res = find_kth_largest(nums, k);
+    std::cout << res << '\n';
+}
---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/80(Kth smallest element in sorted matrix).cpp
@@ -0,0 +1,123 @@
+/*
+Problem statement:
+
+Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+Input:
+matrix = [
+  [ 1,  5,  9],
+  [10, 11, 13],
+  [12, 13, 15]
+],
+k = 8,
+Output: 13
+Note:
+You may assume k is always valid, 1 ≤ k ≤ n^2. You may also assume that 1 <= n <= 1000.
+*/
+
+/*
+-------------------------    My Approaches:
+1. using property of heap
+
+    we can solve this problem using a simple heap. we can go through each element and insert it into the heap keeping he size of the ehap
+    fixed at k. we are using a max heap btw. this way whenever an element greater than the top of the heap and the size of the heap is greater than or equal to k,
+    we pop the top of the heap and instead push this new elmeent in as we are looking for the kth smallest elmenet.
+    this element will be at the top of th e heap at the end.
+
+    Time complexity: O(n^2logk)
+    Space complexity: O(k)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+#include <queue>
+
+int getKSmallest(std::vector<std::vector<int>> matrix, int k){
+    std::priority_queue<int> pq;
+    
+    // insert elements into heap
+    for(int i=0; i< matrix.size(); i++){
+        for(int j=0; j < matrix[i].size(); j++){
+            if(pq.size() < k)
+                pq.push(matrix[i][j]);
+            else{
+                if(pq.top() > matrix[i][j]){
+                    pq.pop();
+                    pq.push(matrix[i][j]);
+                }
+            }
+        }
+    }
+    
+    // get top of heap and return it 
+    return pq.top();
+}
+
+
+int kth_smallest(std::vector<std::vector<int>> matrix, int k) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    return getKSmallest(matrix, k);
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    int matrix_length;
+    std::cin >> matrix_length;
+    ignore_line();
+    std::vector<std::vector<int>> matrix;
+    for (int i = 0; i < matrix_length; i++) {
+        matrix.emplace_back(get_words<int>());
+    }
+    int k;
+    std::cin >> k;
+    ignore_line();
+    int res = kth_smallest(matrix, k);
+    std::cout << res << '\n';
+}
+
+
+
+//  Other Approaches(1)
