bf3e9ca4ba6504dbb402d2f5534f01830d3b78e4
Akshay Mysore <akshay.kum94@gmail.com>
2024-04-09 20:43:11 -0700

    added my approaches(1) pb 86 algo

---
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/86(Insert interval).cpp
@@ -0,0 +1,74 @@
+/*
+Problem statement:
+
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example: 1
+Input:
+intervals = [[1,3],[6,9]]
+newInterval = [2,5]
+Output:
+[[1,5],[6,9]]
+Example: 2
+Input:
+intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+newInterval = [4,8]
+Output:
+[[1, 2], [3, 10], [12, 16]]
+Example: 3
+Input:
+intervals = []
+newInterval = [5,7]
+Output:
+[[5,7]]
+Example: 4
+Input:
+intervals = [[1,5]]
+newInterval = [2,3]
+Output:
+[[1,5]]
+Example: 5
+Input:
+intervals = [[1,5]]
+newInterval = [2,7]
+Output:
+[[1,7]]
+*/
+
+/*
+-------------------------    My Approaches:
+1. merge interval
+
+    we just push the new interval into the array, run sorting algorithm and then we can merge the new interval. there is nothing unique in that so i havent coded it
+
+    Time complexity: O(nlogn)
+    Space complexity: O(1)
+*/
+
+
+/*
+-------------------------    Other Approaches 
+1.
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+/*
+-------------------------    Notes
+
+
+    Time complexity: O()
+    Space complexity: O()
+*/
+
+
+
+//  My approaches(1)
+
+
+
+//  Other Approaches(1)
