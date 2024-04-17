45f9ab892828fd081e3e32e7daf8b35caeced800
Akshay Mysore <akshay.kum94@gmail.com>
2024-04-16 20:46:07 -0700

    completed pb 89 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/89(Daily Temperatures).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/89(Daily Temperatures).cpp
@@ -29,10 +29,26 @@ The length of temperatures will be in the range [1, 30000]. Each temperature wil
 
 /*
 -------------------------    Other Approaches 
-1.
+1. Solution
 
-    Time complexity: O()
-    Space complexity: O()
+    consider the brute force solution that searches through the entire array to find the next warmer day for each day, the runtime is O(n^2). we want to avoid traversing through the entire array
+    during every day, and we can achieve this by optimiing using a monotonic stack
+
+    we ill traverse through the enture array only once and use the stack to store past information. since we want to traverse through the array only once, at index i, we wouldnt know the next warmer
+    day until we visit it, so we store i in the stack. the stack sotres all the days without a solution, in increasing index vlaue. once we encounter a warmer day, we keep popping the indices with lower termpertatures
+    and add the corresponding solution (th enumber of days passed) for each popped day. the logic is as follows: on a new day i, we want to check wehther  the temperature t[i] is warmer than the term,perature on the top of the stack
+    (stack[-1]); we have two outcomes:
+        1. t[1] > t[stack[-1]], then we have found a solution for day prev_day_index = stack[-1], we pop the stack and store res[prev_day_index] = i-prev_day_index.
+        2. t[i] <= t[stack[-1]] and we put day i onto the stack
+
+    note that we continue checking condition 1 with the new top of the stack until we reach condition 2 or there is nothing in the stack. alsom obersve that we always pop the 
+    stacj until t[1] <= t[stack[-1]], so that each time we push an index i onto the stack, we know that the previous index has a warmer temperature. since each day is coller than the previous
+    day on the stack, we deduce that the termperatures associated with these idneices are monotonically decreasing in the stack (from bottom to top of the stack). with the stack property that associated temperatures are
+    monotonically decreasing, we can guarnatee each day will get popped when we find its solution.
+
+    Time complexity: O(n)
+        we lopp throug the array once, and the stack is an O(1) data structure for insertion and popping.
+    Space complexity: O(n)
 */
 
 
@@ -156,3 +172,55 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <sstream> // istringstream
+#include <stack> // stack
+#include <string> // getline, string
+#include <vector> // vector
+
+std::vector<int> daily_temperatures(std::vector<int> t) {
+    std::vector<int> res(t.size(), 0);
+    // stores indices of previous colder days
+    std::stack<int> stack;
+    for (int i = 0; i < t.size(); i++) {
+        // while the stack is not empty and current day is warmer than the day referred by the top of the stack
+        while (!stack.empty() && t[stack.top()] < t[i]) {
+            // get the index of the colder previous day and pop it from stack
+            int prev_day_index = stack.top();
+            stack.pop();
+            // set the popped index in the res array to (current index - the popped index)
+            res[prev_day_index] = i - prev_day_index;
+        }
+        // push the current index to stack
+        stack.push(i);
+    }
+    return res;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<int> t = get_words<int>();
+    std::vector<int> res = daily_temperatures(t);
+    put_words(res);
+}
