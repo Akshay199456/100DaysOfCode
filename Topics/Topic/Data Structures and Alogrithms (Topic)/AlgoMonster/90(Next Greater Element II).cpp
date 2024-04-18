02d2d88d7ee9ddb8898ed7d897f946d31e9a72d6
Akshay Mysore <akshay.kum94@gmail.com>
2024-04-18 08:46:36 -0700

    completed pb 90 algo

--- Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/90(Next Greater Element II).cpp
+++ Topics/Topic/Data Structures and Alogrithms (Topic)/AlgoMonster/90(Next Greater Element II).cpp
@@ -63,10 +63,37 @@ The length of the given array won't exceed 10000.
 
 /*
 -------------------------    Notes
+solution
+
+    this problem is very similar to daily temperatures, except the array is circular. the only change in the implementation is that after the pass, we do a second pass to find a larger
+    number for the remaining items on the stack without pushing new items in (since they are pushed at least once already in the first pass)
+
+    we use the stack to store visited indices without a solution. during each iteration, we visit a new value cur and pop all the smaller elements from the stack until we see an element larger
+    than cur. when an element is popped, we know that cur is greater than that element, thus we have found a next_greater solution for that specific element. that means that when cur is added onto
+    the stack, cur is smaller than its previous element (if it exists) in the stack. the stack follows a monotonically decreasing property, in other words,, each element is smaller than its
+    previous elmeents. by this property, we can guarantee that ekeemnt i will be popped when we find its soluition, because the eelemtns following i in the stack are smaller than element i and therefore will be
+    popped before
 
 
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(n)
+        as we  only trraversed through nums twice and each elemnt has only been pushed and popped once in the stack.
+    Space complexity: O(n)
+
+Additional notes:
+    now that i have played with their approach and understand it, i can descirbe what it does in my own words,
+
+    the first pass is exactly the same a mine no change at all.
+    the secon pass is just slightly different from what i did. what they do is that they still go on a second complete run through the array (on mine, i stop when i reach the index
+    of the max elemnt since no element can be greater than that so no point goin forward after finding it). however, they dont insert any elements into the stack/deque for the second run.
+    instead, they just use the values fo the elenments in the second run to find the next greater element for the elments already in the stack. as a result when you go through the second run,
+    you will come across the leemnts that are larger than the elemtn in the top of the stack in hte proper order. when that happens, we know that element is the next greater element fo thr 
+    element in the top fo the stack and so we pop it and continue to do so just like the first run. we just dont add these leemnts onto the stack/deque since that would just continue the cyucle.
+    similar to my approach, once you arrive at the largest elemtn which is in the stack, no other element can be greater than it so it doesnt matter going through the rest of the elements as
+    no other elemnt can be greater. that is why i stiopped a tthe index of the largest elemnt. in this approach, you just pass through the rest of the elements since they cant be larger than the alrgest
+    element and hence they wont pop anything.
+
+    overall, both approaches are verys imilar.
+
 */
 
 
@@ -153,3 +180,55 @@ int main() {
 
 
 //  Other Approaches(1)
+#include <algorithm> // copy
+#include <iostream> // boolalpha, cin, cout
+#include <iterator> // back_inserter, istream_iterator, ostream_iterator, prev
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+std::vector<int> next_greater_elements(std::vector<int> nums) {
+    std::vector<int> stack;
+    std::vector<int> res(nums.size(), -1);
+
+    for (int i = 0; i < nums.size(); i++) {
+        while (!stack.empty() && nums[i] > nums[stack.back()]) {
+            res[stack.back()] = nums[i];
+            stack.pop_back();
+        }
+        stack.emplace_back(i);
+    }
+    for (int num : nums) {
+        while (num > nums[stack.back()]) {
+            res[stack.back()] = num;
+            stack.pop_back();
+        }
+    }
+    return res;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+template<typename T>
+void put_words(const std::vector<T>& v) {
+    if (!v.empty()) {
+        std::copy(v.begin(), std::prev(v.end()), std::ostream_iterator<T>{std::cout, " "});
+        std::cout << v.back();
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<int> nums = get_words<int>();
+    std::vector<int> res = next_greater_elements(nums);
+    put_words(res);
+}
